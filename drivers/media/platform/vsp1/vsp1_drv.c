9dc32493bc85a39433be92873d26bf54c3ebf28d
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2015-08-03 00:37:01 +0300

    v4l: vsp1: Add VSP+DU support
    Implement internal control of the VSP pipeline to be used by the DU
    DRM/KMS driver when using the VSP as an internal composer handled
    through DRM/KMS only.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    
    ---
    
    Changes since v2:
    
    - Make LIF mandatory for VSPD instances
    
    Changes since vsp1-kms-gen2-20150811:
    
    - Support non-RGB formats at the RPF input
    - Make BRU mandatory for VSPD instances
    - Squash with commits 'v4l: vsp1: Reset the DRM pipeline when stopping
      the stream', 'v4l: vsp1: Fix typo' and 'v4l: vsp1: Support disabling
      and already disabled RPF'.

--- drivers/media/platform/vsp1/vsp1_drv.c
+++ drivers/media/platform/vsp1/vsp1_drv.c
@@ -25,6 +25,7 @@
 
 #include "vsp1.h"
 #include "vsp1_bru.h"
+#include "vsp1_drm.h"
 #include "vsp1_hsit.h"
 #include "vsp1_lif.h"
 #include "vsp1_lut.h"
@@ -120,7 +121,7 @@ static int vsp1_create_sink_links(struct vsp1_device *vsp1,
 	return 0;
 }
 
-static int vsp1_create_links(struct vsp1_device *vsp1)
+static int vsp1_uapi_create_links(struct vsp1_device *vsp1)
 {
 	struct vsp1_entity *entity;
 	unsigned int i;
@@ -143,9 +144,6 @@ static int vsp1_create_links(struct vsp1_device *vsp1)
 		if (ret < 0)
 			return ret;
 	}
-
-	if (!vsp1->pdata.uapi)
-		return 0;
 
 	for (i = 0; i < vsp1->pdata.rpf_count; ++i) {
 		struct vsp1_rwpf *rpf = vsp1->rpf[i];
@@ -355,7 +353,10 @@ static int vsp1_create_entities(struct vsp1_device *vsp1)
 	}
 
 	/* Create links. */
-	ret = vsp1_create_links(vsp1);
+	if (vsp1->pdata.uapi)
+		ret = vsp1_uapi_create_links(vsp1);
+	else
+		ret = vsp1_drm_create_links(vsp1);
 	if (ret < 0)
 		goto done;
 
@@ -369,6 +370,8 @@ static int vsp1_create_entities(struct vsp1_device *vsp1)
 
 	if (vsp1->pdata.uapi)
 		ret = v4l2_device_register_subdev_nodes(&vsp1->v4l2_dev);
+	else
+		ret = vsp1_drm_init(vsp1);
 
 done:
 	if (ret < 0)
