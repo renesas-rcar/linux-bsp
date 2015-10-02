80302eda8c06ac8eae25beef44acf0319c95136b
Gaku Inami <gaku.inami.xw@bp.renesas.com>
2015-10-02 11:55:19 +0900

    arm64: renesas: r8a7795: Add Renesas R8A7795 SoC support
    Initial version of Renesas R-Car H3 support (V10)
    
    Signed-off-by: Gaku Inami <gaku.inami.xw@bp.renesas.com>
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Magnus Damm <damm+renesas@opensource.se>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    ---
    TODO:
    - Split this patch based on ARM-SoC maintainer preference
    
    Changes since v10:
    - None
    
    Changes since v9: (Magnus Damm <damm+renesas@opensource.se>)
    - Added clock-output-names for the CPG
    
    Changes since v8: (Magnus Damm <damm+renesas@opensource.se>)
    - Renamed xtal node name to drop _clk - thanks Geert!
    - Kconfig s/platform/platforms/g - thanks Laurent!
    - Added select PINCTRL - thanks Geert
    - Removed unused Makefile subdir line - thanks Laurent!
    
    Changes since v7: (Magnus Damm <damm+renesas@opensource.se>)
    - Folded together the following patches from v7:
       [PATCH 6/25] arm64: renesas: Add new Renesas R-Car Gen3 SoC Kconfig
       [PATCH 7/25] arm64: renesas: r8a7795: Add Renesas R8A7795 SoC support
       [PATCH 8/25] arm64: renesas: r8a7795: Add initial SoC support
    - Updated Kconfig bits
       Changed to CONFIG_ARCH_R8A7795 and CONFIG_RENESAS
       CONFIG_ARCH_SHMOBILE is still set to be able to build various drivers
       CONFIG_ARCH_SHMOBILE_MULTI is gone
       select PM_GENERIC_DOMAINS if PM
    - Moved "s3d4_clk" to clock patch from geert
    - Replaced CPG clock-output-names with clock-indices
    - set #power-domain-cells to 0

---
+++ include/dt-bindings/clock/r8a7795-clock.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2015 Renesas Electronics Corp.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+#ifndef __DT_BINDINGS_CLOCK_R8A7795_H__
+#define __DT_BINDINGS_CLOCK_R8A7795_H__
+
+/* CPG */
+#define R8A7795_CLK_MAIN		0
+#define R8A7795_CLK_PLL0		1
+#define R8A7795_CLK_PLL1		2
+#define R8A7795_CLK_PLL2		3
+#define R8A7795_CLK_PLL3		4
+#define R8A7795_CLK_PLL4		5
+
+/* MSTP0 */
+
+/* MSTP1 */
+
+/* MSTP2 */
+
+/* MSTP3 */
+
+/* MSTP5 */
+
+/* MSTP7 */
+
+/* MSTP8 */
+
+/* MSTP9 */
+
+/* MSTP10 */
+
+#endif /* __DT_BINDINGS_CLOCK_R8A7795_H__ */
