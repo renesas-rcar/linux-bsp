da172a4eb06f2b4c173256aaef4dd7c90147e426
Takeshi Kihara <takeshi.kihara.df@renesas.com>
2015-10-29 14:06:16 +0900

    serial: sh-sci: Add hardware flow control support
    TIOCM_CTS is enabled to use hardware flow control.
    And hardware flow control is handled by device tree.
    
    This patch adds SCIx device support based on the modification introduced
    by commit 0a8e03cea98c ("serial: sh-sci: Add hardware flow control
    support of R-Car HSCIF").
    
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/tty/serial/sh-sci.c
+++ drivers/tty/serial/sh-sci.c
@@ -1265,11 +1265,17 @@ static void sci_set_mctrl(struct uart_port *port, unsigned int mctrl)
 
 static unsigned int sci_get_mctrl(struct uart_port *port)
 {
+	struct sci_port *s = to_sci_port(port);
+	unsigned int mctrl = TIOCM_DSR | TIOCM_CAR;
+
 	/*
 	 * CTS/RTS is handled in hardware when supported, while nothing
 	 * else is wired up. Keep it simple and simply assert DSR/CAR.
 	 */
-	return TIOCM_DSR | TIOCM_CAR;
+	if (s->cfg->capabilities & SCIx_HAVE_RTSCTS)
+		mctrl |= TIOCM_CTS;
+
+	return mctrl;
 }
 
 #ifdef CONFIG_SERIAL_SH_SCI_DMA
@@ -2628,6 +2634,8 @@ sci_parse_dt(struct platform_device *pdev, unsigned int *dev_id)
 	p->type = info->type;
 	p->regtype = info->regtype;
 	p->scscr = SCSCR_RE | SCSCR_TE;
+	if (of_property_read_bool(np, "ctsrts"))
+		p->capabilities = SCIx_HAVE_RTSCTS;
 
 	index = of_property_match_string(np, "dma-names", "tx");
 	if (index >= 0)
