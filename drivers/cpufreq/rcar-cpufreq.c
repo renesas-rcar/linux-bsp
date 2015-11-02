12d54cde9ef4737a06797e727aea4f5f1b8bd191
Dien Pham <dien.pham.ry@rvc.renesas.com>
2015-11-02 20:14:33 +0700

    cpufreq: rcar: Support AVS for R-Car Gen3 DVFS
    Currently, OPPv2 framework has not supported multi opp table.
    This implementation is internal solution to implement AVS
    function on rcar platform.
    
    When OPPv2 framework supports multiple opp table officially,
    this implementation need to be re-implemented for adapting
    this feature.
    
    Detail of this implementation:
    1/ Read avs value (specific chip info) from hw.
    2/ With each cpufreq, before cpufreq driver (cpufreq-dt) init:
     - Check suitable avs value (in range from 0 and
       less than total opp in operating-points-v2 property.)
     - Get list of opp tables from operating-points-v2 property
       of cpu device node.
     - Re-set operating-points-v2 to table that has index is avs value.
     - Re-set operating-points-v2 property length for
       equivalent for just one opp table.
    
    3/ After cpufreq driver init, it will use the opp table
       that has index is avs value of step 2,
       and change frequency from that table.
    
    Signed-off-by: Dien Pham <dien.pham.ry@rvc.renesas.com>

--- drivers/cpufreq/rcar-cpufreq.c
+++ drivers/cpufreq/rcar-cpufreq.c
@@ -13,11 +13,88 @@
  * GNU General Public License for more details.
  */
 
-#include <linux/platform_device.h>
 #include <linux/module.h>
+#include <linux/platform_device.h>
+
+#ifdef CONFIG_POWER_AVS
+#include <linux/io.h>
+#include <linux/of.h>
+#include <linux/slab.h>
+
+/* Change the default opp_table pattern in device tree.
+ * Set opp_pattern_num is default.
+ */
+
+int change_default_opp_pattern(unsigned int opp_pattern_num)
+{
+	struct device_node *cpu_node = NULL;
+
+	__be32 *list, *pp_val;
+	int size;
+	struct property *pp;
+
+	for_each_node_with_property(cpu_node, "operating-points-v2") {
+		pp = of_find_property(cpu_node, "operating-points-v2", &size);
+		if (!pp || !pp->value)
+			return -ENOENT;
+
+		pp_val = pp->value;
+		size = size / sizeof(*pp_val);
+		if (size > opp_pattern_num) {
+			list = kzalloc(sizeof(*pp_val), GFP_KERNEL);
+			if (!list) {
+				pr_debug("%s(): kzalloc fail, return -ENOMEM\n",
+						__func__);
+				return -ENOMEM;
+			}
+			*list = *(pp_val + opp_pattern_num);
+			pp->value = list;
+		}
+		pp->length = sizeof(*list); /* opp fw only accept 1 opp_tb */
+
+		pr_debug("rcar-cpufreq: %s is running with: %s\n",
+			of_node_full_name(cpu_node),
+			of_node_full_name(of_find_node_by_phandle(
+				be32_to_cpup(pp->value))));
+	}
+
+	return 0;
+}
+
+/* Get AVS value */
+#define ADVFS_BASE		0xE60A0000
+#define KSEN_ADJCNTS		(ADVFS_BASE + 0x13C)
+#define VOLCOND_MASK_0_3	0x0f	/* VOLCOND[3:0] */
+
+#define AVS_TABLE_NUM	7
+
+unsigned int get_avs_value(void)
+{
+	unsigned int ret;
+	void __iomem *ksen_adjcnts = ioremap_nocache(KSEN_ADJCNTS, 4);
+	u32 ksen_adjcnts_value = ioread32(ksen_adjcnts);
+
+	ksen_adjcnts_value &= VOLCOND_MASK_0_3;
+	if (ksen_adjcnts_value >= 0 && ksen_adjcnts_value < AVS_TABLE_NUM) {
+		ret = ksen_adjcnts_value;
+	} else {
+		ret = 0;
+		pr_debug("rcar-cpufreq: hw get invalid avs value, use avs_tb0\n");
+	}
+	pr_debug("rcar-cpufreq: use avs value: %d\n", ksen_adjcnts_value);
+	iounmap(ksen_adjcnts);
+
+	return ret;
+}
+#endif /* CONFIG_POWER_AVS */
 
 int __init rcar_cpufreq_init(void)
 {
+#ifdef CONFIG_POWER_AVS
+	int avs_val = get_avs_value();
+
+	change_default_opp_pattern(avs_val);
+#endif /* CONFIG_POWER_AVS */
 	platform_device_register_simple("cpufreq-dt", -1, NULL, 0);
 	return 0;
 }
