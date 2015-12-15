1589489d3cbf2e54954a4983907727e993920636
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2015-12-15 10:38:59 +0900

    v4l: vsp1: Add FCPVD clock support
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/media/platform/vsp1/vsp1.h
+++ drivers/media/platform/vsp1/vsp1.h
@@ -60,6 +60,7 @@ struct vsp1_device_info {
 	unsigned int num_bru_inputs;
 	bool uapi;
 	unsigned int  wc;
+	bool fcpvd;
 };
 
 struct vsp1_device {
@@ -69,6 +70,7 @@ struct vsp1_device {
 
 	void __iomem *mmio;
 	struct clk *clock;
+	struct clk *fcpvd_clock;
 
 	struct mutex lock;
 	int ref_count;
