abce7ced9ed1f1b398e959a673ebd2df11828930
Hien Dang <hien.dang.eb@rvc.renesas.com>
2016-02-22 14:26:46 +0700

    ASoC: ak4613: Add empty suspend handler
    Signed-off-by: Hien Dang <hien.dang.eb@rvc.renesas.com>
    Signed-off-by: Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>

--- sound/soc/codecs/ak4613.c
+++ sound/soc/codecs/ak4613.c
@@ -444,7 +444,14 @@ static int ak4613_resume(struct snd_soc_codec *codec)
 	return regcache_sync(regmap);
 }
 
+static int ak4613_suspend(struct snd_soc_codec *codec)
+{
+	/* Empty function for now */
+	return 0;
+}
+
 static struct snd_soc_codec_driver soc_codec_dev_ak4613 = {
+	.suspend		= ak4613_suspend,
 	.resume			= ak4613_resume,
 	.set_bias_level		= ak4613_set_bias_level,
 	.controls		= ak4613_snd_controls,
