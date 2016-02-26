585dba7fda00f021eb64b211345294894616af42
Geert Uytterhoeven <geert+renesas@glider.be>
2016-02-26 11:42:10 +0100

    soc: renesas: rcar-sysc: Add support for R-Car H1 power areas
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    ---
    v4:
      - Drop R8A7779_PD_SH, as it's not documented in the datasheet,
      - Reformat table,
    
    v3:
      - New (converted from DT to C).

--- drivers/soc/renesas/rcar-sysc.c
+++ drivers/soc/renesas/rcar-sysc.c
@@ -308,6 +308,9 @@ static void __init rcar_sysc_pd_setup(struct rcar_sysc_pd *pd)
 }
 
 static const struct of_device_id rcar_sysc_matches[] = {
+#ifdef CONFIG_ARCH_R8A7779
+	{ .compatible = "renesas,r8a7779-sysc", .data = &r8a7779_sysc_info },
+#endif
 	{ /* sentinel */ }
 };
 
