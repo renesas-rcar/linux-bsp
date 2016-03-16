2499ae7eca91187c7ace35519ab6e17e9091b96a
Kazuya Mizuguchi <kazuya.mizuguchi.ks@renesas.com>
2016-03-16 15:04:17 +0900

    clk: renesas: r8a7796: Add HS-USB clock
    This patch adds HS-USB-IF clock for R8A7796 SoC.
    
    Signed-off-by: Kazuya Mizuguchi <kazuya.mizuguchi.ks@renesas.com>
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/clk/renesas/r8a7796-cpg-mssr.c
+++ drivers/clk/renesas/r8a7796-cpg-mssr.c
@@ -143,6 +143,7 @@ static const struct mssr_mod_clk r8a7796_mod_clks[] __initconst = {
 	DEF_MOD("vspd0",		 623,	R8A7796_CLK_S0D2),
 	DEF_MOD("ehci1",		 702,	R8A7796_CLK_S3D4),
 	DEF_MOD("ehci0",		 703,	R8A7796_CLK_S3D4),
+	DEF_MOD("hsusb",		 704,	R8A7796_CLK_S3D4),
 	DEF_MOD("du2",			 722,	R8A7796_CLK_S2D1),
 	DEF_MOD("du1",			 723,	R8A7796_CLK_S2D1),
 	DEF_MOD("du0",			 724,	R8A7796_CLK_S2D1),
