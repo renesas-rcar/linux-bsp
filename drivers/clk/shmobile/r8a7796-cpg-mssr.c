1295c8c9b799eba0048d0218a018df867b8253ff
Dien Pham <dien.pham.ry@rvc.renesas.com>
2016-03-18 22:02:32 +0700

    clk: shmobile: r8a7796: Adjust output of PLL0, PLL2 as SYSC-CPUs input
    In HWM, PLL0CR register part has below description:
     - PLL0 output: (PLL0VCO) = (PLL0 reference clock) × (setting+1)×2
     - Sys-cpu clk input: (SYS-CPU divider 1 input) = PLL0VCO × 1/2
    
    So, this patch modify output of PLL0 driver is as
    input of sys-cpu(s). And it is also similar for PLL2.
    
    Signed-off-by: Dien Pham <dien.pham.ry@rvc.renesas.com>

--- drivers/clk/shmobile/r8a7796-cpg-mssr.c
+++ drivers/clk/shmobile/r8a7796-cpg-mssr.c
@@ -248,7 +248,7 @@ struct clk * __init r8a7796_cpg_clk_register(struct device *dev,
 		 * the multiplier value.
 		 */
 		value = readl(base + CPG_PLL0CR);
-		mult = (((value >> 24) & 0x7f) + 1) * 2;
+		mult = ((value >> 24) & 0x7f) + 1;
 		break;
 
 	case CLK_TYPE_GEN3_PLL1:
@@ -263,7 +263,7 @@ struct clk * __init r8a7796_cpg_clk_register(struct device *dev,
 		 * the multiplier value.
 		 */
 		value = readl(base + CPG_PLL2CR);
-		mult = (((value >> 24) & 0x7f) + 1) * 2;
+		mult = ((value >> 24) & 0x7f) + 1;
 		break;
 
 	case CLK_TYPE_GEN3_PLL3:
