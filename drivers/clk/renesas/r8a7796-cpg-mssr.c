1905a75772797ddbf2d1b11bf7ec7ebe277c1a4d
Kazuya Mizuguchi <kazuya.mizuguchi.ks@renesas.com>
2016-03-24 17:18:52 +0900

    clk: renesas: r8a7796: Add ADSP clock
    This patch adds ADSP clocks for R8A7796 SoC.
    
    Signed-off-by: Kazuya Mizuguchi <kazuya.mizuguchi.ks@renesas.com>
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/clk/renesas/r8a7796-cpg-mssr.c
+++ drivers/clk/renesas/r8a7796-cpg-mssr.c
@@ -151,6 +151,7 @@ static const struct mssr_mod_clk r8a7796_mod_clks[] __initconst = {
 	DEF_MOD("intc-ap",		 408,	R8A7796_CLK_S3D1),
 	DEF_MOD("audmac0",		 502,	R8A7796_CLK_S3D4),
 	DEF_MOD("audmac1",		 501,	R8A7796_CLK_S3D4),
+	DEF_MOD("adsp",			 506,	R8A7796_CLK_S1D1),
 	DEF_MOD("drif7",		 508,	R8A7796_CLK_S3D2),
 	DEF_MOD("drif6",		 509,	R8A7796_CLK_S3D2),
 	DEF_MOD("drif5",		 510,	R8A7796_CLK_S3D2),
