9add21d9bf1d238daedb3b88ab0cbc60e77fa399
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2016-05-17 16:28:08 +0900

    drm: rcar-du: Fix remove function
    Since there isn't the suspend procedure in remove function,
    this patch adds it.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_crtc.h
+++ drivers/gpu/drm/rcar-du/rcar_du_crtc.h
@@ -80,6 +80,7 @@ enum rcar_du_output {
 int rcar_du_crtc_create(struct rcar_du_group *rgrp, unsigned int index);
 void rcar_du_crtc_enable_vblank(struct rcar_du_crtc *rcrtc, bool enable);
 void rcar_du_crtc_suspend(struct rcar_du_crtc *rcrtc);
+void rcar_du_crtc_remove_suspend(struct rcar_du_crtc *rcrtc);
 void rcar_du_crtc_resume(struct rcar_du_crtc *rcrtc);
 
 void rcar_du_crtc_route_output(struct drm_crtc *crtc,
