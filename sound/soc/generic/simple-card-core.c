d26b51d15c4aa5de72aec5c0f0639e5fc9d67020
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2016-05-23 14:59:09 +0900

    ASoC: simple-card-core: add asoc_simple_card_canonicalize_cpu()
    simple-card needs remove dai_link->cpu_dai_name if it CPU was
    single DAI.
    This patch makes this method simple style standard.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- sound/soc/generic/simple-card-core.c
+++ sound/soc/generic/simple-card-core.c
@@ -373,3 +373,22 @@ int asoc_simple_card_canonicalize_dailink(struct snd_soc_dai_link *dai_link)
 	return 0;
 }
 EXPORT_SYMBOL_GPL(asoc_simple_card_canonicalize_dailink);
+
+int asoc_simple_card_canonicalize_cpu(struct snd_soc_dai_link *dai_link,
+				      int is_single_links)
+{
+	/*
+	 * In soc_bind_dai_link() will check cpu name after
+	 * of_node matching if dai_link has cpu_dai_name.
+	 * but, it will never match if name was created by
+	 * fmt_single_name() remove cpu_dai_name if cpu_args
+	 * was 0. See:
+	 *	fmt_single_name()
+	 *	fmt_multiple_name()
+	 */
+	if (is_single_links)
+		dai_link->cpu_dai_name = NULL;
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(asoc_simple_card_canonicalize_cpu);
