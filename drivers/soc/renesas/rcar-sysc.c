27b11cbf7cbfa2b110f7cc728bb76754c76266e5
Dien Pham <dien.pham.ry@rvc.renesas.com>
2016-05-27 16:36:54 +0700

    soc: renesas: rcar-sysc: Add wait 1us before turning off power domain
    [dien.pham.ry: Apply wait 1us for only turning off power domain]
    
    As hardware updated info,
    some power domains require 1us before turning off,
    e.g. A3IR, A3VP, A3VC on R-Car H3; or A3IR, A3VC on M3.
    
    Because the waiting time is small. So, it is decided to apply
    commonly for all SoC of R-Car Gen3 platform.
    
    Signed-off-by: Dien Pham <dien.pham.ry@rvc.renesas.com>
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>
    Signed-off-by: Tho Vu <tho.vu.wh@renesas.com>

--- drivers/soc/renesas/rcar-sysc.c
+++ drivers/soc/renesas/rcar-sysc.c
@@ -68,6 +68,8 @@ static u32 rcar_sysc_extmask_offs, rcar_sysc_extmask_val;
 
 static const char *to_pd_name(const struct rcar_sysc_ch *sysc_ch);
 
+static const char *to_pd_name(const struct rcar_sysc_ch *sysc_ch);
+
 static int rcar_sysc_pwr_on_off(const struct rcar_sysc_ch *sysc_ch, bool on)
 {
 	unsigned int sr_bit, reg_offs;
@@ -88,6 +90,12 @@ static int rcar_sysc_pwr_on_off(const struct rcar_sysc_ch *sysc_ch, bool on)
 					SYSCSR_TIMEOUT);
 	if (ret)
 		return -EAGAIN;
+
+	/* Start W/A for A3VP, A3VC, and A3IR domains */
+	if (!on && (!strcmp("a3vp", to_pd_name(sysc_ch)) ||
+		    !strcmp("a3ir", to_pd_name(sysc_ch)) ||
+		    !strcmp("a3vc", to_pd_name(sysc_ch))))
+		udelay(1);
 
 	/* Start W/A for A3VP, A3VC, and A3IR domains */
 	if (!on && (!strcmp("a3vp", to_pd_name(sysc_ch)) ||
