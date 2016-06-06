4384f9c31d9050877b00bcd113a1b75218c84726
Magnus Damm <damm+renesas@opensource.se>
2016-06-06 12:59:07 +0900

    iommu/ipmmu-vmsa: Hook up r8a7795 DT matching code
    Tie in r8a7795 features and update the IOMMU_OF_DECLARE
    compat string to hook up the updated compat string.
    
    TODO:
     - Go over init order once more
     - Consider counting number of IPMMU devices from ->xlate()
     - Experiment with delaying call to bus_set_iommu()
    
    Signed-off-by: Magnus Damm <damm+renesas@opensource.se>
    (cherry picked from commit e604b3ce08da105bdb36252d91fc89205f27575f)
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/iommu/ipmmu-vmsa.c
+++ drivers/iommu/ipmmu-vmsa.c
@@ -1074,10 +1074,21 @@ static const struct ipmmu_features ipmmu_features_default = {
 	.twobit_imttbcr_sl0 = false,
 };
 
+static const struct ipmmu_features ipmmu_features_r8a7795 = {
+	.use_ns_alias_offset = false,
+	.has_cache_leaf_nodes = true,
+	.has_eight_ctx = true,
+	.setup_imbuscr = false,
+	.twobit_imttbcr_sl0 = true,
+};
+
 static const struct of_device_id ipmmu_of_ids[] = {
 	{
 		.compatible = "renesas,ipmmu-vmsa",
 		.data = &ipmmu_features_default,
+	}, {
+		.compatible = "renesas,ipmmu-r8a7795",
+		.data = &ipmmu_features_r8a7795,
 	}, {
 		/* Terminator */
 	},
@@ -1254,6 +1265,8 @@ static int __init ipmmu_vmsa_iommu_of_setup(struct device_node *np)
 }
 
 IOMMU_OF_DECLARE(ipmmu_vmsa_iommu_of, "renesas,ipmmu-vmsa",
+		 ipmmu_vmsa_iommu_of_setup);
+IOMMU_OF_DECLARE(ipmmu_r8a7795_iommu_of, "renesas,ipmmu-r8a7795",
 		 ipmmu_vmsa_iommu_of_setup);
 #endif
 
