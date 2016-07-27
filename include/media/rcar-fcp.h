c04fe1e20dd290bab041382c3f5fd20bca4a5422
Magnus Damm <damm+renesas@opensource.se>
2016-07-27 10:33:56 +0700

    v4l: rcar-fcp: Add helper to retrieve FCP device
    Signed-off-by: Magnus Damm <damm+renesas@opensource.se>
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- include/media/rcar-fcp.h
+++ include/media/rcar-fcp.h
@@ -20,6 +20,7 @@ struct rcar_fcp_device;
 struct rcar_fcp_device *rcar_fcp_get(const struct device_node *np);
 void rcar_fcp_put(struct rcar_fcp_device *fcp);
 int rcar_fcp_enable(struct rcar_fcp_device *fcp);
+struct device *rcar_fcp_device(struct rcar_fcp_device *fcp);
 void rcar_fcp_disable(struct rcar_fcp_device *fcp);
 #else
 static inline struct rcar_fcp_device *rcar_fcp_get(const struct device_node *np)
@@ -30,6 +31,10 @@ static inline void rcar_fcp_put(struct rcar_fcp_device *fcp) { }
 static inline int rcar_fcp_enable(struct rcar_fcp_device *fcp)
 {
 	return -ENOSYS;
+}
+static inline struct device *rcar_fcp_device(struct rcar_fcp_device *fcp)
+{
+	return NULL;
 }
 static inline void rcar_fcp_disable(struct rcar_fcp_device *fcp) { }
 #endif
