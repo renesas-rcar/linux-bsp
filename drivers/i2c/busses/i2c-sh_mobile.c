5639699142cb56fd819d56e851761cf019c377dc
Hien Dang <hien.dang.eb@renesas.com>
2016-08-17 15:18:26 +0700

    i2c: sh_mobile: Use pm_runtime_get/put when suspend
    Due to the commit '39dd0f234fc37d ("PM / Domains: Allow runtime PM during
    system PM phases")', runtime PM status might be suspended when back up
    module registers. Therefore, it is necessary to ensure device is
    on during suspend by using pm_runtime_get/put.
    
    Signed-off-by: Hien Dang <hien.dang.eb@renesas.com>

--- drivers/i2c/busses/i2c-sh_mobile.c
+++ drivers/i2c/busses/i2c-sh_mobile.c
@@ -1040,7 +1040,9 @@ static int sh_mobile_i2c_suspend(struct device *dev)
 
 	if (!i2c_dvfs_ip.virt_addr)
 		i2c_dvfs_ip.virt_addr = i2c_data->reg;
+	pm_runtime_get_sync(dev);
 	ret = rcar_handle_registers(&i2c_dvfs_ip, DO_BACKUP);
+	pm_runtime_put(dev);
 #endif /* CONFIG_RCAR_DDR_BACKUP  */
 	return ret;
 }
