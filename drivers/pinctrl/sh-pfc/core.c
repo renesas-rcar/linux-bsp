833100bdb2ccdaefdb661df65e274942e039f3fb
Hien Dang <hien.dang.eb@renesas.com>
2016-08-24 14:33:49 +0700

    pinctrl: sh-pfc: Change DDR backup function names
    This is to adapt with commit 6d351b2b0ff5 ("soc: s2ram_ddr_backup:
    Rename core functions to avoid ambiguity")
    
    Signed-off-by: Hien Dang <hien.dang.eb@renesas.com>

--- drivers/pinctrl/sh-pfc/core.c
+++ drivers/pinctrl/sh-pfc/core.c
@@ -749,11 +749,11 @@ static int sh_pfc_suspend(struct device *dev)
 	pr_debug("%s\n", __func__);
 
 	if (!pfc_ip.virt_addr)
-		ret = handle_registers(&pfc_ip, DO_IOREMAP);
+		ret = rcar_handle_registers(&pfc_ip, DO_IOREMAP);
 	if (ret)
 		return ret;
 
-	ret = handle_registers(&pfc_ip, DO_BACKUP);
+	ret = rcar_handle_registers(&pfc_ip, DO_BACKUP);
 #endif
 	return ret;
 }
@@ -764,7 +764,7 @@ static int sh_pfc_resume(struct device *dev)
 #if defined(CONFIG_PINCTRL_PFC_R8A7795) && \
 	defined(CONFIG_RCAR_DDR_BACKUP)
 	pr_debug("%s\n", __func__);
-	ret = handle_registers(&pfc_ip, DO_RESTORE);
+	ret = rcar_handle_registers(&pfc_ip, DO_RESTORE);
 
 #endif
 	return ret;
