7898f648a61141cfc087efdd9088cacf0dd520b3
Dien Pham <dien.pham.ry@renesas.com>
2016-08-25 11:23:41 +0700

    mmc: sh_mobile_sdhi: Fix build issue when disable SUSPEND
    Signed-off-by: Dien Pham <dien.pham.ry@renesas.com>

--- drivers/mmc/host/sh_mobile_sdhi.c
+++ drivers/mmc/host/sh_mobile_sdhi.c
@@ -850,10 +850,13 @@ static int sh_mobile_sdhi_remove(struct platform_device *pdev)
 }
 
 static const struct dev_pm_ops tmio_mmc_dev_pm_ops = {
-	.runtime_suspend = tmio_mmc_host_runtime_suspend,
-	.runtime_resume = tmio_mmc_host_runtime_resume,
+	SET_RUNTIME_PM_OPS(tmio_mmc_host_runtime_suspend,
+			tmio_mmc_host_runtime_resume,
+			NULL)
+#ifdef CONFIG_PM_SLEEP
 	.suspend_late = tmio_mmc_host_suspend,
 	.resume_early = tmio_mmc_host_resume,
+#endif
 };
 
 static struct platform_driver sh_mobile_sdhi_driver = {
