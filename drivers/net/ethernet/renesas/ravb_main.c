f41831b038f29525249a9477750ebe3ea68adc59
Kazuya Mizuguchi <kazuya.mizuguchi.ks@renesas.com>
2016-10-05 21:37:03 +0900

    ravb: Fix ptp does not work after suspend and resume
    This patch fixes the problem that ptp4l command does not work after
    suspend and resume.
    Add the initial setting in ravb_suspend() and ravb_resume(),
    because ptp does not work.
    
    Signed-off-by: Kazuya Mizuguchi <kazuya.mizuguchi.ks@renesas.com>

--- drivers/net/ethernet/renesas/ravb_main.c
+++ drivers/net/ethernet/renesas/ravb_main.c
@@ -2258,6 +2258,9 @@ static int __maybe_unused ravb_suspend(struct device *dev)
 	else
 		ret = ravb_close(ndev);
 
+	if (priv->chip_id != RCAR_GEN2)
+		ravb_ptp_stop(ndev);
+
 	return ret;
 }
 
@@ -2265,6 +2268,7 @@ static int __maybe_unused ravb_resume(struct device *dev)
 {
 	struct net_device *ndev = dev_get_drvdata(dev);
 	struct ravb_private *priv = netdev_priv(ndev);
+	struct platform_device *pdev = priv->pdev;
 	int ret = 0;
 
 	if (priv->wol_enabled) {
@@ -2313,6 +2317,9 @@ static int __maybe_unused ravb_resume(struct device *dev)
 
 	/* Restore descriptor base address table */
 	ravb_write(ndev, priv->desc_bat_dma, DBAT);
+
+	if (priv->chip_id != RCAR_GEN2)
+		ravb_ptp_init(ndev, pdev);
 
 	if (netif_running(ndev)) {
 		if (priv->wol_enabled) {
