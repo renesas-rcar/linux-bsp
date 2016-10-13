992de344d0eb15260ad363f406a53db194468880
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2016-10-13 14:37:32 +0900

    drm: rcar-du: Add wait until access to VSP stops
    This patch sets both the DEN and DRES bits in DSYSR to 0 to
    stop access to the VSP, and wait vblank status bit of each CRTCs.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_crtc.h
+++ drivers/gpu/drm/rcar-du/rcar_du_crtc.h
@@ -82,5 +82,6 @@ void rcar_du_crtc_resume(struct rcar_du_crtc *rcrtc);
 
 void rcar_du_crtc_route_output(struct drm_crtc *crtc,
 			       enum rcar_du_output output);
+void rcar_du_crtc_vbk_check(struct rcar_du_crtc *rcrtc);
 
 #endif /* __RCAR_DU_CRTC_H__ */
