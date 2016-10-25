dcce76ab928a0451c36aeca6e16238e9f525c0ac
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2016-10-25 00:36:13 +0000

    ASoC: rsnd: remove non DT support for DMA
    Current Renesas Sound driver is based on DeviceTree, and no one is
    using this driver from non DT. Non-DT support is no longer needed.
    Let's remove it.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit b99305d20122174c9fd0469bae036f0c401999b5)
    Signed-off-by: Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>

--- sound/soc/sh/rcar/src.c
+++ sound/soc/sh/rcar/src.c
@@ -477,7 +477,7 @@ static int rsnd_src_probe_(struct rsnd_mod *mod,
 			return ret;
 	}
 
-	ret = rsnd_dma_attach(io, mod, &src->dma, 0);
+	ret = rsnd_dma_attach(io, mod, &src->dma);
 
 	return ret;
 }
