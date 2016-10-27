89bb23e628acd39461d763997bafae1a558fadc5
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2016-10-27 17:12:14 +0200

    rcar-vin: add flag to switch to media controller mode
    On Gen3 a media controller API needs to be used to allow userspace to
    configure the subdevices in the pipeline instead of directly controlling
    a single source subdevice, which is and will continue to be the mode of
    operation on Gen2.
    
    Prepare for these two modes of operation by adding a flag to struct
    rvin_graph_entity which will control which mode to use.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>

--- drivers/media/platform/rcar-vin/rcar-core.c
+++ drivers/media/platform/rcar-vin/rcar-core.c
@@ -279,18 +279,21 @@ static int rvin_digital_graph_init(struct rvin_dev *vin)
 
 static const struct rvin_info rcar_info_h1 = {
 	.chip = RCAR_H1,
+	.use_mc = false,
 	.max_width = 2048,
 	.max_height = 2048,
 };
 
 static const struct rvin_info rcar_info_m1 = {
 	.chip = RCAR_M1,
+	.use_mc = false,
 	.max_width = 2048,
 	.max_height = 2048,
 };
 
 static const struct rvin_info rcar_info_gen2 = {
 	.chip = RCAR_GEN2,
+	.use_mc = false,
 	.max_width = 2048,
 	.max_height = 2048,
 };
