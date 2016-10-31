a474047152ea7780c7cf73ac980259e3579bf358
Hien Dang <hien.dang.eb@renesas.com>
2016-10-31 11:17:04 +0700

    usb: renesas_usbhs: Use macro to define pm_ops
    Signed-off-by: Hien Dang <hien.dang.eb@renesas.com>
    Signed-off-by: Junki Kato <junki.kato.xk@renesas.com>
    Signed-off-by: Phuong Nguyen <phuong.nguyen.xw@renesas.com>

--- drivers/usb/renesas_usbhs/common.c
+++ drivers/usb/renesas_usbhs/common.c
@@ -792,10 +792,9 @@ static int usbhsc_runtime_nop(struct device *dev)
 }
 
 static const struct dev_pm_ops usbhsc_pm_ops = {
-	.suspend		= usbhsc_suspend,
-	.resume			= usbhsc_resume,
-	.runtime_suspend	= usbhsc_runtime_nop,
-	.runtime_resume		= usbhsc_runtime_nop,
+	SET_SYSTEM_SLEEP_PM_OPS(usbhsc_suspend, usbhsc_resume)
+	SET_RUNTIME_PM_OPS(usbhsc_runtime_nop, usbhsc_runtime_nop,
+			   NULL)
 };
 
 static struct platform_driver renesas_usbhs_driver = {
