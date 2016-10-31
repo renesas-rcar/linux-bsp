2ca838881e804ef3f4dcb53a02d248638320a198
Harunobu Kurokawa <harunobu.kurokawa.dn@renesas.com>
2016-10-31 12:50:43 +0900

    Revert "PCI: rcar: Remove unused platform data"
    This reverts commit d9c64c500aecfc1623a9f21e5a563916d4730832.
    
     In S2RAM code, pcie driver is extracted from private data of device in
    rcar_pcie_suspend(). It needs to add pcie as private data for device.
    
    Reported-by: Hien Dang <hien.dang.eb@renesas.com>
    Signed-off-by: Harunobu Kurokawa <harunobu.kurokawa.dn@renesas.com>

--- drivers/pci/host/pcie-rcar.c
+++ drivers/pci/host/pcie-rcar.c
@@ -1162,6 +1162,7 @@ static int rcar_pcie_probe(struct platform_device *pdev)
 	pcie = pci_host_bridge_priv(bridge);
 
 	pcie->dev = dev;
+	platform_set_drvdata(pdev, pcie);
 
 	INIT_LIST_HEAD(&pcie->resources);
 
