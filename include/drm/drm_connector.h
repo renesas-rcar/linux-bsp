75dfe96439e461c9363ad59834939912dab48e88
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2016-11-18 16:55:48 +0200

    drm: Add data transmission order bus flag
    The flags indicate whether data is transmitted lsb to msb or msb to lsb
    on the bus.
    
    The exact meaning is bus-type dependent. For instance, for LVDS buses
    the flags indicate whether the seven data bits transmitted in a clock
    pulse are sent in normal order (msb to lsb, slots 0 to 6) or reverse
    order (lsb to msb, slots 6 to 0).
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    ---
    Changes since v2:
    
    - Rename the flag to DRM_BUS_FLAG_DATA_LSB_TO_MSB and add a
      corresponding DRM_BUS_FLAG_DATA_MSB_TO_LSB flag.

--- include/drm/drm_connector.h
+++ include/drm/drm_connector.h
@@ -160,6 +160,10 @@ struct drm_display_info {
 #define DRM_BUS_FLAG_PIXDATA_POSEDGE	(1<<2)
 /* drive data on neg. edge */
 #define DRM_BUS_FLAG_PIXDATA_NEGEDGE	(1<<3)
+/* data is transmitted msb to lsb on the bus */
+#define DRM_BUS_FLAG_DATA_MSB_TO_LSB	(1<<4)
+/* data is transmitted lsb to msb on the bus */
+#define DRM_BUS_FLAG_DATA_LSB_TO_MSB	(1<<5)
 
 	/**
 	 * @bus_flags: Additional information (like pixel signal polarity) for
