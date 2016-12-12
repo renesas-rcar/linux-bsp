61fd8df5bbbeda9c1562fa1c40e5579b4587181b
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2016-12-12 18:04:37 +0900

    drm: rcar-du: Fix DPLL workaround
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_regs.h
+++ drivers/gpu/drm/rcar-du/rcar_du_regs.h
@@ -280,7 +280,7 @@
 #define DPLLCR			0x20044
 #define DPLLCR_CODE		(0x95 << 24)
 #define DPLLCR_PLCS1		(1 << 23)
-#define DPLLCR_PLCS0		(1 << 20)
+#define DPLLCR_PLCS0		(1 << 21)
 #define DPLLCR_CLKE		(1 << 18)
 #define DPLLCR_FDPLL(n)		((n) << 12)	/* n=0 Setting prohibited */
 /* H'00 to H'26, H'78 to H'7F: Setting prohibited.*/
