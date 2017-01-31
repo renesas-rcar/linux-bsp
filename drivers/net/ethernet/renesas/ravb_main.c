dcf1a6011a40cc7f7ee303fecbbb0b9e19971bf3
Tsutomu Izawa <tsutomu.izawa.xk@renesas.com>
2017-01-31 15:58:37 +0900

    ravb: Fix ravb_ptp_interrupt clear interrupt all status
    This patch fixes ravb_ptp_interrupt clears GIS register of all interrupts
    status. It corrects to clear PTCF bit or PTMF bit.
    Also it fixes returned value to IRQ_HANDLED or IRQ_NONE.
    
    Signed-off-by: Tsutomu Izawa <tsutomu.izawa.xk@renesas.com>
    Signed-off-by: Kazuya Mizuguchi <kazuya.mizuguchi.ks@renesas.com>
    Signed-off-by: Tho Vu <tho.vu.wh@renesas.com>

--- drivers/net/ethernet/renesas/ravb_main.c
+++ drivers/net/ethernet/renesas/ravb_main.c
@@ -1218,10 +1218,8 @@ static irqreturn_t ravb_interrupt(int irq, void *dev_id)
 	}
 
 	/* gPTP interrupt status summary */
-	if (iss & ISS_CGIS) {
-		ravb_ptp_interrupt(ndev);
-		result = IRQ_HANDLED;
-	}
+	if (iss & ISS_CGIS)
+		result = ravb_ptp_interrupt(ndev);
 
 	spin_unlock(&priv->lock);
 	return result;
@@ -1250,10 +1248,8 @@ static irqreturn_t ravb_multi_interrupt(int irq, void *dev_id)
 	}
 
 	/* gPTP interrupt status summary */
-	if (iss & ISS_CGIS) {
-		ravb_ptp_interrupt(ndev);
-		result = IRQ_HANDLED;
-	}
+	if (iss & ISS_CGIS)
+		result = ravb_ptp_interrupt(ndev);
 
 	spin_unlock(&priv->lock);
 	return result;
