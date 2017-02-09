0babbe17cd5eba9224e367d6b9ef446379535ece
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2017-02-09 10:08:10 +0900

    v4l: vsp1: Fix WPF interrupt enable
    Fix merge miss of "v4l: vsp1: Add underrun hung-up workaround"
    (commit ID: be7c07e).
    Modify WPF interrupt enable from FREE to DFEE with this patch.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/media/platform/vsp1/vsp1_wpf.c
+++ drivers/media/platform/vsp1/vsp1_wpf.c
@@ -408,7 +408,7 @@ static void wpf_configure(struct vsp1_entity *entity,
 	/* Enable interrupts */
 	vsp1_dl_list_write(dl, VI6_WPF_IRQ_STA(wpf->entity.index), 0);
 	vsp1_dl_list_write(dl, VI6_WPF_IRQ_ENB(wpf->entity.index),
-			   VI6_WFP_IRQ_ENB_FREE | VI6_WFP_IRQ_ENB_UNDE);
+			   VI6_WFP_IRQ_ENB_DFEE | VI6_WFP_IRQ_ENB_UNDE);
 }
 
 static const struct vsp1_entity_operations wpf_entity_ops = {
