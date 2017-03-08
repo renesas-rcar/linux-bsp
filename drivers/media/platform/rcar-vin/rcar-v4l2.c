4eebfc672dc293532a66a98de986b63e542e9aa0
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2017-03-08 13:36:07 +0100

    rcar-vin: move media bus configuration to struct rvin_info
    Bus configuration will once the driver is extended to to support Gen3
    contain information not specific to only the directly connected parallel
    subdevice. Move it to struct rvin_info to show it's not always coupled
    to the parallel subdevice.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>

--- drivers/media/platform/rcar-vin/rcar-v4l2.c
+++ drivers/media/platform/rcar-vin/rcar-v4l2.c
@@ -180,7 +180,7 @@ static int __rvin_try_format_source(struct rvin_dev *vin,
 
 	sd = vin_to_source(vin);
 
-	v4l2_fill_mbus_format(&format.format, pix, vin->digital.code);
+	v4l2_fill_mbus_format(&format.format, pix, vin->code);
 
 	pad_cfg = v4l2_subdev_alloc_pad_config(sd);
 	if (pad_cfg == NULL)
