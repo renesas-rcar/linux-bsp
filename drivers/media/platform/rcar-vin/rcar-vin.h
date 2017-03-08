96a76fd6202063f24c0563edf6b2a2d65901aa80
Niklas Soderlund <niklas.soderlund+renesas@ragnatech.se>
2017-03-08 14:22:33 +0100

    media: rcar-vin: do not allow changing scaling and composing while streaming
    It is possible on Gen2 to change the registers controlling composing and
    scaling while the stream is running. It is however not a good idea to do
    so and could result in trouble. There are also no good reasons to allow
    this, remove immediate reflection in hardware registers from
    vidioc_s_selection and only configure scaling and composing when the
    stream starts.
    
    Signed-off-by: Niklas Soderlund <niklas.soderlund+renesas@ragnatech.se>
    Reviewed-by: Hans Verkuil <hans.verkuil@cisco.com>
    (cherry picked from commit 1f08884d31dd1aa8eaf3b1cdf79fd6ba93e81d14)
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/media/platform/rcar-vin/rcar-vin.h
+++ drivers/media/platform/rcar-vin/rcar-vin.h
@@ -282,9 +282,6 @@ const struct rvin_video_format *rvin_format_from_pixel(struct rvin_dev *vin,
 						       u32 pixelformat);
 
 
-/* Cropping, composing and scaling */
-void rvin_crop_scale_comp(struct rvin_dev *vin);
-
 int rvin_set_channel_routing(struct rvin_dev *vin, u8 chsel);
 void rvin_set_alpha(struct rvin_dev *vin, unsigned int alpha);
 
