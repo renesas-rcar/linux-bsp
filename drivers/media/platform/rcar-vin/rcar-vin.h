ab66e0268f5f389d0300979fd47ab6764c347cd7
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2017-03-09 15:34:26 +0100

    rcar-vin: use different v4l2 operations in media controller mode
    When the driver runs in media controller mode it should not directly
    control the subdevice instead userspace will be responsible for
    configuring the pipeline. To be able to run in this mode a different set
    of v4l2 operations needs to be used.
    
    Add a new set of v4l2 operations to support the running without directly
    interacting with the source subdevice.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Reviewed-by: Hans Verkuil <hans.verkuil@cisco.com>
    (cherry picked from commit 84a7e1abb4b9d662b8ddf9430e9eee7088667490)
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/media/platform/rcar-vin/rcar-vin.h
+++ drivers/media/platform/rcar-vin/rcar-vin.h
@@ -21,6 +21,7 @@
 #include <media/v4l2-ctrls.h>
 #include <media/v4l2-dev.h>
 #include <media/v4l2-device.h>
+#include <media/v4l2-mc.h>
 #include <media/videobuf2-v4l2.h>
 
 /* Number of HW buffers */
