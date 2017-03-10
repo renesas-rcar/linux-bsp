0f51b49f589260929c86037366c95309422a6031
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2017-03-10 20:37:03 +0100

    rcar-vin: fix handling of single field frames (top, bottom and alternate fields)
    There was never proper support in the VIN driver to deliver ALTERNATING
    field format to user-space, remove this field option. For sources using
    this field format instead use the VIN hardware feature of combining the
    fields to an interlaced format. This mode of operation was previously
    the default behavior and ALTERNATING was only delivered to user-space if
    explicitly requested. Allowing this to be explicitly requested was a
    mistake and was never properly tested and never worked due to the
    constraints put on the field format when it comes to sequence numbers and
    timestamps etc.
    
    The height should not be cut in half for the format for TOP or BOTTOM
    fields settings. This was a mistake and it was made visible by the
    scaling refactoring. Correct behavior is that the user should request a
    frame size that fits the half height frame reflected in the field
    setting. If not the VIN will do its best to scale the top or bottom to
    the requested format and cropping and scaling do not work as expected.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Reviewed-by: Hans Verkuil <hans.verkuil@cisco.com>
    (cherry picked from commit 56809d80d852ffb16cf253801b751e384ea62107)
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/media/platform/rcar-vin/rcar-dma.c
+++ drivers/media/platform/rcar-vin/rcar-dma.c
@@ -617,7 +617,6 @@ static int rvin_setup(struct rvin_dev *vin)
 	case V4L2_FIELD_INTERLACED_BT:
 		vnmc = VNMC_IM_FULL | VNMC_FOC;
 		break;
-	case V4L2_FIELD_ALTERNATE:
 	case V4L2_FIELD_NONE:
 		if (vin->continuous) {
 			vnmc = VNMC_IM_ODD_EVEN;
@@ -755,18 +754,6 @@ static int rvin_get_active_slot(struct rvin_dev *vin, u32 vnms)
 		return (vnms & VNMS_FBS_MASK) >> VNMS_FBS_SHIFT;
 
 	return 0;
-}
-
-static enum v4l2_field rvin_get_active_field(struct rvin_dev *vin, u32 vnms)
-{
-	if (vin->format.field == V4L2_FIELD_ALTERNATE) {
-		/* If FS is set it's a Even field */
-		if (vnms & VNMS_FS)
-			return V4L2_FIELD_BOTTOM;
-		return V4L2_FIELD_TOP;
-	}
-
-	return vin->format.field;
 }
 
 static void rvin_set_slot_addr(struct rvin_dev *vin, int slot, dma_addr_t addr)
@@ -941,7 +928,7 @@ static irqreturn_t rvin_irq(int irq, void *data)
 		goto done;
 
 	/* Capture frame */
-	vin->queue_buf[slot]->field = rvin_get_active_field(vin, vnms);
+	vin->queue_buf[slot]->field = vin->format.field;
 	vin->queue_buf[slot]->sequence = sequence;
 	vin->queue_buf[slot]->vb2_buf.timestamp = ktime_get_ns();
 	vb2_buffer_done(&vin->queue_buf[slot]->vb2_buf, VB2_BUF_STATE_DONE);
