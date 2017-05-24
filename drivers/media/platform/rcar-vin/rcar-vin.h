83976f8a3f17dadba38a82d6f65484c1b33fbcdd
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2017-05-24 02:15:38 +0200

    rcar-vin: register the video device at probe time
    The driver registers the video device from the async complete callback
    and unregistered in the async unbind callback. This creates problems if
    if the subdevice is bound, unbound and later rebound. The second time
    video_register_device() is called it fails:
    
       kobject (eb3be918): tried to init an initialized object, something is seriously wrong.
    
    To prevent this register the video device at probe time and don't allow
    user-space to open the video device if the subdevice is not bound yet.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    (cherry picked from commit 9c1dfab1b662f5ebc082310bc71e7f74e624cc50)
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/media/platform/rcar-vin/rcar-vin.h
+++ drivers/media/platform/rcar-vin/rcar-vin.h
@@ -156,8 +156,9 @@ struct rvin_dev {
 int rvin_dma_probe(struct rvin_dev *vin, int irq);
 void rvin_dma_remove(struct rvin_dev *vin);
 
-int rvin_v4l2_probe(struct rvin_dev *vin);
-void rvin_v4l2_remove(struct rvin_dev *vin);
+int rvin_v4l2_register(struct rvin_dev *vin);
+void rvin_v4l2_unregister(struct rvin_dev *vin);
+int rvin_reset_format(struct rvin_dev *vin);
 
 const struct rvin_video_format *rvin_format_from_pixel(u32 pixelformat);
 
