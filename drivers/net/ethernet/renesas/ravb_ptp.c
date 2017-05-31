a08bd425067975a09095fea294fd27bf7ed8fa5f
Masaru Nagai <masaru.nagai.vx@renesas.com>
2017-05-31 13:20:34 +0900

    ravb: Fix interrupt cause bit overwrite in ravb_ptp_interrupt
    Fix interrupt cause bit overwrite in ravb_ptp_interrupt by
    clearing GIS.PTCF and GIS.PTMF
    
    Signed-off-by: Masaru Nagai <masaru.nagai.vx@renesas.com>
    Signed-off-by: Kazuya Mizuguchi <kazuya.mizuguchi.ks@renesas.com>
    Signed-off-by: Tho Vu <tho.vu.wh@renesas.com>

--- drivers/net/ethernet/renesas/ravb_ptp.c
+++ drivers/net/ethernet/renesas/ravb_ptp.c
@@ -317,7 +317,7 @@ irqreturn_t ravb_ptp_interrupt(struct net_device *ndev)
 		ptp_clock_event(priv->ptp.clock, &event);
 
 		result = IRQ_HANDLED;
-		gis &= ~GIS_PTCF;
+		ravb_write(ndev, ~GIS_PTCF, GIS);
 	}
 	if (gis & GIS_PTMF) {
 		struct ravb_ptp_perout *perout = priv->ptp.perout;
@@ -328,10 +328,8 @@ irqreturn_t ravb_ptp_interrupt(struct net_device *ndev)
 		}
 
 		result = IRQ_HANDLED;
-		gis &= ~GIS_PTMF;
+		ravb_write(ndev, ~GIS_PTMF, GIS);
 	}
-
-	ravb_write(ndev, (gis & ~GIS_RESERVED), GIS);
 
 	return result;
 }
