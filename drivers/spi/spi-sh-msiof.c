db10c387b8dd929ca50428261b750a5550f0ef32
Geert Uytterhoeven <geert+renesas@glider.be>
2017-07-12 12:26:01 +0200

    spi: sh-msiof: Limit minimum divider on R-Car Gen3
    On R-Car Gen3 SoCs (excluding R-Car H3 ES1.x, which cannot be used for
    SPI due to a hardware erratum), BRPS x BRDV = 1/1 is an invalid divider
    setting.
    Implement this limitation using an SoC/family-specific minimum divider.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 61a8dec502b873879fa240ec7614601523b46a43)
    Signed-off-by: Hiromitsu Yamasaki <hiromitsu.yamasaki.ym@renesas.com>
    
    Conflicts:
    	drivers/spi/spi-sh-msiof.c

--- drivers/spi/spi-sh-msiof.c
+++ drivers/spi/spi-sh-msiof.c
@@ -40,6 +40,7 @@ struct sh_msiof_chipdata {
 	u16 tx_fifo_size;
 	u16 rx_fifo_size;
 	u16 master_flags;
+	u16 min_div;
 };
 
 #ifdef CONFIG_SPI_SH_MSIOF_TRANSFER_SYNC_DEBUG
@@ -56,6 +57,7 @@ struct sh_msiof_spi_priv {
 	struct completion done_dma_tx, done_dma_rx;
 	unsigned int tx_fifo_size;
 	unsigned int rx_fifo_size;
+	unsigned int min_div;
 	int mode;
 	int cs;
 	void *tx_dma_page;
@@ -198,15 +200,12 @@ struct sh_msiof_spi_priv {
 /* Check LSI revisions and set specific quirk value */
 #define TRANSFER_WORKAROUND_H3WS10  BIT(0) /* H3ES1.0 workaround */
 #define TRANSFER_WORKAROUND_H3WS11  BIT(1) /* H3ES1.1 workaround */
-#define UNDIVIDED_PROHIBIT          BIT(2) /* Prohibition of 1/1 division */
 
 static const struct soc_device_attribute rcar_quirks_match[]  = {
 	{ .soc_id = "r8a7795", .revision = "ES1.0",
 		.data = (void *)TRANSFER_WORKAROUND_H3WS10, },
 	{ .soc_id = "r8a7795", .revision = "ES1.1",
 		.data = (void *)TRANSFER_WORKAROUND_H3WS11, },
-	{ .soc_id = "r8a7796*",
-		.data = (void *)UNDIVIDED_PROHIBIT, },
 	{/*sentinel*/},
 };
 
@@ -318,14 +317,12 @@ static void sh_msiof_spi_set_clk_regs(struct sh_msiof_spi_priv *p,
 	if (!WARN_ON(!spi_hz || !parent_rate))
 		div = DIV_ROUND_UP(parent_rate, spi_hz);
 
+	div = max_t(unsigned long, div, p->min_div);
+
 	for (k = 0; k < ARRAY_SIZE(sh_msiof_spi_div_table); k++) {
 		brps = DIV_ROUND_UP(div, sh_msiof_spi_div_table[k].div);
 		/* SCR_BRDV_DIV_1 is valid only if BRPS is x 1/1 or x 1/2 */
 		if (sh_msiof_spi_div_table[k].div == 1 && brps > 2)
-			continue;
-		/* r8a7796 is invalid only when BRPS x BRDV = 1/1 */
-		if ((p->quirks & UNDIVIDED_PROHIBIT) &&
-			sh_msiof_spi_div_table[k].div == 1 && brps == 1)
 			continue;
 		if (brps <= 32) /* max of brdv is 32 */
 			break;
@@ -1171,25 +1168,37 @@ static const struct sh_msiof_chipdata sh_data = {
 	.tx_fifo_size = 64,
 	.rx_fifo_size = 64,
 	.master_flags = 0,
+	.min_div = 1,
 };
 
-static const struct sh_msiof_chipdata r8a779x_data = {
+static const struct sh_msiof_chipdata rcar_gen2_data = {
 	.tx_fifo_size = 64,
 	.rx_fifo_size = 64,
 	.master_flags = SPI_MASTER_MUST_TX,
+	.min_div = 1,
+};
+
+static const struct sh_msiof_chipdata rcar_gen3_data = {
+	.tx_fifo_size = 64,
+	.rx_fifo_size = 64,
+	.master_flags = SPI_MASTER_MUST_TX,
+	.min_div = 2,
 };
 
 static const struct of_device_id sh_msiof_match[] = {
 	{ .compatible = "renesas,sh-msiof",        .data = &sh_data },
 	{ .compatible = "renesas,sh-mobile-msiof", .data = &sh_data },
-	{ .compatible = "renesas,msiof-r8a7790",   .data = &r8a779x_data },
-	{ .compatible = "renesas,msiof-r8a7791",   .data = &r8a779x_data },
-	{ .compatible = "renesas,msiof-r8a7792",   .data = &r8a779x_data },
-	{ .compatible = "renesas,msiof-r8a7793",   .data = &r8a779x_data },
-	{ .compatible = "renesas,msiof-r8a7794",   .data = &r8a779x_data },
-	{ .compatible = "renesas,msiof-r8a7795",   .data = &r8a779x_data },
-	{ .compatible = "renesas,msiof-r8a7796",   .data = &r8a779x_data },
-	{ .compatible = "renesas,msiof-r8a77965",  .data = &r8a779x_data },
+	{ .compatible = "renesas,msiof-r8a7790",   .data = &rcar_gen2_data },
+	{ .compatible = "renesas,msiof-r8a7791",   .data = &rcar_gen2_data },
+	{ .compatible = "renesas,msiof-r8a7792",   .data = &rcar_gen2_data },
+	{ .compatible = "renesas,msiof-r8a7793",   .data = &rcar_gen2_data },
+	{ .compatible = "renesas,msiof-r8a7794",   .data = &rcar_gen2_data },
+	{ .compatible = "renesas,rcar-gen2-msiof", .data = &rcar_gen2_data },
+	{ .compatible = "renesas,msiof-r8a7795",   .data = &rcar_gen3_data },
+	{ .compatible = "renesas,msiof-r8a7796",   .data = &rcar_gen3_data },
+	{ .compatible = "renesas,msiof-r8a77965",  .data = &rcar_gen3_data },
+	{ .compatible = "renesas,rcar-gen3-msiof", .data = &rcar_gen3_data },
+	{ .compatible = "renesas,sh-msiof",        .data = &sh_data }, /* Deprecated */
 	{},
 };
 MODULE_DEVICE_TABLE(of, sh_msiof_match);
@@ -1401,6 +1410,7 @@ static int sh_msiof_spi_probe(struct platform_device *pdev)
 		goto err1;
 	}
 
+	p->min_div = chipdata->min_div;
 	attr = soc_device_match(rcar_quirks_match);
 	if (attr)
 		p->quirks = (uintptr_t)attr->data;
