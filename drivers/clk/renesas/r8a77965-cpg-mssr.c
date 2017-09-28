a375fd02687a517d8989fb533e5c09ee3666fed6
Takeshi Kihara <takeshi.kihara.df@renesas.com>
2017-09-28 14:47:11 +0900

    clk: renesas: r8a77965: Add RAVB clock
    This patch adds RAVB clock to the R8A77965 SoC.
    
    Based on a similar patch of the R8A7796 CPG/MSSR driver
    by Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>.
    
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/clk/renesas/r8a77965-cpg-mssr.c
+++ drivers/clk/renesas/r8a77965-cpg-mssr.c
@@ -103,6 +103,7 @@ static const struct mssr_mod_clk r8a77965_mod_clks[] __initconst = {
 	DEF_MOD("scif0",		 207,	R8A77965_CLK_S3D4),
 	DEF_MOD("scif2",		 310,	R8A77965_CLK_S3D4),
 	DEF_MOD("intc-ap",		 408,	R8A77965_CLK_S3D1),
+	DEF_MOD("etheravb",		 812,	R8A77965_CLK_S0D6),
 	DEF_MOD("gpio7",		 905,	R8A77965_CLK_S3D4),
 	DEF_MOD("gpio6",		 906,	R8A77965_CLK_S3D4),
 	DEF_MOD("gpio5",		 907,	R8A77965_CLK_S3D4),
