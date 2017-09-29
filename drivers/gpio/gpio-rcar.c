f31b829f8ffe11e660cbc47e5c9e598827cc246f
Takeshi Kihara <takeshi.kihara.df@renesas.com>
2017-09-29 17:27:25 +0900

    gpio: rcar: Add R8A77965 support
    This patch adds support for the Renesas R-Car M3N (R8A77965) SoC.
    R-Car Gen3's GPIO blocks are identical to Gen2's in every respect.
    
    Based on work for the R8A7796 by Simon Horman
    <horms+renesas@verge.net.au>.
    
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/gpio/gpio-rcar.c
+++ drivers/gpio/gpio-rcar.c
@@ -371,6 +371,10 @@ static const struct of_device_id gpio_rcar_of_table[] = {
 		/* Gen3 GPIO is identical to Gen2. */
 		.data = &gpio_rcar_info_gen2,
 	}, {
+		.compatible = "renesas,gpio-r8a77965",
+		/* Gen3 GPIO is identical to Gen2. */
+		.data = &gpio_rcar_info_gen2,
+	}, {
 		.compatible = "renesas,rcar-gen1-gpio",
 		.data = &gpio_rcar_info_gen1,
 	}, {
