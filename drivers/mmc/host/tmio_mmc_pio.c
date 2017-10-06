6317dda1472de368e84a974f087166c1eb627e88
Masaharu Hayakawa <masaharu.hayakawa.ry@renesas.com>
2017-10-06 15:40:54 +0900

    mmc: tmio: Add identification of TAP and clock settings of HS400
    The number of TAP and clock setting in HS400 mode is different
    on the device. Since it is necessary to identify 4TAP or 8TAP
    for each device, use soc_device_match().
    
    Signed-off-by: Masaharu Hayakawa <masaharu.hayakawa.ry@renesas.com>

--- drivers/mmc/host/tmio_mmc_pio.c
+++ drivers/mmc/host/tmio_mmc_pio.c
@@ -269,11 +269,11 @@ static void tmio_mmc_set_clock(struct tmio_mmc_host *host,
 		return;
 	}
 	/*
-	 * Both HS400 and HS200/SD104 set 200MHz, but HS400 sets 400MHz
-	 * to distinguish the CPG settings.
+	 * Both HS400 and HS200/SD104 set 200MHz, but some devices need to
+	 * set 400MHz to distinguish the CPG settings in HS400.
 	 */
 	if (host->mmc->ios.timing == MMC_TIMING_MMC_HS400 &&
-			new_clock == 200000000)
+	    !host->hs400_use_8tap && new_clock == 200000000)
 		new_clock = 400000000;
 
 	if (host->clk_update)
