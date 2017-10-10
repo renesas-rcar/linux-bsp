40c88486b48d42cf8150d7543204334c9896220a
Dien Pham <dien.pham.ry@renesas.com>
2017-10-10 15:47:13 +0700

    soc: renesas: Adjust SYSC PM Domain that supports r8a77965
    Signed-off-by: Dien Pham <dien.pham.ry@renesas.com>

--- include/dt-bindings/power/r8a77965-sysc.h
+++ include/dt-bindings/power/r8a77965-sysc.h
@@ -16,14 +16,13 @@
 
 #define R8A77965_PD_CA57_CPU0		 0
 #define R8A77965_PD_CA57_CPU1		 1
+#define R8A77965_PD_A3VP		 9
 #define R8A77965_PD_CA57_SCU		12
 #define R8A77965_PD_CR7			13
 #define R8A77965_PD_A3VC		14
 #define R8A77965_PD_3DG_A		17
 #define R8A77965_PD_3DG_B		18
-#define R8A77965_PD_CA53_SCU		21
 #define R8A77965_PD_A3IR		24
-#define R8A77965_PD_A2VC0		25
 #define R8A77965_PD_A2VC1		26
 
 /* Always-on power area */
