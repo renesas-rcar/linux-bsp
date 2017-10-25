ee3ea691dca09b8433b7402aa9878b91675eba7d
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2017-10-25 21:25:37 +0900

    drm: rcar-du: Fix display max size to 4096x2160 size
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_kms.c
+++ drivers/gpu/drm/rcar-du/rcar_du_kms.c
@@ -529,8 +529,8 @@ int rcar_du_modeset_init(struct rcar_du_device *rcdu)
 
 	dev->mode_config.min_width = 0;
 	dev->mode_config.min_height = 0;
-	dev->mode_config.max_width = 4095;
-	dev->mode_config.max_height = 2047;
+	dev->mode_config.max_width = 4096;
+	dev->mode_config.max_height = 2160;
 	dev->mode_config.funcs = &rcar_du_mode_config_funcs;
 	dev->mode_config.helper_private = &rcar_du_mode_config_helper;
 
