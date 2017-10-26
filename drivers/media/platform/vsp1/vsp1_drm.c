fd8fdbcb11f93b58786a819419b0517790af1d2e
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2017-10-26 16:44:51 +0900

    v4l: vsp1: Fix YCbCr planar formats format
    Fix mistake of pitch calculation about YCbCr planar formats.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/media/platform/vsp1/vsp1_drm.c
+++ drivers/media/platform/vsp1/vsp1_drm.c
@@ -1,7 +1,7 @@
 /*
  * vsp1_drm.c  --  R-Car VSP1 DRM API
  *
- * Copyright (C) 2015 Renesas Electronics Corporation
+ * Copyright (C) 2015-2017 Renesas Electronics Corporation
  *
  * Contact: Laurent Pinchart (laurent.pinchart@ideasonboard.com)
  *
@@ -349,7 +349,13 @@ int vsp1_du_atomic_update(struct device *dev, unsigned int pipe_index,
 	rpf->fmtinfo = fmtinfo;
 	rpf->format.num_planes = fmtinfo->planes;
 	rpf->format.plane_fmt[0].bytesperline = cfg->pitch;
-	rpf->format.plane_fmt[1].bytesperline = cfg->pitch;
+	if (rpf->fmtinfo->fourcc == V4L2_PIX_FMT_YUV420M ||
+	    rpf->fmtinfo->fourcc == V4L2_PIX_FMT_YVU420M ||
+	    rpf->fmtinfo->fourcc == V4L2_PIX_FMT_YUV422M ||
+	    rpf->fmtinfo->fourcc == V4L2_PIX_FMT_YVU422M)
+		rpf->format.plane_fmt[1].bytesperline = cfg->pitch / 2;
+	else
+		rpf->format.plane_fmt[1].bytesperline = cfg->pitch;
 	rpf->alpha = cfg->alpha;
 
 	rpf->mem.addr[0] = cfg->mem[0];
