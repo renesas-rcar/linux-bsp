b52fec88bbb350e888c428e843f9d972f9a54445
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2017-11-02 15:39:16 +0900

    media: i2c: adv748x: Fix get_format function
    When calling the VIDIOC_SUBDEV_S_FMT ioctl, the resolution value
    may be not reflected.
    This patch adds resolution information query procedure in set_fmt
    callback function for corresponding its problem.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>
    (cherry picked from commit f708c5e232ea1bfea2ea427508bcd47af38d0049)
    Signed-off-by: Linh Phung <linh.phung.jy@renesas.com>

--- drivers/media/i2c/adv748x/adv748x-hdmi.c
+++ drivers/media/i2c/adv748x/adv748x-hdmi.c
@@ -435,6 +435,11 @@ static int adv748x_hdmi_get_format(struct v4l2_subdev *sd,
 							sdformat->pad);
 		sdformat->format = *mbusformat;
 	} else {
+		struct v4l2_dv_timings timings;
+
+		adv748x_hdmi_query_dv_timings(&hdmi->sd, &timings);
+		hdmi->timings = timings;
+
 		adv748x_hdmi_fill_format(hdmi, &sdformat->format);
 		adv748x_hdmi_propagate_pixelrate(hdmi);
 	}
