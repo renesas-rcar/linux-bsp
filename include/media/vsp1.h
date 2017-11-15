0771402aecda70da900d40823bad5fc34d1a9924
Alexandru Gheorghe <Alexandru_Gheorghe@mentor.com>
2017-11-15 10:12:57 +0900

    v4l: vsp1: Add support for colorkey alpha blending
    The vsp2 hw supports changing of the alpha of pixels that match a color
    key, this patch adds support for this feature in order to be used by
    the rcar-du driver.
    The colorkey is interpreted different depending of the pixel format:
    	* RGB   - all color components have to match.
    	* YCbCr - only the Y component has to match.
    
    Reported-by: Alexandru Gheorghe <Alexandru_Gheorghe@mentor.com>
    Signed-off-by: Alexandru Gheorghe <Alexandru_Gheorghe@mentor.com>
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>
    Signed-off-by: Ni Nguyen <ni.nguyen.cj@bp.renesas.com>

--- include/media/vsp1.h
+++ include/media/vsp1.h
@@ -60,6 +60,9 @@ int vsp1_du_setup_lif(struct device *dev, unsigned int pipe_index,
  * @alpha: alpha value (0: fully transparent, 255: fully opaque)
  * @zpos: Z position of the plane (from 0 to number of planes minus 1)
  * @premult: true for premultiplied alpha
+ * @colorkey: colorkey value
+ * @colorkey_alpha: colorkey alpha value
+ * @colorkey_en: colorkey enable value
  */
 struct vsp1_du_atomic_config {
 	u32 pixelformat;
@@ -70,6 +73,9 @@ struct vsp1_du_atomic_config {
 	unsigned int alpha;
 	unsigned int zpos;
 	bool premult;
+	u32 colorkey;
+	u32 colorkey_alpha;
+	bool colorkey_en;
 };
 
 /**
