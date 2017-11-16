b13c54e1870ec9394c76ab05cacd8d0883d4446b
Masaharu Hayakawa <masaharu.hayakawa.ry@renesas.com>
2017-11-16 13:02:41 +0900

    mmc: renesas_sdhi: Set the SCC register address immediately
    By setting the address of the SCC register immediately
    after starting the kernel, the processing that was returning
    at the beginning of reset_hs400_mode is unnecessary.
    As a result, reset_hs400_mode is processed even when the
    kernel is started in HS400 mode.
    
    Signed-off-by: Masaharu Hayakawa <masaharu.hayakawa.ry@renesas.com>

--- drivers/mmc/host/renesas_sdhi_core.c
+++ drivers/mmc/host/renesas_sdhi_core.c
@@ -72,7 +72,7 @@ struct renesas_sdhi {
 	struct tmio_mmc_dma dma_priv;
 	struct pinctrl *pinctrl;
 	struct pinctrl_state *pins_default, *pins_uhs;
-	void __iomem *scc_ctl;
+	int scc_offset;
 };
 
 static void renesas_sdhi_sdbuf_width(struct tmio_mmc_host *host, int width)
@@ -260,14 +260,14 @@ static int renesas_sdhi_start_signal_voltage_switch(struct mmc_host *mmc,
 static inline u32 sd_scc_read32(struct tmio_mmc_host *host,
 				struct renesas_sdhi *priv, int addr)
 {
-	return readl(priv->scc_ctl + (addr << host->bus_shift));
+	return readl(host->ctl + priv->scc_offset + (addr << host->bus_shift));
 }
 
 static inline void sd_scc_write32(struct tmio_mmc_host *host,
 				  struct renesas_sdhi *priv,
 				  int addr, u32 val)
 {
-	writel(val, priv->scc_ctl + (addr << host->bus_shift));
+	writel(val, host->ctl + priv->scc_offset + (addr << host->bus_shift));
 }
 
 static unsigned int renesas_sdhi_init_tuning(struct tmio_mmc_host *host)
@@ -394,9 +394,6 @@ static void renesas_sdhi_reset_hs400_mode(struct mmc_host *mmc)
 {
 	struct tmio_mmc_host *host = mmc_priv(mmc);
 	struct renesas_sdhi *priv = host_to_priv(host);
-
-	if (!priv->scc_ctl)
-		return;
 
 	sd_ctrl_write16(host, CTL_SD_CARD_CLK_CTL, ~CLK_CTL_SCLKEN &
 			sd_ctrl_read16(host, CTL_SD_CARD_CLK_CTL));
@@ -727,6 +724,7 @@ int renesas_sdhi_probe(struct platform_device *pdev,
 		mmc_data->max_segs = of_data->max_segs;
 		dma_priv->dma_buswidth = of_data->dma_buswidth;
 		host->bus_shift = of_data->bus_shift;
+		priv->scc_offset = of_data->scc_offset;
 	}
 
 	host->dma		= dma_priv;
@@ -804,7 +802,6 @@ int renesas_sdhi_probe(struct platform_device *pdev,
 		if (!hit)
 			dev_warn(&host->pdev->dev, "Unknown clock rate for SDR104\n");
 
-		priv->scc_ctl = host->ctl + of_data->scc_offset;
 		host->init_tuning = renesas_sdhi_init_tuning;
 		host->prepare_tuning = renesas_sdhi_prepare_tuning;
 		host->compare_scc_data	= renesas_sdhi_compare_scc_data;
