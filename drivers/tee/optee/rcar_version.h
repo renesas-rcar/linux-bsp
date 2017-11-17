3cbb8e50e2057c2c2a642ae4d1fc7cce311b8fca
Tsukasa Kawaguchi <tsukasa.kawaguchi.aw@hitachi.com>
2017-11-17 16:10:40 +0900

    tee: optee: add r-car original function
    Add debug log output function and Suspend To RAM suppression function
    as r-car original processing.
    At the time of optee initialization processing,
    register the Suspendcallback function
    and address convert the base address of the optee log area.
    At the optee end processing, delete the Suspend callback function.
    Suspend is suppressed when smc request is being sent to optee upon
    Suspend transition.
    When receiving the rpc command to output the debug log,
    dynamically allocate memory, schedule delay processing,
    and output the log with pr_alert.
    
    Signed-off-by: Tsukasa Kawaguchi <tsukasa.kawaguchi.aw@hitachi.com>
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

---
+++ drivers/tee/optee/rcar_version.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2015-2017, Renesas Electronics Corporation
+ * All rights reserved.
+ */
+
+#ifndef RCAR_VERSION_H
+#define RCAR_VERSION_H
+
+#define VERSION_OF_RENESAS	"1.0.8"
+
+#endif /* RCAR_VERSION_H */
