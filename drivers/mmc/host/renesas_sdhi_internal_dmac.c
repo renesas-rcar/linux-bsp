efa7723335c031285994732afe7efecbd6a145a2
Masaharu Hayakawa <masaharu.hayakawa.ry@renesas.com>
2017-11-17 14:17:36 +0900

    mmc: renesas_sdhi: Add r8a77965 support
    This patch adds r8a77965 support in SDHI.
    
    Signed-off-by: Masaharu Hayakawa <masaharu.hayakawa.ry@renesas.com>

--- drivers/mmc/host/renesas_sdhi_internal_dmac.c
+++ drivers/mmc/host/renesas_sdhi_internal_dmac.c
@@ -101,6 +101,7 @@ static const struct renesas_sdhi_of_data of_rcar_gen3_compatible = {
 static const struct of_device_id renesas_sdhi_internal_dmac_of_match[] = {
 	{ .compatible = "renesas,sdhi-r8a7795", .data = &of_rcar_gen3_compatible, },
 	{ .compatible = "renesas,sdhi-r8a7796", .data = &of_rcar_gen3_compatible, },
+	{ .compatible = "renesas,sdhi-r8a77965", .data = &of_rcar_gen3_compatible, },
 	{},
 };
 MODULE_DEVICE_TABLE(of, renesas_sdhi_internal_dmac_of_match);
@@ -313,6 +314,7 @@ static const struct soc_device_attribute gen3_soc_whitelist[] = {
 	/* generic ones */
 	{ .soc_id = "r8a7795" },
 	{ .soc_id = "r8a7796" },
+	{ .soc_id = "r8a77965" },
 	{ /* sentinel */ }
 };
 
