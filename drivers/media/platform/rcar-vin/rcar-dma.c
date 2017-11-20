2bb98b27ecff2dc7261fe52851577848c3b6c2b6
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2017-11-20 10:04:52 +0900

    rcar-vin: Fix IFMD register
    Bit 27 in IFMD register was reserved bit by H/W manual update,
    so this patch removes it.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/media/platform/rcar-vin/rcar-dma.c
+++ drivers/media/platform/rcar-vin/rcar-dma.c
@@ -145,7 +145,6 @@
 #define VNDMR2_VLV(n)		((n & 0xf) << 12)
 
 /* Video n CSI2 Interface Mode Register (Gen3) */
-#define VNCSI_IFMD_DES2		(1 << 27)
 #define VNCSI_IFMD_DES1		(1 << 26)
 #define VNCSI_IFMD_DES0		(1 << 25)
 #define VNCSI_IFMD_CSI_CHSEL(n) ((n & 0xf) << 0)
@@ -1577,7 +1576,7 @@ void rvin_set_chsel(struct rvin_dev *vin, u8 chsel)
 	vnmc = rvin_read(vin, VNMC_REG) & ~VNMC_VUP;
 	rvin_write(vin, vnmc, VNMC_REG);
 
-	ifmd = VNCSI_IFMD_DES2 | VNCSI_IFMD_DES1 | VNCSI_IFMD_DES0 |
+	ifmd = VNCSI_IFMD_DES1 | VNCSI_IFMD_DES0 |
 		VNCSI_IFMD_CSI_CHSEL(chsel);
 
 	rvin_write(vin, ifmd, VNCSI_IFMD_REG);
