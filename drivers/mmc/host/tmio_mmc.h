20e5623df1aaa74301675122b910f063cdcabe7e
Masaharu Hayakawa <masaharu.hayakawa.ry@renesas.com>
2017-11-22 11:02:22 +0900

    mmc: tmio: Add internal DMA transfer end
    Although the completion of the data transfer processing was judged only
    by the Access End interrupt of SDHI, the Transfer End interruption of
    DMAC was also added to the condition.
    
    Signed-off-by: Masaharu Hayakawa <masaharu.hayakawa.ry@renesas.com>
    [saito: rebase on v5.4]
    Signed-off-by: Takeshi Saito <takeshi.saito.xv@renesas.com>

--- drivers/mmc/host/tmio_mmc.h
+++ drivers/mmc/host/tmio_mmc.h
@@ -105,6 +105,9 @@
 		TMIO_STAT_CARD_REMOVE | TMIO_STAT_CARD_INSERT)
 #define TMIO_MASK_IRQ     (TMIO_MASK_READOP | TMIO_MASK_WRITEOP | TMIO_MASK_CMD)
 
+#define TMIO_TRANSTATE_DEND	0x00000001
+#define TMIO_TRANSTATE_AEND	0x00000002
+
 #define TMIO_MAX_BLK_SIZE 512
 
 struct tmio_mmc_data;
@@ -118,6 +121,7 @@ struct tmio_mmc_dma_ops {
 	void (*release)(struct tmio_mmc_host *host);
 	void (*abort)(struct tmio_mmc_host *host);
 	void (*dataend)(struct tmio_mmc_host *host);
+	bool (*dma_irq)(struct tmio_mmc_host *host);
 
 	/* optional */
 	void (*end)(struct tmio_mmc_host *host);	/* held host->lock */
@@ -159,6 +163,7 @@ struct tmio_mmc_host {
 	/* Cache */
 	u32			sdcard_irq_mask;
 	u32			sdio_irq_mask;
+	u32			dma_irq_mask;
 	unsigned int		clk_cache;
 	u32			sdcard_irq_setbit_mask;
 
@@ -168,6 +173,9 @@ struct tmio_mmc_host {
 	bool			native_hotplug;
 	bool			runtime_synced;
 	bool			sdio_irq_enabled;
+
+	spinlock_t		trans_lock;	/* protect trans_state */
+	unsigned int		trans_state;
 
 	/* Mandatory callback */
 	int (*clk_enable)(struct tmio_mmc_host *host);
@@ -195,6 +203,9 @@ void tmio_mmc_host_free(struct tmio_mmc_host *host);
 int tmio_mmc_host_probe(struct tmio_mmc_host *host);
 void tmio_mmc_host_remove(struct tmio_mmc_host *host);
 void tmio_mmc_do_data_irq(struct tmio_mmc_host *host);
+
+void tmio_mmc_set_transtate(struct tmio_mmc_host *host, unsigned int state);
+void tmio_mmc_clear_transtate(struct tmio_mmc_host *host);
 
 void tmio_mmc_enable_mmc_irqs(struct tmio_mmc_host *host, u32 i);
 void tmio_mmc_disable_mmc_irqs(struct tmio_mmc_host *host, u32 i);
