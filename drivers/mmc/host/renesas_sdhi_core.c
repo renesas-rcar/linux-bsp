392cec847dc9c84ab574f28daf14ee5222f4f6a9
Masaharu Hayakawa <masaharu.hayakawa.ry@renesas.com>
2017-11-22 11:02:56 +0900

    mmc: renesas_sdhi: Add internal DMA transfer end
    Although the completion of the data transfer processing was judged only
    by the Access End interrupt of SDHI, the Transfer End interruption of
    DMAC was also added to the condition.
    
    Signed-off-by: Masaharu Hayakawa <masaharu.hayakawa.ry@renesas.com>

--- drivers/mmc/host/renesas_sdhi_core.c
+++ drivers/mmc/host/renesas_sdhi_core.c
@@ -33,6 +33,7 @@
 #include <linux/pinctrl/consumer.h>
 #include <linux/pinctrl/pinctrl-state.h>
 #include <linux/regulator/consumer.h>
+#include <linux/sys_soc.h>
 
 #include "renesas_sdhi.h"
 #include "tmio_mmc.h"
@@ -48,6 +49,18 @@
 
 #define host_to_priv(host) \
 	container_of((host)->pdata, struct renesas_sdhi, mmc_data)
+
+static const struct soc_device_attribute sdhi_quirks_match[]  = {
+	{ .soc_id = "r8a7795", .revision = "ES1.*",
+	  .data = (void *)DTRAEND1_SET_BIT17, },
+	{ .soc_id = "r8a7795", .revision = "ES2.0",
+	  .data = 0, },
+	{ .soc_id = "r8a7796", .revision = "ES1.0",
+	  .data = (void *)DTRAEND1_SET_BIT17, },
+	{ .soc_id = "r8a7796", .revision = "ES1.1",
+	  .data = 0, },
+	{/*sentinel*/},
+};
 
 struct renesas_sdhi {
 	struct clk *clk;
@@ -493,6 +506,7 @@ int renesas_sdhi_probe(struct platform_device *pdev,
 	struct tmio_mmc_host *host;
 	struct renesas_sdhi *priv;
 	struct resource *res;
+	const struct soc_device_attribute *attr;
 	int irq, ret, i;
 
 	of_data = of_device_get_match_data(&pdev->dev);
@@ -544,6 +558,10 @@ int renesas_sdhi_probe(struct platform_device *pdev,
 		ret = -ENOMEM;
 		goto eprobe;
 	}
+
+	attr = soc_device_match(sdhi_quirks_match);
+	if (attr)
+		host->sdhi_quirks = (uintptr_t)attr->data;
 
 	if (of_data) {
 		mmc_data->flags |= of_data->tmio_flags;
