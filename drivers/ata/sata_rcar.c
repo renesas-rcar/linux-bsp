354968aa5f954a7abed9d5aba343abfbe981cef9
Masaharu Hayakawa <masaharu.hayakawa.ry@renesas.com>
2017-11-27 16:23:54 +0900

    ata: sata_rcar: Add r8a77965 support
    This patch adds r8a77965 support in SATA.
    
    Signed-off-by: Masaharu Hayakawa <masaharu.hayakawa.ry@renesas.com>

--- drivers/ata/sata_rcar.c
+++ drivers/ata/sata_rcar.c
@@ -859,6 +859,10 @@ static const struct of_device_id sata_rcar_match[] = {
 		.data = (void *)RCAR_GEN2_SATA
 	},
 	{
+		.compatible = "renesas,sata-r8a77965",
+		.data = (void *)RCAR_GEN2_SATA
+	},
+	{
 		.compatible = "renesas,rcar-gen2-sata",
 		.data = (void *)RCAR_GEN2_SATA
 	},
