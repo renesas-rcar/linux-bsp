c54baf44f64c1c2ac0bf26bbae07a704d8430c3b
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2017-11-28 18:59:03 +0900

    v4l: vsp1: Add write back support
    Execute this function, and a user get screen of one shot.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/media/platform/vsp1/vsp1_wpf.c
+++ drivers/media/platform/vsp1/vsp1_wpf.c
@@ -14,6 +14,7 @@
 #include <linux/device.h>
 
 #include <media/v4l2-subdev.h>
+#include <media/vsp1.h>
 
 #include "vsp1.h"
 #include "vsp1_dl.h"
@@ -256,7 +257,7 @@ static void wpf_prepare(struct vsp1_entity *entity,
 						   wpf->entity.config,
 						   RWPF_PAD_SOURCE);
 	/* Format */
-	if (!pipe->lif) {
+	if (!pipe->lif || pipe->output->write_back == WB_STAT_CATP_SET) {
 		const struct v4l2_pix_format_mplane *format = &wpf->format;
 		const struct vsp1_format_info *fmtinfo = wpf->fmtinfo;
 
@@ -296,7 +297,11 @@ static void wpf_prepare(struct vsp1_entity *entity,
 	vsp1_dl_fragment_write(dlb, VI6_DPR_WPF_FPORCH(wpf->entity.index),
 			       VI6_DPR_WPF_FPORCH_FP_WPFN);
 
-	vsp1_dl_fragment_write(dlb, VI6_WPF_WRBCK_CTRL, 0);
+	if (pipe->lif && pipe->output->write_back == WB_STAT_CATP_SET)
+		vsp1_wpf_write(wpf, dlb, VI6_WPF_WRBCK_CTRL,
+			       VI6_WPF_WRBCK_CTRL_WBMD);
+	else
+		vsp1_wpf_write(wpf, dlb, VI6_WPF_WRBCK_CTRL, 0);
 
 	/*
 	 * Sources. If the pipeline has a single input and BRU is not used,
@@ -398,9 +403,19 @@ static void wpf_configure(struct vsp1_entity *entity,
 		       (0 << VI6_WPF_SZCLIP_OFST_SHIFT) |
 		       (height << VI6_WPF_SZCLIP_SIZE_SHIFT));
 
-	if (pipe->lif)
+	if (pipe->lif) {
+		if (wpf->write_back == WB_STAT_CATP_SET) {
+			vsp1_wpf_write(wpf, dlb, VI6_WPF_DSTM_ADDR_Y,
+				       wpf->buf_addr[0]);
+			if (format->num_planes > 1)
+				vsp1_wpf_write(wpf, dlb, VI6_WPF_DSTM_ADDR_C0,
+					       wpf->buf_addr[1]);
+			if (format->num_planes > 2)
+				vsp1_wpf_write(wpf, dlb, VI6_WPF_DSTM_ADDR_C1,
+					       wpf->buf_addr[2]);
+		}
 		return;
-
+	}
 	/*
 	 * Update the memory offsets based on flipping configuration.
 	 * The destination addresses point to the locations where the
