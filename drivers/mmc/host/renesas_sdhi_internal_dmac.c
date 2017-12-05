d389e4f01a8e0fe2e9f3706a4ef4f4cba5b9bd18
Masaharu Hayakawa <masaharu.hayakawa.ry@renesas.com>
2017-12-05 10:26:18 +0900

    mmc: renesas_sdhi: Add PIO mode support
    This patch adds switching function of the transfer mode of PIO and DMA.
    
    Signed-off-by: Masaharu Hayakawa <masaharu.hayakawa.ry@renesas.com>
    [saito: adjust context]
    Signed-off-by: Takeshi Saito <takeshi.saito.xv@renesas.com>
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/mmc/host/renesas_sdhi_internal_dmac.c
+++ drivers/mmc/host/renesas_sdhi_internal_dmac.c
@@ -593,8 +593,12 @@ static int renesas_sdhi_internal_dmac_probe(struct platform_device *pdev)
 	/* value is max of SD_SECCNT. Confirmed by HW engineers */
 	dma_set_max_seg_size(dev, 0xffffffff);
 
+#ifndef CONFIG_MMC_SDHI_PIO
 	return renesas_sdhi_probe(pdev, &renesas_sdhi_internal_dmac_dma_ops,
 				  of_data_quirks->of_data, quirks);
+#else
+	return renesas_sdhi_probe(pdev, NULL, of_data_quirks->of_data, quirks);
+#endif
 }
 
 static const struct dev_pm_ops renesas_sdhi_internal_dmac_dev_pm_ops = {
