c3eb50deca092bbdc0edcdd8b4f3f9292db838e1
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2017-12-06 20:30:24 +0900

    drm: rcar-du: Fix digital RGB routing for R8A77995
    This patch adds D3 definition for DU and fixes digital RGB routing.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_group.c
+++ drivers/gpu/drm/rcar-du/rcar_du_group.c
@@ -93,6 +93,10 @@ static void rcar_du_group_setup_defr8(struct rcar_du_group *rgrp)
 
 		if (crtc / 2 == rgrp->index)
 			defr8 |= DEFR8_DRGBS_DU(crtc);
+
+		if (rcar_du_has(rcdu, RCAR_DU_FEATURE_R8A77995_REGS))
+			defr8 |= (DEFR8_DRGBS_DU(rcdu->dpad0_source) |
+				 DEFR8_DRGBS_DU(crtc));
 	}
 
 	rcar_du_group_write(rgrp, DEFR8, defr8);
