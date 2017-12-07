939523388bc945b1362d8e71641cc494dd12ffe3
Geert Uytterhoeven <geert+renesas@glider.be>
2017-12-07 11:15:19 +0100

    PCI: rcar: Fix use-after-free in probe error path
    If CONFIG_DEBUG_SLAB=y, and no PCIe card is inserted, the kernel crashes
    during probe on r8a7791/koelsch:
    
      rcar-pcie fe000000.pcie: PCIe link down
      Unable to handle kernel paging request at virtual address 6b6b6b6b
    
    (seeing this message requires earlycon and keep_bootcon).
    
    Indeed, pci_free_host_bridge() frees the PCI host bridge, including the
    embedded rcar_pcie object, so pci_free_resource_list() must not be called
    afterwards.
    
    To fix this, move the call to pci_free_resource_list() up, and update the
    label name accordingly.
    
    Fixes: ddd535f1ea3eb27e ("PCI: rcar: Fix memory leak when no PCIe card is inserted")
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Simon Horman <horms+renesas@verge.net.au>
    Acked-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    (cherry picked from commit 0c31f1d7be1b5c4858b1d714dcefa25f41428cab)
    Signed-off-by: Phong Hai Hoang <phong.hoang.wz@rvc.renesas.com>

--- drivers/pci/host/pcie-rcar.c
+++ drivers/pci/host/pcie-rcar.c
@@ -1227,12 +1227,12 @@ static int rcar_pcie_probe(struct platform_device *pdev)
 	err = rcar_pcie_get_resources(pcie);
 	if (err < 0) {
 		dev_err(dev, "failed to request resources: %d\n", err);
-		goto err_free_bridge;
+		goto err_free_resource_list;
 	}
 
 	err = rcar_pcie_parse_map_dma_ranges(pcie, dev->of_node);
 	if (err)
-		goto err_free_bridge;
+		goto err_free_resource_list;
 
 	/* Failure to get a link might just be that no cards are inserted */
 	hw_init_fn = of_device_get_match_data(dev);
@@ -1268,9 +1268,9 @@ err_pm_put:
 err_pm_disable:
 	pm_runtime_disable(dev);
 
-err_free_bridge:
-	pci_free_host_bridge(bridge);
+err_free_resource_list:
 	pci_free_resource_list(&pcie->resources);
+	pci_free_host_bridge(bridge);
 
 	return err;
 }
