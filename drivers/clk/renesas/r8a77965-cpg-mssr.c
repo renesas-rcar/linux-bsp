46bc6c735e87bfef549d3c95cfa9ab05880e81d9
Takeshi Kihara <takeshi.kihara.df@renesas.com>
2017-12-14 22:37:49 +0900

    clk: renesas: r8a77965: Add IIC-DVFS clock
    This patch adds IIC-DVFS clock to the R8A77965 SoC.
    
    Based on a similar patch of the R8A7796 CPG/MSSR driver
    by Khiem Nguyen <khiem.nguyen.xt@renesas.com>
    
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/clk/renesas/r8a77965-cpg-mssr.c
+++ drivers/clk/renesas/r8a77965-cpg-mssr.c
@@ -185,6 +185,7 @@ static const struct mssr_mod_clk r8a77965_mod_clks[] __initconst = {
 	DEF_MOD("gpio0",		 912,	R8A77965_CLK_S3D4),
 	DEF_MOD("i2c6",			 918,	R8A77965_CLK_S0D6),
 	DEF_MOD("i2c5",			 919,	R8A77965_CLK_S0D6),
+	DEF_MOD("i2c-dvfs",		 926,	R8A77965_CLK_CP),
 	DEF_MOD("i2c4",			 927,	R8A77965_CLK_S0D6),
 	DEF_MOD("i2c3",			 928,	R8A77965_CLK_S0D6),
 	DEF_MOD("i2c2",			 929,	R8A77965_CLK_S3D2),
