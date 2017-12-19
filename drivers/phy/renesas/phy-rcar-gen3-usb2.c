9c92bd2a5e232fdd908fec741f4f25f446019537
Junki Kato <junki.kato.xk@renesas.com>
2017-12-19 13:11:43 +0900

    phy: rcar-gen3-usb2: Add a compatible string for r8a77995
    This patch adds a compatible string for r8a77995.
    
    Signed-off-by: Junki Kato <junki.kato.xk@renesas.com>
    Signed-off-by: Hoang Vo <hoang.vo.eb@renesas.com>
    Signed-off-by: Tho Vu <tho.vu.wh@renesas.com>
    Signed-off-by: Truong Minh Quang <quang.truong.xt@bp.renesas.com>

--- drivers/phy/renesas/phy-rcar-gen3-usb2.c
+++ drivers/phy/renesas/phy-rcar-gen3-usb2.c
@@ -590,6 +590,10 @@ static const struct of_device_id rcar_gen3_phy_usb2_match_table[] = {
 		.data = &rz_g1c_phy_usb2_data,
 	},
 	{
+		.compatible = "renesas,usb2-phy-r8a77995",
+		.data = &rcar_gen3_phy_usb2_data,
+	},
+	{
 		.compatible = "renesas,usb2-phy-r8a7795",
 		.data = &rcar_gen3_phy_usb2_data,
 	},
