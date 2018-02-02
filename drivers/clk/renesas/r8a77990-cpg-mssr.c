ce20511349e403eea686401207233eac93e32f08
Takeshi Kihara <takeshi.kihara.df@renesas.com>
2018-02-02 22:49:44 +0900

    clk: renesas: r8a77990: Add GPIO clocks
    This patch adds GPIO clocks to the R8A77990 SoC.
    
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/clk/renesas/r8a77990-cpg-mssr.c
+++ drivers/clk/renesas/r8a77990-cpg-mssr.c
@@ -109,6 +109,13 @@ static const struct cpg_core_clk r8a77990_core_clks[] __initconst = {
 static const struct mssr_mod_clk r8a77990_mod_clks[] __initconst = {
 	DEF_MOD("scif2",		 310,	R8A77990_CLK_S3D4C),
 	DEF_MOD("intc-ap",		 408,	R8A77990_CLK_S0D3),
+	DEF_MOD("gpio6",		 906,	R8A77990_CLK_S3D4),
+	DEF_MOD("gpio5",		 907,	R8A77990_CLK_S3D4),
+	DEF_MOD("gpio4",		 908,	R8A77990_CLK_S3D4),
+	DEF_MOD("gpio3",		 909,	R8A77990_CLK_S3D4),
+	DEF_MOD("gpio2",		 910,	R8A77990_CLK_S3D4),
+	DEF_MOD("gpio1",		 911,	R8A77990_CLK_S3D4),
+	DEF_MOD("gpio0",		 912,	R8A77990_CLK_S3D4),
 };
 
 static const unsigned int r8a77990_crit_mod_clks[] __initconst = {
