9edc51eae3576269f8d2027580a289862c0e4dfb
Takeshi Kihara <takeshi.kihara.df@renesas.com>
2018-02-02 16:36:05 +0900

    clk: renesas: cpg-mssr: Add support for R-Car E3
    Initial support for R-Car E3 (R8A77990), including basic core clocks,
    and SCIF2 (console) and INTC-AP (GIC) module clocks.
    
    Based on a similar patch of the R8A7795 CPG/MSSR driver
    by Geert Uytterhoeven <geert+renesas@glider.be>
    
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/clk/renesas/renesas-cpg-mssr.h
+++ drivers/clk/renesas/renesas-cpg-mssr.h
@@ -142,6 +142,7 @@ extern const struct cpg_mssr_info r8a7795_cpg_mssr_info;
 extern const struct cpg_mssr_info r8a7796_cpg_mssr_info;
 extern const struct cpg_mssr_info r8a77965_cpg_mssr_info;
 extern const struct cpg_mssr_info r8a77970_cpg_mssr_info;
+extern const struct cpg_mssr_info r8a77990_cpg_mssr_info;
 extern const struct cpg_mssr_info r8a77995_cpg_mssr_info;
 
 
