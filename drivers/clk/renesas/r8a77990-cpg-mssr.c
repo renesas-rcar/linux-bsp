096a516a3906809910d6cae8cc090341932c38c0
Takeshi Kihara <takeshi.kihara.df@renesas.com>
2018-02-06 09:26:19 +0900

    clk: renesas: r8a77990: Add I2C clocks
    This patch adds I2C{0,1,2,3,4,5,6} clocks to the R8A77990 SoC.
    
    Based on similar work of the R8A7795 CPG/MSSR driver
    by Geert Uytterhoeven <geert+renesas@glider.be>
    
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/clk/renesas/r8a77990-cpg-mssr.c
+++ drivers/clk/renesas/r8a77990-cpg-mssr.c
@@ -116,6 +116,13 @@ static const struct mssr_mod_clk r8a77990_mod_clks[] __initconst = {
 	DEF_MOD("gpio2",		 910,	R8A77990_CLK_S3D4),
 	DEF_MOD("gpio1",		 911,	R8A77990_CLK_S3D4),
 	DEF_MOD("gpio0",		 912,	R8A77990_CLK_S3D4),
+	DEF_MOD("i2c6",			 918,	R8A77990_CLK_S3D2),
+	DEF_MOD("i2c5",			 919,	R8A77990_CLK_S3D2),
+	DEF_MOD("i2c4",			 927,	R8A77990_CLK_S3D2),
+	DEF_MOD("i2c3",			 928,	R8A77990_CLK_S3D2),
+	DEF_MOD("i2c2",			 929,	R8A77990_CLK_S3D2),
+	DEF_MOD("i2c1",			 930,	R8A77990_CLK_S3D2),
+	DEF_MOD("i2c0",			 931,	R8A77990_CLK_S3D2),
 };
 
 static const unsigned int r8a77990_crit_mod_clks[] __initconst = {
