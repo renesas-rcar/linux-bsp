48583b59bf651b1efa52fe771d7898d47be7e9a7
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2018-02-22 15:37:17 +0900

    rcar-vin: Fix power management procedure when resuming
    The timing of the queue buf start may be executed before
    the processing of the delay work (setup of VIN, video decoder and CSI2)
    at the time of resume, and recovery processing could not be done well.
    
    When queue buf starts, it modifies it so that it restarts after
    waiting for setup process executed at resume.
    
    In addition, add procedure for waiting for capture stop when suspending.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/media/platform/rcar-vin/rcar-core.c
+++ drivers/media/platform/rcar-vin/rcar-core.c
@@ -1413,7 +1413,11 @@ static int rcar_vin_suspend(struct device *dev)
 
 	rvin_suspend_stop_streaming(vin);
 
+	vin->suspend = true;
+
 	pm_runtime_put(vin->dev);
+	reset_control_assert(vin->rstc);
+	reset_control_deassert(vin->rstc);
 
 	return 0;
 }
@@ -1429,8 +1433,8 @@ static int rcar_vin_resume(struct device *dev)
 		return 0;
 
 	pm_runtime_get_sync(vin->dev);
-	queue_delayed_work(vin->work_queue, &vin->rvin_resume,
-			   msecs_to_jiffies(CONNECTION_TIME));
+	queue_delayed_work_on(0, vin->work_queue, &vin->rvin_resume,
+			      msecs_to_jiffies(CONNECTION_TIME));
 
 	return 0;
 }
