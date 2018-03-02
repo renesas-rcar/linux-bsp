8a0cb18a5b1221e1fc45ed0872ae3d559f2df94d
Takeshi Kihara <takeshi.kihara.df@renesas.com>
2018-03-02 22:16:46 +0900

    Revert "soc: renesas: rcar-rst: Add R8A77965 support"
    This reverts commit 3198315cdc82 ("soc: renesas: rcar-rst: Add R8A77965
    support") to replace with the kernel/git/horms/renesas.git commit.
    
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/soc/renesas/rcar-rst.c
+++ drivers/soc/renesas/rcar-rst.c
@@ -41,7 +41,6 @@ static const struct of_device_id rcar_rst_matches[] __initconst = {
 	/* R-Car Gen3 is handled like R-Car Gen2 */
 	{ .compatible = "renesas,r8a7795-rst", .data = &rcar_rst_gen2 },
 	{ .compatible = "renesas,r8a7796-rst", .data = &rcar_rst_gen2 },
-	{ .compatible = "renesas,r8a77965-rst", .data = &rcar_rst_gen2 },
 	{ .compatible = "renesas,r8a77970-rst", .data = &rcar_rst_gen2 },
 	{ .compatible = "renesas,r8a77995-rst", .data = &rcar_rst_gen2 },
 	{ /* sentinel */ }
