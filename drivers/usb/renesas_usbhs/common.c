f4a4fe160b6533f7e9ef076c43ea7e0f051aa859
Phuong Viet. Nguyen <phuong.nguyen.xw@rvc.renesas.com>
2018-03-06 20:41:00 +0700

    usb: renesas_usbhs: Add support for R-Car E3
    This patch adds support for R-Car E3. This SoC needs to release
    the PLL reset by the UGCTRL register. Same as R-Car D3 SoCs.
    
    Signed-off-by: Phuong Nguyen <phuong.nguyen.xw@renesas.com>

--- drivers/usb/renesas_usbhs/common.c
+++ drivers/usb/renesas_usbhs/common.c
@@ -482,6 +482,10 @@ static const struct of_device_id usbhs_of_match[] = {
 		.data = (void *)USBHS_TYPE_RCAR_GEN3_WITH_PLL,
 	},
 	{
+		.compatible = "renesas,usbhs-r8a77990",
+		.data = (void *)USBHS_TYPE_RCAR_GEN3_WITH_PLL,
+	},
+	{
 		.compatible = "renesas,usbhs-r8a77965",
 		.data = (void *)USBHS_TYPE_RCAR_GEN3,
 	},
