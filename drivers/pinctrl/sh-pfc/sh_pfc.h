ef9a01e25c58753f7ae61420161b92fd8e9c7dee
Takeshi Kihara <takeshi.kihara.df@renesas.com>
2018-03-07 15:26:12 +0900

    pinctrl: sh-pfc: Add PORT_GP_11 helper macro
    This follows the style of existion PORT_GP_X macros and
    will be used by a follow-up patch for the r8a77990 SoC.
    
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/pinctrl/sh-pfc/sh_pfc.h
+++ drivers/pinctrl/sh-pfc/sh_pfc.h
@@ -2,6 +2,7 @@
  * SuperH Pin Function Controller Support
  *
  * Copyright (c) 2008 Magnus Damm
+ * Copyright (C) 2018 Renesas Electronics Corp.
  *
  * This file is subject to the terms and conditions of the GNU General Public
  * License.  See the file "COPYING" in the main directory of this archive
@@ -408,9 +409,13 @@ extern const struct sh_pfc_soc_info shx3_pinmux_info;
 	PORT_GP_CFG_1(bank, 9,  fn, sfx, cfg)
 #define PORT_GP_10(bank, fn, sfx)	PORT_GP_CFG_10(bank, fn, sfx, 0)
 
-#define PORT_GP_CFG_12(bank, fn, sfx, cfg)				\
+#define PORT_GP_CFG_11(bank, fn, sfx, cfg)				\
 	PORT_GP_CFG_10(bank, fn, sfx, cfg),				\
-	PORT_GP_CFG_1(bank, 10, fn, sfx, cfg),				\
+	PORT_GP_CFG_1(bank, 10,  fn, sfx, cfg)
+#define PORT_GP_11(bank, fn, sfx)	PORT_GP_CFG_11(bank, fn, sfx, 0)
+
+#define PORT_GP_CFG_12(bank, fn, sfx, cfg)				\
+	PORT_GP_CFG_11(bank, fn, sfx, cfg),				\
 	PORT_GP_CFG_1(bank, 11, fn, sfx, cfg)
 #define PORT_GP_12(bank, fn, sfx)	PORT_GP_CFG_12(bank, fn, sfx, 0)
 
