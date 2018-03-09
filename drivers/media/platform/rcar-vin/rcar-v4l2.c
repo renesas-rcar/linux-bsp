00aaed50fe6e9b457821efa0a747dff574f00832
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2018-03-09 11:07:32 +0900

    rcar-vin: Add MSTP status check after CPG reset
    According to H/W manual, it is necessary to check MSTP status
    after CPG reset. This patch adds it.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/media/platform/rcar-vin/rcar-v4l2.c
+++ drivers/media/platform/rcar-vin/rcar-v4l2.c
@@ -14,6 +14,8 @@
  * option) any later version.
  */
 
+#include <linux/clk-provider.h>
+#include <linux/delay.h>
 #include <linux/pm_runtime.h>
 
 #include <media/v4l2-event.h>
@@ -1056,6 +1058,7 @@ static int rvin_mc_release(struct file *file)
 {
 	struct rvin_dev *vin = video_drvdata(file);
 	int ret;
+	u32 timeout = MSTP_WAIT_TIME;
 
 	mutex_lock(&vin->lock);
 
@@ -1063,7 +1066,20 @@ static int rvin_mc_release(struct file *file)
 	ret = _vb2_fop_release(file, NULL);
 
 	v4l2_pipeline_pm_use(&vin->vdev.entity, 0);
-	pm_runtime_put(vin->dev);
+	pm_runtime_put_sync(vin->dev);
+	while (1) {
+		bool enable;
+
+		enable = __clk_is_enabled(vin->clk);
+		if (enable)
+			break;
+		if (!timeout) {
+			dev_warn(vin->dev, "MSTP status timeout\n");
+			break;
+		}
+		usleep_range(10, 15);
+		timeout--;
+	}
 	reset_control_assert(vin->rstc);
 
 	mutex_unlock(&vin->lock);
