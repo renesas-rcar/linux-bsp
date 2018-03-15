80b86f5cc61ec85e695fb752976c7ecd17c2d623
Dien Pham <dien.pham.ry@renesas.com>
2018-03-15 18:15:14 +0700

    soc: renesas: rcar-sysc: Improve power domains always on implementation
    R-Car Gen3 SoC has some power domains, which are transitioned by
    multiple triggers. According to Hardware Manual, it is necessary
    to control that the state transitions do not conflict for each
    power domains.
    
    The patterns of conflict are below.
    
    1. CA5x  vs CA5x : Exclusion control with HW
    2. CA5x  vs 3DG  : Exclusion control with HW
    3. CA5x  vs Other: Exclusion control with SW (Not implemented yet)
    4. 3DG   vs Other: Exclusion control with SW (already implemented)
    5. Other vs Other: Exclusion control with SW (already implemented)
    
    This patch fixes pattern 3 by turning on the other power domains
    at always. This conflict issue now occurs only on: H3 v1.*,
    H3 v2.0, M3 v1.*, M3N v1.0. So, this implementation is improved
    to apply for these platforms only.
    
    Please note this change causes an increase of power consumption.
    
    Signed-off-by: Dien Pham <dien.pham.ry@renesas.com>
    Signed-off-by: Sang Nguyen <sang.nguyen.eb@renesas.com>
    Signed-off-by: Tho Vu <tho.vu.wh@renesas.com>

--- drivers/soc/renesas/rcar-sysc.c
+++ drivers/soc/renesas/rcar-sysc.c
@@ -6,6 +6,9 @@
  * Copyright (C) 2015-2017 Glider bvba
  */
 
+#include <dt-bindings/power/r8a7795-sysc.h>
+#include <dt-bindings/power/r8a7796-sysc.h>
+#include <dt-bindings/power/r8a77965-sysc.h>
 #include <linux/clk/renesas.h>
 #include <linux/delay.h>
 #include <linux/err.h>
@@ -17,6 +20,7 @@
 #include <linux/io.h>
 #include <linux/iopoll.h>
 #include <linux/soc/renesas/rcar-sysc.h>
+#include <linux/sys_soc.h>
 
 #include "rcar-sysc.h"
 
@@ -61,6 +65,37 @@ struct rcar_sysc_ch {
 	u8 chan_bit;
 	u8 isr_bit;
 };
+
+static
+const struct soc_device_attribute rcar_sysc_quirks_match[] __initconst = {
+	{
+		.soc_id = "r8a7795", .revision = "ES2.0",
+		.data = (void *)(BIT(R8A7795_PD_A3VP) | BIT(R8A7795_PD_CR7)
+			| BIT(R8A7795_PD_A3VC) | BIT(R8A7795_PD_A2VC0)
+			| BIT(R8A7795_PD_A2VC1) | BIT(R8A7795_PD_A3IR)),
+	},
+	{
+		.soc_id = "r8a7795", .revision = "ES1.*",
+		.data = (void *)(BIT(R8A7795_PD_A3VP) | BIT(R8A7795_PD_CR7)
+			| BIT(R8A7795_PD_A3VC) | BIT(R8A7795_PD_A2VC0)
+			| BIT(R8A7795_PD_A2VC1) | BIT(R8A7795_PD_A3IR)),
+	},
+	{
+		.soc_id = "r8a7796", .revision = "ES1.*",
+		.data = (void *)(BIT(R8A7796_PD_CR7) | BIT(R8A7796_PD_A3VC)
+			| BIT(R8A7796_PD_A2VC0) | BIT(R8A7796_PD_A2VC1)
+			| BIT(R8A7796_PD_A3IR)),
+	},
+	{
+		.soc_id = "r8a77965", .revision = "ES1.0",
+		.data = (void *)(BIT(R8A77965_PD_A3VP) | BIT(R8A77965_PD_CR7)
+			| BIT(R8A77965_PD_A3VC) | BIT(R8A77965_PD_A2VC1)
+			| BIT(R8A77965_PD_A3IR)),
+	},
+	{ /* sentinel */ }
+};
+
+static u32 rcar_sysc_quirks;
 
 static void __iomem *rcar_sysc_base;
 static DEFINE_SPINLOCK(rcar_sysc_lock); /* SMP CPUs + I/O devices */
@@ -288,6 +323,27 @@ finalize:
 	return error;
 }
 
+struct rcar_sysc_pd *rcar_domains[RCAR_PD_ALWAYS_ON + 1];
+
+static void rcar_power_on_force(void)
+{
+	int i;
+
+	for (i = 0; i < RCAR_PD_ALWAYS_ON; i++) {
+		struct rcar_sysc_pd *pd = rcar_domains[i];
+
+		if (!pd)
+			continue;
+
+		if (rcar_sysc_quirks & BIT(pd->ch.isr_bit)) {
+			if (!rcar_sysc_power_is_off(&pd->ch))
+				continue;
+
+			rcar_sysc_power(&pd->ch, true);
+		}
+	}
+}
+
 static const struct of_device_id rcar_sysc_matches[] __initconst = {
 #ifdef CONFIG_SYSC_R8A7742
 	{ .compatible = "renesas,r8a7742-sysc", .data = &r8a7742_sysc_info },
@@ -375,6 +431,7 @@ static int __init rcar_sysc_pd_init(void)
 	void __iomem *base;
 	unsigned int i;
 	int error;
+	const struct soc_device_attribute *attr;
 
 	np = of_find_matching_node_and_match(NULL, rcar_sysc_matches, &match);
 	if (!np)
@@ -390,6 +447,10 @@ static int __init rcar_sysc_pd_init(void)
 
 	has_cpg_mstp = of_find_compatible_node(NULL, NULL,
 					       "renesas,cpg-mstp-clocks");
+
+	attr = soc_device_match(rcar_sysc_quirks_match);
+	if (attr)
+		rcar_sysc_quirks = (uintptr_t)attr->data;
 
 	base = of_iomap(np, 0);
 	if (!base) {
@@ -438,11 +499,15 @@ static int __init rcar_sysc_pd_init(void)
 		pd->ch.isr_bit = area->isr_bit;
 		pd->flags = area->flags;
 
+		if (rcar_sysc_quirks & BIT(pd->ch.isr_bit))
+			pd->flags |= PD_NO_CR;
+
 		error = rcar_sysc_pd_setup(pd);
 		if (error)
 			goto out_put;
 
 		domains->domains[area->isr_bit] = &pd->genpd;
+		rcar_domains[i] = pd;
 
 		if (area->parent < 0)
 			continue;
@@ -455,6 +520,8 @@ static int __init rcar_sysc_pd_init(void)
 			goto out_put;
 		}
 	}
+
+	rcar_power_on_force();
 
 	error = of_genpd_add_provider_onecell(np, &domains->onecell_data);
 	if (!error)
