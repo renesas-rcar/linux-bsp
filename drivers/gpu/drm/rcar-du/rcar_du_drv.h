f04dafc1062c41dc723bc9351f63e6fb4c6a79b6
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2018-03-16 12:07:46 +0900

    drm: rcar-du: Fix possible crtcs for plane when using VSPDL
    If two or more channels are specified for possible crtc of plane,
    if user move the overlay between monitors in the system test,
    the following error occurs.
    
    switching CRTC directly
    atomic core check failed
    
    In order to solve the problem, this patch fixes it by fixing
    assignment of possible crtc for each plane.
    
    In addtion, add an option to change the assignment of the
    overlay sharing with VSPDL.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_drv.h
+++ drivers/gpu/drm/rcar-du/rcar_du_drv.h
@@ -104,6 +104,9 @@ struct rcar_du_device {
 	unsigned int dpad0_source;
 	unsigned int vspd1_sink;
 
+	bool vspdl_fix;
+	unsigned int brs_num;
+
 	struct rcar_du_lvdsenc *lvds[RCAR_DU_MAX_LVDS];
 };
 
