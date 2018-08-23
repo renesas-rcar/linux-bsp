693d3a96cbcf35d7729a5c880789fa830801d780
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2018-08-23 17:08:42 +0900

    v4l: vsp1: Add interlaced mode support
    Set extended display list header, and set Auto-FLD mode for
    interlaced mode.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/media/platform/vsp1/vsp1_video.c
+++ drivers/media/platform/vsp1/vsp1_video.c
@@ -2,7 +2,7 @@
 /*
  * vsp1_video.c  --  R-Car VSP1 Video Node
  *
- * Copyright (C) 2013-2015 Renesas Electronics Corporation
+ * Copyright (C) 2013-2018 Renesas Electronics Corporation
  *
  * Contact: Laurent Pinchart (laurent.pinchart@ideasonboard.com)
  */
@@ -435,7 +435,7 @@ static void vsp1_video_pipeline_run(struct vsp1_pipeline *pipe)
 	}
 
 	/* Complete, and commit the head display list. */
-	vsp1_dl_list_commit(dl, false);
+	vsp1_dl_list_commit(dl, false, 0);
 	pipe->configured = true;
 
 	vsp1_pipeline_run(pipe);
