a8dbe70703900b19e339a8e1f4fdfac696750a2d
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2018-08-23 17:52:31 +0900

    v4l: vsp1: Add Vmute function support
    The mute function of VSPD is supported.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_kms.h
+++ drivers/gpu/drm/rcar-du/rcar_du_kms.h
@@ -1,7 +1,7 @@
 /*
  * rcar_du_kms.h  --  R-Car Display Unit Mode Setting
  *
- * Copyright (C) 2013-2014 Renesas Electronics Corporation
+ * Copyright (C) 2013-2018 Renesas Electronics Corporation
  *
  * Contact: Laurent Pinchart (laurent.pinchart@ideasonboard.com)
  *
@@ -35,5 +35,6 @@ int rcar_du_modeset_init(struct rcar_du_device *rcdu);
 
 int rcar_du_dumb_create(struct drm_file *file, struct drm_device *dev,
 			struct drm_mode_create_dumb *args);
+int rcar_du_async_commit(struct drm_device *dev, struct drm_crtc *crtc);
 
 #endif /* __RCAR_DU_KMS_H__ */
