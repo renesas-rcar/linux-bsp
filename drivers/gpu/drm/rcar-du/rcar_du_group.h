92e9b398c9d47fd721cf479fbcbf48d1926d6be1
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2018-08-24 18:54:59 +0900

    drm: rcar-du: Add routing set for digital RGB previously
    When using only DU0 without DRGBS register setting, DU0 outputs to LVDS
    and Digital RGB. By setting DRGBS register for the routing of digital RGB
    previously, DU0 outputs to LVDS only.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_group.h
+++ drivers/gpu/drm/rcar-du/rcar_du_group.h
@@ -1,7 +1,7 @@
 /*
  * rcar_du_group.c  --  R-Car Display Unit Planes and CRTCs Group
  *
- * Copyright (C) 2013-2014 Renesas Electronics Corporation
+ * Copyright (C) 2013-2018 Renesas Electronics Corporation
  *
  * Contact: Laurent Pinchart (laurent.pinchart@ideasonboard.com)
  *
@@ -61,7 +61,9 @@ void rcar_du_group_put(struct rcar_du_group *rgrp);
 void rcar_du_group_start_stop(struct rcar_du_group *rgrp, bool start);
 void rcar_du_group_restart(struct rcar_du_group *rgrp);
 int rcar_du_group_set_routing(struct rcar_du_group *rgrp);
-
+void rcar_du_pre_group_set_routing(struct rcar_du_group *rgrp,
+				   struct rcar_du_crtc *rcrtc,
+				   unsigned int swindex);
 int rcar_du_set_dpad0_vsp1_routing(struct rcar_du_device *rcdu);
 
 #endif /* __RCAR_DU_GROUP_H__ */
