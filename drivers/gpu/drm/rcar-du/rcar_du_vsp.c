c74382366dda06c2246211507c703ba2a04ac3df
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2018-09-10 15:29:51 +0900

    drm: rcar-du: Add support for colorkey alpha blending
    Add two new plane properties colorkey and colorkey_alpha for rcar gen3.
    * colorkey:
    	- used for specifying the color on which the filtering is done.
    	- bits 0 to 23 are interpreted as RGB888 format, in case we are
    	  dealing with an YCbCr format, only the Y componenet is
    	  compared and it is represented by the G bits from RGB888
    	  format.
    	- bit 24 tells if it is enabled or not.
    * colorkey_alpha:
    	- the alpha to be set for matching pixels, in case it is
    	  missing the pixels will be made transparent
    
    Reported-by: Alexandru Gheorghe <Alexandru_Gheorghe@mentor.com>
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_vsp.c
+++ drivers/gpu/drm/rcar-du/rcar_du_vsp.c
@@ -183,6 +183,11 @@ static void rcar_du_vsp_plane_setup(struct rcar_du_vsp_plane *plane)
 		.pitch = fb->pitches[0],
 		.alpha = state->alpha,
 		.zpos = state->state.zpos,
+		.colorkey = state->colorkey & RCAR_DU_COLORKEY_COLOR_MASK,
+		.colorkey_en =
+			((state->colorkey & RCAR_DU_COLORKEY_EN_MASK) != 0),
+		.colorkey_alpha =
+			(state->colorkey_alpha & RCAR_DU_COLORKEY_ALPHA_MASK),
 	};
 	unsigned int i;
 
@@ -327,6 +332,8 @@ rcar_du_vsp_plane_atomic_duplicate_state(struct drm_plane *plane)
 
 	__drm_atomic_helper_plane_duplicate_state(plane, &copy->state);
 	copy->alpha = to_rcar_vsp_plane_state(plane->state)->alpha;
+	copy->colorkey = to_rcar_vsp_plane_state(plane->state)->colorkey;
+	copy->colorkey_alpha = to_rcar_vsp_plane_state(plane->state)->colorkey_alpha;
 
 	return &copy->state;
 }
@@ -352,6 +359,8 @@ static void rcar_du_vsp_plane_reset(struct drm_plane *plane)
 		return;
 
 	state->alpha = 255;
+	state->colorkey = RCAR_DU_COLORKEY_NONE;
+	state->colorkey_alpha = 0;
 	state->state.zpos = plane->type == DRM_PLANE_TYPE_PRIMARY ? 0 : 1;
 
 	plane->state = &state->state;
@@ -479,6 +488,10 @@ static int rcar_du_vsp_plane_atomic_set_property(struct drm_plane *plane,
 
 	if (property == rcdu->props.alpha)
 		rstate->alpha = val;
+	else if (property == rcdu->props.colorkey)
+		rstate->colorkey = val;
+	else if (property == rcdu->props.colorkey_alpha)
+		rstate->colorkey_alpha = val;
 	else
 		return -EINVAL;
 
@@ -495,6 +508,10 @@ static int rcar_du_vsp_plane_atomic_get_property(struct drm_plane *plane,
 
 	if (property == rcdu->props.alpha)
 		*val = rstate->alpha;
+	else if (property == rcdu->props.colorkey)
+		*val = rstate->colorkey;
+	else if (property == rcdu->props.colorkey_alpha)
+		*val = rstate->colorkey_alpha;
 	else
 		return -EINVAL;
 
@@ -568,6 +585,13 @@ int rcar_du_vsp_init(struct rcar_du_vsp *vsp, struct device_node *np,
 
 		drm_object_attach_property(&plane->plane.base,
 					   rcdu->props.alpha, 255);
+		drm_object_attach_property(&plane->plane.base,
+					   rcdu->props.colorkey,
+					   RCAR_DU_COLORKEY_NONE);
+		if (rcdu->props.colorkey_alpha)
+			drm_object_attach_property(&plane->plane.base,
+						   rcdu->props.colorkey_alpha,
+						   0);
 		drm_plane_create_zpos_property(&plane->plane, 1, 1,
 					       vsp->num_planes - 1);
 	}
