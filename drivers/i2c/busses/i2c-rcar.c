fdb9df5a868a3a24f7d6f5aa119ef0d5e9947e6e
Hiromitsu Yamasaki <hiromitsu.yamasaki.ym@renesas.com>
2018-09-12 17:52:14 +0900

    i2c: rcar: Fix dma_direction variable clear
    When I2C DMA is completed, rcar_i2c_dma_callback() is called as
    CallBack from the DMAE driver(thread), and rcar_i2c_dma_unmap()
    is called in it.
    
    By disabling the DMA Enable Register(ICDMAER), rcar_i2c_dma_unmap()
    enables interrupts for register settings(such as Master Control
    Register(ICMCR)) in I2C and advances the I2C transfer sequence.
    
    If a task switch occurs immediately after ICDMAER is disabled,
    the I2C transfer sequence controlled by the I2C interrupt proceeds
    and the DMA transfer of the next message is prepared(rcar_i2c_dma()).
    
    DMA_NONE is set to the dma_direction flag when control returns to
    rcar_i2c_dma_unmap() from task switching.
    
    However, the dma_direction flag is set in preparing the DMA for
    the next message.
    
    By checking the dma_direction flag at the completion of DMA of
    the next message due to the overwriting of the dma_direction flag
    with DMA_NONE, BUG_ON () is displayed.
    
    Signed-off-by: Hiromitsu Yamasaki <hiromitsu.yamasaki.ym@renesas.com>

--- drivers/i2c/busses/i2c-rcar.c
+++ drivers/i2c/busses/i2c-rcar.c
@@ -331,10 +331,10 @@ static void rcar_i2c_dma_unmap(struct rcar_i2c_priv *priv)
 	/* Reset default delay */
 	rcar_i2c_write(priv, ICFBSCR, TCYC06);
 
+	priv->dma_direction = DMA_NONE;
+
 	/* Disable DMA Master Received/Transmitted */
 	rcar_i2c_write(priv, ICDMAER, 0);
-
-	priv->dma_direction = DMA_NONE;
 }
 
 static void rcar_i2c_cleanup_dma(struct rcar_i2c_priv *priv)
