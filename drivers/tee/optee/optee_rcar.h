7537aefce153b3c0b840f48e2a5f49d50e28feea
Jun Miyauchi <jun.miyauchi.sr@hitachi.com>
2018-09-13 11:41:20 +0900

    tee: optee: Change workqueue to kthread in debug log function
    Improve the issue that the load of workqueue causes core dump.
    When receiving many requests to output debug logs from OP-TEE,
    R-Car OP-TEE driver queues many requests to workqueue.
    This patch modifies R-Car OP-TEE driver to output debug logs
    with kthread instead of workqueue.
    
    Signed-off-by: Jun Miyauchi <jun.miyauchi.sr@hitachi.com>
    Signed-off-by: Hiroki Negishi <hiroki.negishi.bx@renesas.com>

--- drivers/tee/optee/optee_rcar.h
+++ drivers/tee/optee/optee_rcar.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2017, Renesas Electronics Corporation
+ * Copyright (c) 2015-2018, Renesas Electronics Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License Version 2 as
@@ -14,9 +14,24 @@
 #ifndef OPTEE_RCAR_H
 #define OPTEE_RCAR_H
 
+#include <linux/types.h>
+#include <linux/list.h>
+#include <linux/wait.h>
+
 #include "rcar_version.h"
 
 #define OPTEE_MSG_RPC_CMD_DEBUG_LOG	(0x3F000000U)
+
+struct rcar_debug_log_info {
+	wait_queue_head_t waitq;
+	struct list_head queue;
+	spinlock_t q_lock; /* queue lock */
+};
+
+struct rcar_debug_log_node {
+	struct list_head list;
+	char *logmsg;
+};
 
 void handle_rpc_func_cmd_debug_log(struct optee_msg_arg *arg);
 int optee_rcar_probe(struct optee *optee);
