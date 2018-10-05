fa1cae90999bc49aef85ca647873ee81bb91c12d
Takeshi Kihara <takeshi.kihara.df@renesas.com>
2018-10-05 11:24:14 +0900

    Revert "soc: renesas: r8a77990-sysc: Add workaround for 3DG-{A,B}"
    This reverts commit ef5eaf2658db7a3a81df869d59c02288cb299de5.
    
    Revert commit ef5eaf2658db ("soc: renesas: r8a77990-sysc: Add workaround
    for 3DG-{A,B}") switched the clock parent as workaround for 3DG-{A,B} of
    R-Car E3 ES1.0, but the power domain will not work unless the
    r8a77990_areas[] array is also sorted.
    
    Therefore, revert the commit and make modification to switch parent and
    array for 3DG-{A,B} of R-Car E3 ES1.0.
    
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/soc/renesas/r8a77990-sysc.c
+++ drivers/soc/renesas/r8a77990-sysc.c
@@ -8,13 +8,12 @@
 #include <linux/bitops.h>
 #include <linux/bug.h>
 #include <linux/kernel.h>
-#include <linux/sys_soc.h>
 
 #include <dt-bindings/power/r8a77990-sysc.h>
 
 #include "rcar-sysc.h"
 
-static struct rcar_sysc_area r8a77990_areas[] __initdata = {
+static const struct rcar_sysc_area r8a77990_areas[] __initconst = {
 	{ "always-on",	    0, 0, R8A77990_PD_ALWAYS_ON, -1, PD_ALWAYS_ON },
 	{ "ca53-scu",	0x140, 0, R8A77990_PD_CA53_SCU,  R8A77990_PD_ALWAYS_ON,
 	  PD_SCU },
@@ -29,45 +28,11 @@ static struct rcar_sysc_area r8a77990_areas[] __initdata = {
 	{ "3dg-b",	0x100, 1, R8A77990_PD_3DG_B,	R8A77990_PD_3DG_A },
 };
 
-static void __init rcar_sysc_fix_parent(struct rcar_sysc_area *areas,
-					unsigned int num_areas, u8 id,
-					int new_parent)
-{
-	unsigned int i;
-
-	for (i = 0; i < num_areas; i++)
-		if (areas[i].isr_bit == id) {
-			areas[i].parent = new_parent;
-			return;
-		}
-}
-
-/* Fixups for R-Car E3 ES1.0 revision */
-static const struct soc_device_attribute r8a77990[] __initconst = {
-	{ .soc_id = "r8a77990", .revision = "ES1.0" },
-	{ /* sentinel */ }
-};
-
-static int __init r8a77990_sysc_init(void)
-{
-	if (soc_device_match(r8a77990)) {
-		rcar_sysc_fix_parent(r8a77990_areas,
-				     ARRAY_SIZE(r8a77990_areas),
-				     R8A77990_PD_3DG_A, R8A77990_PD_3DG_B);
-		rcar_sysc_fix_parent(r8a77990_areas,
-				     ARRAY_SIZE(r8a77990_areas),
-				     R8A77990_PD_3DG_B, R8A77990_PD_ALWAYS_ON);
-	}
-
-	return 0;
-}
-
 static struct rcar_sysc_extra_regs r8a77990_extra_regs = {
 	.sysc_extmask_offs = 0x2F8, .sysc_extmask_msks = BIT(0)
 };
 
 const struct rcar_sysc_info r8a77990_sysc_info __initconst = {
-	.init = r8a77990_sysc_init,
 	.areas = r8a77990_areas,
 	.num_areas = ARRAY_SIZE(r8a77990_areas),
 	.extra_regs = &r8a77990_extra_regs,
