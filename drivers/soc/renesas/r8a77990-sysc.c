5f666b0181cb18e5475d57e0c9e5651e29813d08
Takeshi Kihara <takeshi.kihara.df@renesas.com>
2018-10-05 11:45:35 +0900

    soc: renesas: r8a77990-sysc: Add workaround for 3DG-{A,B}
    This patch adds workaround for 3DG-{A,B} of R-Car E3 ES1.0 because
    the SoC has a restriction about the order.
    
    NOTE: This is a provisional measure. The state transitions of the power
          domains do not work unless the arrays are rearranged according to
          the change of the parent.
    
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/soc/renesas/r8a77990-sysc.c
+++ drivers/soc/renesas/r8a77990-sysc.c
@@ -24,8 +24,8 @@ static const struct rcar_sysc_area r8a77990_areas[] __initconst = {
 	{ "cr7",	0x240, 0, R8A77990_PD_CR7,	R8A77990_PD_ALWAYS_ON },
 	{ "a3vc",	0x380, 0, R8A77990_PD_A3VC,	R8A77990_PD_ALWAYS_ON },
 	{ "a2vc1",	0x3c0, 1, R8A77990_PD_A2VC1,	R8A77990_PD_A3VC },
-	{ "3dg-a",	0x100, 0, R8A77990_PD_3DG_A,	R8A77990_PD_ALWAYS_ON },
-	{ "3dg-b",	0x100, 1, R8A77990_PD_3DG_B,	R8A77990_PD_3DG_A },
+	{ "3dg-b",	0x100, 1, R8A77990_PD_3DG_B,	R8A77990_PD_ALWAYS_ON },
+	{ "3dg-a",	0x100, 0, R8A77990_PD_3DG_A,	R8A77990_PD_3DG_B },
 };
 
 static struct rcar_sysc_extra_regs r8a77990_extra_regs = {
