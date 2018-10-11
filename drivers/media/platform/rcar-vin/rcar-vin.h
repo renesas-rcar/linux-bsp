0e5a0b9f739689ff930ebed9b498554537b128a7
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2018-10-11 12:53:07 +0900

    media: rcar-vin: Fix VnCSI_IFMD register access for r8a77990
    According to H/W manual v0.54, DES1 bit in VnCSI_IFMD register
    is reserved in r8a77990. This patch corresponds it.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/media/platform/rcar-vin/rcar-vin.h
+++ drivers/media/platform/rcar-vin/rcar-vin.h
@@ -43,6 +43,8 @@
 
 #define MSTP_WAIT_TIME 100
 
+#define RCAR_VIN_DES1_RESERVED		BIT(0)
+
 struct rvin_group;
 
 enum model_id {
@@ -205,6 +207,7 @@ struct rvin_info {
  * @chsel:		channel selection
  * @setup_wait:		wait queue used to setup VIN
  * @suspend:		suspend flag
+ * @chip_info:         chip information by each device
  */
 struct rvin_dev {
 	struct device *dev;
@@ -249,6 +252,7 @@ struct rvin_dev {
 	unsigned int chsel;
 	wait_queue_head_t setup_wait;
 	bool suspend;
+	u32 chip_info;
 };
 
 #define vin_to_source(vin)		((vin)->digital->subdev)
