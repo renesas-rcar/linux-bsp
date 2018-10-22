35857ec96bc773dfc799716b185bc17cd4988de1
Takeshi Kihara <takeshi.kihara.df@renesas.com>
2018-10-22 18:22:32 +0900

    soc: renesas: rcar-sysc: 3DG Power domains for r8a77965 are always on
    As in commit e4003822fbbb ("soc: renesas: rcar-sysc: power domains are
    always on for r8a77965"), it shows that Hardware needs to be controlled
    with the state transitions do not conflict for each power domains.
    
    However, in the 3DG power domain, there are 2 patterns of automatic turn
    off by HW power event and turn off by SW SYSC control. With these commits
    SW SYSC control cases has been missed.
    
    After verifying again, the patterns of conflict are updated as below.
    
             CA5x vs CA5x      : HW exclusion control
             CA5x vs 3DG(HW)   : HW exclusion control
             CA5x vs 3DG(SW)   : SW exclusion control (Not implemented yet)
             CA5x vs Other(SW) : SW exclusion control (already implemented)
          3DG(HW) vs 3DG(HW)   : HW exclusion control
          3DG(HW) vs 3DG(SW)   : HW exclusion control
          3DG(HW) vs Other(SW) : SW exclusion control (already implemented)
          3DG(SW) vs 3DG(SW)   : SW exclusion control (already implemented)
          3DG(SW) vs Other(SW) : SW exclusion control (already implemented)
        Other(SW) vs Other(SW) : SW exclusion control (already implemented)
    
    This patch fixes CA5x vs 3DG(SW) pattern by turning on the 3DG power
    domains at always.
    
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/soc/renesas/r8a77965-sysc.c
+++ drivers/soc/renesas/r8a77965-sysc.c
@@ -1,7 +1,7 @@
 /*
  * Renesas R-Car M3N System Controller
  *
- * Copyright (C) 2017 Renesas Electronics Corp.
+ * Copyright (C) 2017-2018 Renesas Electronics Corp.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,8 +31,10 @@ static const struct rcar_sysc_area r8a77965_areas[] __initconst = {
 	  (PD_NO_CR | PD_ON_ONCE) },
 	{ "a2vc1",	0x3c0, 1, R8A77965_PD_A2VC1,	R8A77965_PD_A3VC,
 	  (PD_NO_CR | PD_ON_ONCE) },
-	{ "3dg-a",	0x100, 0, R8A77965_PD_3DG_A,	R8A77965_PD_ALWAYS_ON },
-	{ "3dg-b",	0x100, 1, R8A77965_PD_3DG_B,	R8A77965_PD_3DG_A },
+	{ "3dg-a",	0x100, 0, R8A77965_PD_3DG_A,	R8A77965_PD_ALWAYS_ON,
+	  (PD_NO_CR | PD_ON_ONCE) },
+	{ "3dg-b",	0x100, 1, R8A77965_PD_3DG_B,	R8A77965_PD_3DG_A,
+	  (PD_NO_CR | PD_ON_ONCE) },
 	{ "a3ir",	0x180, 0, R8A77965_PD_A3IR,	R8A77965_PD_ALWAYS_ON,
 	  (PD_NO_CR | PD_ON_ONCE) },
 };
