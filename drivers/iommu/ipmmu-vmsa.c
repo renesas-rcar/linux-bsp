2fe53acb936e7da994f7d633f37530d3ab8308f2
Hai Nguyen Pham <hai.pham.ud@renesas.com>
2018-11-26 14:50:20 +0700

    iommu/ipmmu-vmsa: Whitelist devices for r8a77965
    This adds whitelist devices support for r8a77965 (R-Car M3N)
    
    - For unsupported R-Car Gen3 SoCs, IPMMU will probe error.
    - For unsupported IPMMU cache devices or unsupported master devices,
    IPMMU will not do address translation.
    
    Signed-off-by: Hai Nguyen Pham <hai.pham.ud@renesas.com>

--- drivers/iommu/ipmmu-vmsa.c
+++ drivers/iommu/ipmmu-vmsa.c
@@ -189,6 +189,46 @@ static struct ipmmu_whitelist *r8a7796_whitelist[] = {
 	NULL, /* Terminator */
 };
 
+/* R-Car M3N (R8A77965) */
+static struct ipmmu_whitelist r8a77965_ipmmu_vi0 = {
+	.ipmmu_name	= "febd0000.mmu",
+	.base_addr	= IPMMU_VI0_BASE,
+	.ip_masters	= M3N_IPMMU_VI0_MASTER,
+};
+
+static struct ipmmu_whitelist r8a77965_ipmmu_hc = {
+	.ipmmu_name	= "e6570000.mmu",
+	.base_addr	= IPMMU_HC_BASE,
+	.ip_masters	= M3N_IPMMU_HC_MASTER,
+};
+
+static struct ipmmu_whitelist r8a77965_ipmmu_mp = {
+	.ipmmu_name	= "ec670000.mmu",
+	.base_addr	= IPMMU_MP_BASE,
+	.ip_masters	= M3N_IPMMU_MP_MASTER,
+};
+
+static struct ipmmu_whitelist r8a77965_ipmmu_ds0 = {
+	.ipmmu_name	= "e6740000.mmu",
+	.base_addr	= IPMMU_DS0_BASE,
+	.ip_masters	= M3N_IPMMU_DS0_MASTER,
+};
+
+static struct ipmmu_whitelist r8a77965_ipmmu_ds1 = {
+	.ipmmu_name	= "e7740000.mmu",
+	.base_addr	= IPMMU_DS1_BASE,
+	.ip_masters	= M3N_IPMMU_DS1_MASTER,
+};
+
+static struct ipmmu_whitelist *r8a77965_whitelist[] = {
+	&r8a77965_ipmmu_vi0,
+	&r8a77965_ipmmu_hc,
+	&r8a77965_ipmmu_mp,
+	&r8a77965_ipmmu_ds0,
+	&r8a77965_ipmmu_ds1,
+	NULL, /* Terminator */
+};
+
 #endif /* CONFIG_IPMMU_VMSA_WHITELIST */
 
 #define TLB_LOOP_TIMEOUT		100	/* 100us */
@@ -954,6 +994,11 @@ static const struct soc_device_attribute r8a7796[]  = {
 	{ /* sentinel */ }
 };
 
+static const struct soc_device_attribute r8a77965[]  = {
+	{ .soc_id = "r8a77965" },
+	{ /* sentinel */ }
+};
+
 static bool ipmmu_slave_whitelist(struct device *dev, u32 *ids)
 {
 	struct ipmmu_vmsa_device *mmu = to_ipmmu(dev);
@@ -1293,6 +1338,8 @@ static int ipmmu_whitelist_init(struct ipmmu_vmsa_device *mmu)
 		mmu->whitelist = r8a7795_whitelist;
 	else if (soc_device_match(r8a7796))
 		mmu->whitelist = r8a7796_whitelist;
+	else if (soc_device_match(r8a77965))
+		mmu->whitelist = r8a77965_whitelist;
 	else
 		mmu->whitelist = NULL;
 
