0c7a66e6849d205d4443ee7d8bdd39c43e0e36e1
Dirk Behme <dirk.behme@de.bosch.com>
2019-01-18 18:11:39 +0900

    dmaengine: sh: rcar-dmac: With cyclic DMA residue 0 is valid
    Having a cyclic DMA, a residue 0 is not an indication of a completed
    DMA. In case of cyclic DMA make sure that dma_set_residue() is called
    and with this a residue of 0 is forwarded correctly to the caller.
    
    Fixes: 3544d2878817 ("dmaengine: rcar-dmac: use result of updated get_residue in tx_status")
    Signed-off-by: Dirk Behme <dirk.behme@de.bosch.com>
    Signed-off-by: Achim Dahlhoff <Achim.Dahlhoff@de.bosch.com>
    Signed-off-by: Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>
    Cc: <stable@vger.kernel.org> # v4.8+
    
    Link: https://patchwork.kernel.org/patch/10839055/
    Signed-off-by: Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>

--- drivers/dma/sh/rcar-dmac.c
+++ drivers/dma/sh/rcar-dmac.c
@@ -1387,7 +1387,7 @@ static enum dma_status rcar_dmac_tx_status(struct dma_chan *chan,
 	spin_unlock_irqrestore(&rchan->lock, flags);
 
 	/* if there's no residue, the cookie is complete */
-	if (!residue)
+	if (!residue && !rchan->desc.running->cyclic)
 		return DMA_COMPLETE;
 
 	dma_set_residue(txstate, residue);
