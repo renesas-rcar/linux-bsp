37e0a4f9455dd109e0deea3d00356300e24426f0
Nam Dang <nam.dang.uw@rvc.renesas.com>
2019-01-29 10:12:53 +0700

    soc: renesas: rcar-sysc: Remove power domains always on for M3N v1.0
    In the past, we have enabled power domains always on
    for M3N v1.0 due to hardware restriction.
    But after confirming from hardware team
    it is not needed. So, this patch removes it.
    
    Signed-off-by: Nam Dang <nam.dang.uw@rvc.renesas.com>
    Signed-off-by: Dien Pham <dien.pham.ry@renesas.com>
    Signed-off-by: Tho Vu <tho.vu.wh@renesas.com>

--- drivers/soc/renesas/rcar-sysc.c
+++ drivers/soc/renesas/rcar-sysc.c
@@ -94,12 +94,6 @@ const struct soc_device_attribute rcar_sysc_quirks_match[] __initconst = {
 			| BIT(R8A7796_PD_A3IR) | BIT(R8A7796_PD_3DG_A)
 			| BIT(R8A7796_PD_3DG_B)),
 	},
-	{
-		.soc_id = "r8a77965", .revision = "ES1.0",
-		.data = (void *)(BIT(R8A77965_PD_A3VP) | BIT(R8A77965_PD_CR7)
-			| BIT(R8A77965_PD_A3VC) | BIT(R8A77965_PD_A2VC1)
-			| BIT(R8A77965_PD_A3IR)),
-	},
 	{ /* sentinel */ }
 };
 
