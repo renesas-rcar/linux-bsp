3391bf1223c5bb3bc33210f0f00f7fc5b5c8bcce
Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>
2019-04-19 17:57:24 +0900

    Revert "dmaengine: sh: rcar-dmac: With cyclic DMA residue 0 is valid"
    This reverts commit 0c7a66e6849d205d4443ee7d8bdd39c43e0e36e1.
    Link: https://patchwork.kernel.org/patch/10839055/
    It included NULL access problem, therefore revert it.
    
    Signed-off-by: Hiroyuki Yokoyama <hiroyuki.yokoyama.vx@renesas.com>

--- drivers/dma/sh/rcar-dmac.c
+++ drivers/dma/sh/rcar-dmac.c
@@ -1407,7 +1407,7 @@ static enum dma_status rcar_dmac_tx_status(struct dma_chan *chan,
 	spin_unlock_irqrestore(&rchan->lock, flags);
 
 	/* if there's no residue, the cookie is complete */
-	if (!residue && !rchan->desc.running->cyclic)
+	if (!residue)
 		return DMA_COMPLETE;
 
 	dma_set_residue(txstate, residue);
