ce50f275803d3cd9cc1ee3c306f95dd8122ae704
Loic Pallardy <loic.pallardy@st.com>
2019-11-28 12:33:51 +0100

    remoteproc: add support for co-processor loaded and booted before kernel
    [ From https://patchwork.kernel.org/project/linux-remoteproc/patch/20200211174205.22247-2-arnaud.pouliquen@st.com/ ]
    
    Remote processor could boot independently or be loaded/started before
    Linux kernel by bootloader or any firmware.
    This patch introduces a new property in rproc core, named skip_fw_load,
    to be able to allocate resources and sub-devices like vdev and to
    synchronize with current state without loading firmware from file system.
    It is platform driver responsibility to implement the right firmware
    load ops according to HW specificities.
    
    Signed-off-by: Loic Pallardy <loic.pallardy@st.com>
    Acked-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>

--- include/linux/remoteproc.h
+++ include/linux/remoteproc.h
@@ -508,6 +508,7 @@ struct rproc_dump_segment {
  * @has_iommu: flag to indicate if remote processor is behind an MMU
  * @auto_boot: flag to indicate if remote processor should be auto-started
  * @autonomous: true if an external entity has booted the remote processor
+ * @skip_fw_load: remote processor has been preloaded before start sequence
  * @dump_segments: list of segments in the firmware
  * @nb_vdev: number of vdev currently handled by rproc
  * @char_dev: character device of the rproc
@@ -545,6 +546,7 @@ struct rproc {
 	bool has_iommu;
 	bool auto_boot;
 	bool autonomous;
+	bool skip_fw_load;
 	struct list_head dump_segments;
 	int nb_vdev;
 	u8 elf_class;
