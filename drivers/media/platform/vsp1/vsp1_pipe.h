8f3e43f52012aa44e60055969f7d7aa910ea28f6
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2019-12-10 18:43:55 +0900

    v4l: vsp1: Add write back support
    Execute this function, and a user get screen of one shot.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/media/platform/vsp1/vsp1_pipe.h
+++ drivers/media/platform/vsp1/vsp1_pipe.h
@@ -109,6 +109,8 @@ struct vsp1_partition {
  * @partition: The current partition for configuration to process
  * @part_table: The pre-calculated partitions used by the pipeline
  * @vmute_flag: Enable mute flag of VSPD
+ * @event_wait: wait queue to wait for write back state
+ * @completed: display update completion flag
  */
 struct vsp1_pipeline {
 	struct media_pipeline pipe;
@@ -151,6 +153,8 @@ struct vsp1_pipeline {
 	struct vsp1_partition *part_table;
 
 	bool vmute_flag;
+	wait_queue_head_t event_wait;
+	bool completed;
 };
 
 void vsp1_pipeline_reset(struct vsp1_pipeline *pipe);
