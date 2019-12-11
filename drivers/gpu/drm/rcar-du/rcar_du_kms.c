b46748099c0d2c9bebfc9cf2301d727a25d78edf
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2019-12-11 13:20:15 +0900

    drm: rcar-du: Fix possible crtcs for plane when using VSPDL
    If two or more channels are specified for possible crtc of plane,
    if user move the overlay between monitors in the system test,
    the following error occurs.
    
    switching CRTC directly, atomic core check failed.
    
    In order to solve the problem, this patch fixes it by fixing
    assignment of possible crtc for each plane.
    
    In addtion, add an option to change the assignment of the
    overlay sharing with VSPDL.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_kms.c
+++ drivers/gpu/drm/rcar-du/rcar_du_kms.c
@@ -652,12 +652,18 @@ static int rcar_du_vsps_init(struct rcar_du_device *rcdu)
 
 	for (i = 0; i < rcdu->num_crtcs; ++i) {
 		unsigned int j;
+		unsigned int brs_num = 0;
 
 		ret = of_parse_phandle_with_fixed_args(np, "vsps", cells, i,
 						       &args);
 		if (ret < 0)
 			goto error;
 
+		ret = of_property_read_u32(args.np, "renesas,#brs", &brs_num);
+		if (brs_num > 2) {
+			dev_err(rcdu->dev, "error: brs number\n");
+			goto error;
+		}
 		/*
 		 * Add the VSP to the list or update the corresponding existing
 		 * entry if the VSP has already been added.
@@ -677,6 +683,13 @@ static int rcar_du_vsps_init(struct rcar_du_device *rcdu)
 		/* Store the VSP pointer and pipe index in the CRTC. */
 		rcdu->crtcs[i].vsp = &rcdu->vsps[j];
 		rcdu->crtcs[i].vsp_pipe = cells >= 1 ? args.args[0] : 0;
+
+		/* Has VSPDL */
+		if (rcdu->crtcs[i].vsp_pipe) {
+			if (!ret)
+				rcdu->vspdl_fix = true;
+			rcdu->brs_num = brs_num;
+		}
 	}
 
 	/*
@@ -740,6 +753,8 @@ int rcar_du_modeset_init(struct rcar_du_device *rcdu)
 	}
 
 	rcdu->num_crtcs = hweight8(rcdu->info->channels_mask);
+	rcdu->vspdl_fix = false;
+	rcdu->brs_num = 0;
 
 	ret = rcar_du_properties_init(rcdu);
 	if (ret < 0)
