5c4e8b8da066954f120dd8c320625e8ac0e85fce
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2019-12-16 11:55:37 +0900

    drm: rcar-du: Add dev_type for dw-hdmi driver
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_dw_hdmi.c
+++ drivers/gpu/drm/rcar-du/rcar_dw_hdmi.c
@@ -108,6 +108,7 @@ static const struct dw_hdmi_plat_data rcar_dw_hdmi_plat_data = {
 	.output_port = 1,
 	.mode_valid = rcar_hdmi_mode_valid,
 	.configure_phy	= rcar_hdmi_phy_configure,
+	.dev_type	= RCAR_HDMI,
 };
 
 static int rcar_dw_hdmi_probe(struct platform_device *pdev)
