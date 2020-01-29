7a1e2d4a75647bc24312df86b09e00196f594454
Takeshi Kihara <takeshi.kihara.df@renesas.com>
2020-01-29 16:00:01 +0530

    clk: renesas: r8a7796: Remove INTC-AP and INTC-EX clocks
    According to the R-Car Gen3 Hardware Manual Rev 1.00, the the module clock
    status, module clock control, and reset control bits of the INTC-AP and
    INTC-EX modules on R-Car M3 are removed.
    
    Continue to change the clock properties of the gic and irqc nodes in the
    device tree.
    
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/clk/renesas/r8a7796-cpg-mssr.c
+++ drivers/clk/renesas/r8a7796-cpg-mssr.c
@@ -146,8 +146,6 @@ static const struct mssr_mod_clk r8a7796_mod_clks[] __initconst = {
 	DEF_MOD("usb-dmac0",		 330,	R8A7796_CLK_S3D1),
 	DEF_MOD("usb-dmac1",		 331,	R8A7796_CLK_S3D1),
 	DEF_MOD("rwdt",			 402,	R8A7796_CLK_R),
-	DEF_MOD("intc-ex",		 407,	R8A7796_CLK_CP),
-	DEF_MOD("intc-ap",		 408,	R8A7796_CLK_S0D3),
 	DEF_MOD("audmac1",		 501,	R8A7796_CLK_S1D2),
 	DEF_MOD("audmac0",		 502,	R8A7796_CLK_S1D2),
 	DEF_MOD("drif31",		 508,	R8A7796_CLK_S3D2),
@@ -249,11 +247,6 @@ static const struct mssr_mod_clk r8a7796_mod_clks[] __initconst = {
 	DEF_MOD("scu-src0",		1031,	MOD_CLK_ID(1017)),
 };
 
-static const unsigned int r8a7796_crit_mod_clks[] __initconst = {
-	MOD_CLK_ID(408),	/* INTC-AP (GIC) */
-};
-
-
 /*
  * CPG Clock Data
  */
@@ -334,10 +327,6 @@ const struct cpg_mssr_info r8a7796_cpg_mssr_info __initconst = {
 	.mod_clks = r8a7796_mod_clks,
 	.num_mod_clks = ARRAY_SIZE(r8a7796_mod_clks),
 	.num_hw_mod_clks = 12 * 32,
-
-	/* Critical Module Clocks */
-	.crit_mod_clks = r8a7796_crit_mod_clks,
-	.num_crit_mod_clks = ARRAY_SIZE(r8a7796_crit_mod_clks),
 
 	/* Callbacks */
 	.init = r8a7796_cpg_mssr_init,
