c455abc152c42481c40e05ae465d25507697b828
Ryo Kataoka <ryo.kataoka.wt@renesas.com>
2020-02-13 18:00:29 +0530

    spi: sh-msiof: Add sleep before master transfer for test
    This patch is for debug of transfer between master and slave.
    Since the slave needs to complete a preparation in data transfer
    before the master working, the sleep wait is put before
    the data transfer of the master.
    
    Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>
    (cherry picked from commit ed869ddbeb8d7f60d3f4e7301c34eb2462ec8420)
    Signed-off-by: Thanh Quan <thanh.quan.xn@renesas.com>

--- drivers/spi/spi-sh-msiof.c
+++ drivers/spi/spi-sh-msiof.c
@@ -41,6 +41,10 @@ struct sh_msiof_chipdata {
 	u32 flags;
 };
 
+#ifdef CONFIG_SPI_SH_MSIOF_TRANSFER_SYNC_DEBUG
+#define TRANSFAR_SYNC_DELAY (CONFIG_SPI_SH_MSIOF_TRANSFER_SYNC_DEBUG_MSLEEP)
+#endif /* CONFIG_SPI_SH_MSIOF_TRANSFER_SYNC_DEBUG */
+
 struct sh_msiof_spi_priv {
 	struct spi_controller *ctlr;
 	void __iomem *mapbase;
@@ -863,6 +867,11 @@ static int sh_msiof_transfer_one(struct spi_controller *ctlr,
 		if (tx_buf)
 			copy32(p->tx_dma_page, tx_buf, l / 4);
 
+#ifdef CONFIG_SPI_SH_MSIOF_TRANSFER_SYNC_DEBUG
+		if (!!spi_controller_is_target(p->ctlr))
+			msleep(TRANSFAR_SYNC_DELAY);
+#endif /* CONFIG_SPI_SH_MSIOF_TRANSFER_SYNC_DEBUG */
+
 		ret = sh_msiof_dma_once(p, tx_buf, rx_buf, l, max_wdlen);
 		if (ret == -EAGAIN) {
 			dev_warn_once(&p->pdev->dev,
@@ -935,6 +944,11 @@ static int sh_msiof_transfer_one(struct spi_controller *ctlr,
 	words = len / bytes_per_word;
 
 	while (words > 0) {
+#ifdef CONFIG_SPI_SH_MSIOF_TRANSFER_SYNC_DEBUG
+		if (!!spi_controller_is_target(p->ctlr))
+			msleep(TRANSFAR_SYNC_DELAY);
+#endif /* CONFIG_SPI_SH_MSIOF_TRANSFER_SYNC_DEBUG */
+
 		n = sh_msiof_spi_txrx_once(p, tx_fifo, rx_fifo, tx_buf, rx_buf,
 					   words, bits);
 		if (n < 0)
