966e7b6ef35063bb38389fb3ea0a8b9077f0cde2
LUU HOAI <hoai.luu.ub@renesas.com>
2020-02-17 16:20:22 +0900

    drm: rcar-du: Add DIDSRO register setting for V3U
    In V3U, To enable DSI/CSI-2-TX-IF1 pin, LDCS1 and LDCS0 bits
    are always set to 1 in DIDSR0 register.
    
    Signed-off-by: LUU HOAI <hoai.luu.ub@renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_group.c
+++ drivers/gpu/drm/rcar-du/rcar_du_group.c
@@ -148,6 +148,8 @@ static void rcar_du_group_setup_didsr(struct rcar_du_group *rgrp)
 			      |  DIDSR_PDCS_CLK(i, 0);
 		else if (rcdu->info->dsi_clk_mask & BIT(rcrtc->index))
 			didsr |= DIDSR_LDCS_DSI(i);
+		else if (rcdu->info->mipi_dsi_clk_mask & BIT(rcrtc->index))
+			didsr |= DIDSR_LDCS_LVDS0(i);
 		else
 			didsr |= DIDSR_LDCS_DCLKIN(i)
 			      |  DIDSR_PDCS_CLK(i, 0);
