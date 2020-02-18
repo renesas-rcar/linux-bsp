ae0e82db6d63371f5068f45f76849018698848c7
Ryo Kataoka <ryo.kataoka.wt@renesas.com>
2020-02-18 09:51:28 +0530

    spi: sh-msiof: Add MSIOF module clock changing processing for R-Car Gen3
    This patch adds processing to set MSIOF module clock.
    
    To use this processing, make the following preparations to the device tree.
    1.Add a pointer to the "msiof_ref_clk" node to the clocks property.
    
    2.Define the module clock to Hz in the "clock-frequency" property
      of the msiof_ref_clk node. Please set a value based on electrical
      characteristics.
    
    /* module clock */
    &msiof_ref_clk {
        clock-frequency = <133333333>;
    };
    
    Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>
    Signed-off-by: Duc Nguyen <duc.nguyen.ub@renesas.com>
    Signed-off-by: Thanh Quan <thanh.quan.xn@renesas.com>

--- drivers/spi/spi-sh-msiof.c
+++ drivers/spi/spi-sh-msiof.c
@@ -1214,6 +1214,7 @@ static int sh_msiof_spi_probe(struct platform_device *pdev)
 	struct sh_msiof_spi_priv *p;
 	struct device *dev = &pdev->dev;
 	unsigned long clksrc;
+	struct clk *ref_clk;
 	int i;
 	int ret;
 
@@ -1260,6 +1261,17 @@ static int sh_msiof_spi_probe(struct platform_device *pdev)
 		dev_err(dev, "cannot get clock\n");
 		ret = PTR_ERR(p->clk);
 		goto err1;
+	}
+
+	/* MSIOF module clock setup */
+	ref_clk = devm_clk_get(&pdev->dev, "mso");
+	if (!IS_ERR(ref_clk)) {
+		clksrc = clk_get_rate(ref_clk);
+		if (clksrc) {
+			clk_prepare_enable(p->clk);
+			clk_set_rate(p->clk, clksrc);
+			clk_disable_unprepare(p->clk);
+		}
 	}
 
 	i = platform_get_irq(pdev, 0);
