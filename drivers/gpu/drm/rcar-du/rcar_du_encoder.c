fa8eaecfddae6e185d77f68ce9f7ebb2ae1a5f23
LUU HOAI <hoai.luu.ub@renesas.com>
2020-02-25 16:42:23 +0900

    drm: rcar-du: Add output checking in encoder for DSI port.
    Add output checking for DSI port when encoder cannot find
    the bridge.
    
    Signed-off-by: LUU HOAI <hoai.luu.ub@renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_encoder.c
+++ drivers/gpu/drm/rcar-du/rcar_du_encoder.c
@@ -87,6 +87,13 @@ int rcar_du_encoder_init(struct rcar_du_device *rcdu,
 #else
 				return 0;
 #endif
+			} else if (output == RCAR_DU_OUTPUT_MIPI_DSI0 ||
+				   output == RCAR_DU_OUTPUT_MIPI_DSI1) {
+#if IS_ENABLED(CONFIG_DRM_RCAR_MIPI_DSI)
+				return -EPROBE_DEFER;
+#else
+				return 0;
+#endif
 			} else {
 				return -EPROBE_DEFER;
 			}
