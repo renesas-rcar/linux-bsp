168b86b2e40dfd66b75186bc1e05e53306683e44
Dung Nguyen <dung.nguyen.zy@renesas.com>
2020-03-09 14:08:54 +0700

    clk: renesas: r8a77965: Add CCREE clock
    This patch adds the clock used by the CryptoCell 630p instance in
    the SoC.
    
    Signed-off-by: Dung Nguyen <dung.nguyen.zy@renesas.com>

--- drivers/clk/renesas/r8a77965-cpg-mssr.c
+++ drivers/clk/renesas/r8a77965-cpg-mssr.c
@@ -132,7 +132,7 @@ static const struct mssr_mod_clk r8a77965_mod_clks[] __initconst = {
 	DEF_MOD("sys-dmac2",		217,	R8A77965_CLK_S3D1),
 	DEF_MOD("sys-dmac1",		218,	R8A77965_CLK_S3D1),
 	DEF_MOD("sys-dmac0",		219,	R8A77965_CLK_S0D3),
-
+	DEF_MOD("sceg-pub",		229,	R8A77965_CLK_CR),
 	DEF_MOD("cmt3",			300,	R8A77965_CLK_R),
 	DEF_MOD("cmt2",			301,	R8A77965_CLK_R),
 	DEF_MOD("cmt1",			302,	R8A77965_CLK_R),
