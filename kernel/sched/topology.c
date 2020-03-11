f145038857301862795233a47fc145ffa10a1664
Takeshi Kihara <takeshi.kihara.df@renesas.com>
2020-03-11 11:23:16 +0530

    sched/core: Disable SD_ASYM_CPUCAPACITY for root_domains without asymmetry
    When hotplugging cpus out or creating exclusive cpusets (disabling
    sched_load_balance) systems which were asymmetric at boot might become
    symmetric. In this case leaving the flag set might lead to suboptimal
    scheduling decisions.
    
    The arch-code proving the flag doesn't have visibility of the cpuset
    configuration so it must either be told by passing a cpumask or the
    generic topology code has to verify if the flag should still be set
    when taking the actual sched_domain_span() into account. This patch
    implements the latter approach.
    
    We need to detect capacity based on calling arch_scale_cpu_capacity()
    directly as rq->cpu_capacity_orig hasn't been set yet early in the boot
    process.
    
    Cherry-picked from https://patchwork.kernel.org/patch/10506521/
    
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- kernel/sched/topology.c
+++ kernel/sched/topology.c
@@ -1610,6 +1610,26 @@ sd_init(struct sched_domain_topology_level *tl,
 		  "CPU capacity asymmetry not supported on SMT\n");
 
 	/*
+	 * Check if cpu_map eclipses cpu capacity asymmetry.
+	 */
+
+	if (sd->flags & SD_ASYM_CPUCAPACITY) {
+		int i;
+		bool disable = true;
+		long capacity = arch_scale_cpu_capacity(sd_id);
+
+		for_each_cpu(i, sched_domain_span(sd)) {
+			if (capacity != arch_scale_cpu_capacity(i)) {
+				disable = false;
+				break;
+			}
+		}
+
+		if (disable)
+			sd->flags &= ~SD_ASYM_CPUCAPACITY;
+	}
+
+	/*
 	 * Convert topological properties into behaviour.
 	 */
 	/* Don't attempt to spread across CPUs of different capacities. */
