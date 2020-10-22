32cdac51bf5556c3d826786cc67fd1c7b8a054f9
Linh Phung <linh.phung.jy@renesas.com>
2020-10-22 11:36:03 +0700

    Revert "cpufreq: Remove CPUFREQ_ADJUST and CPUFREQ_NOTIFY policy notifier events"
    This reverts commit df0eea4488081e0698b0b58ccd1e8c8823e22841.
    
    Renesas EMS driver still depends on these notifier events
    for scaling down CPU frequency in case of thermal overheat.
    Therefore, reverting this patch.
    
    Signed-off-by: Hien Dang <hien.dang.eb@rvc.renesas.com>
    Signed-off-by: Linh Phung <linh.phung.jy@renesas.com>
    Signed-off-by: Tho Vu <tho.vu.wh@renesas.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -494,8 +494,10 @@ static inline void cpufreq_resume(void) {}
 #define CPUFREQ_POSTCHANGE		(1)
 
 /* Policy Notifiers  */
-#define CPUFREQ_CREATE_POLICY		(0)
-#define CPUFREQ_REMOVE_POLICY		(1)
+#define CPUFREQ_ADJUST			(0)
+#define CPUFREQ_NOTIFY			(1)
+#define CPUFREQ_CREATE_POLICY		(2)
+#define CPUFREQ_REMOVE_POLICY		(3)
 
 #ifdef CONFIG_CPU_FREQ
 int cpufreq_register_notifier(struct notifier_block *nb, unsigned int list);
