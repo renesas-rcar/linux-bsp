5b6220307e91981a7aaa9e64143d2f2b1bea1043
Phil Edworthy <phil.edworthy@renesas.com>
2020-10-30 12:04:12 +0000

    rcar soc: Remove Cortex-R7 power domain
    When using the Cortex-R7 core before Linux is started, we need to make
    sure Linux does not turn off the CR7 power domain.

--- drivers/soc/renesas/r8a77965-sysc.c
+++ drivers/soc/renesas/r8a77965-sysc.c
@@ -22,7 +22,6 @@ static const struct rcar_sysc_area r8a77965_areas[] __initconst = {
 	  PD_CPU_NOCR },
 	{ "ca57-cpu1",	 0x80, 1, R8A77965_PD_CA57_CPU1, R8A77965_PD_CA57_SCU,
 	  PD_CPU_NOCR },
-	{ "cr7",	0x240, 0, R8A77965_PD_CR7,	R8A77965_PD_ALWAYS_ON },
 	{ "a3vc",	0x380, 0, R8A77965_PD_A3VC,	R8A77965_PD_ALWAYS_ON },
 	{ "a3vp",	0x340, 0, R8A77965_PD_A3VP,	R8A77965_PD_ALWAYS_ON },
 	{ "a2vc1",	0x3c0, 1, R8A77965_PD_A2VC1,	R8A77965_PD_A3VC },
