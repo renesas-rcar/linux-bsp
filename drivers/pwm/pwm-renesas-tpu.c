b8d2d86cb77ed1de69d80378c21a0736ac91dc00
Duc Nguyen <duc.nguyen.ub@renesas.com>
2020-11-19 09:59:17 +0700

    pwm: pwm-renesas-tpu: Correct the valid period and duty_cycle
    This patch fixes the valid period and duty_cycle value that can
    be covered same clock division.
    
    Signed-off-by: Duc Nguyen <duc.nguyen.ub@renesas.com>

--- drivers/pwm/pwm-renesas-tpu.c
+++ drivers/pwm/pwm-renesas-tpu.c
@@ -301,11 +301,14 @@ static int tpu_pwm_config(struct pwm_chip *chip, struct pwm_device *pwm,
 
 	period >>= 2 * prescaler;
 
-	if (duty_ns)
+	if (duty_ns) {
 		duty = mul_u64_u64_div_u64(clk_rate, duty_ns,
 					   (u64)NSEC_PER_SEC << (2 * prescaler));
-	else
+		if (duty > period || duty == 0)
+			return -EINVAL;
+	} else {
 		duty = 0;
+	}
 
 	dev_dbg(&tpu->pdev->dev,
 		"rate %u, prescaler %u, period %u, duty %u\n",
