30a03b1618164d1bd3695f50f722f8cc6d0db50e
Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
2021-03-01 20:25:01 +0900

    tee: optee: add SMC of START_DLOG_OUTPUT to rcar_optee_init_debug_log()
    For the R-Car logging function,
    the OP-TEE log output was modified to start from sending
    START_DLOG_OUTPUT SMC.
    
    Signed-off-by: Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
    Signed-off-by: Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>

--- drivers/tee/optee/rcar_version.h
+++ drivers/tee/optee/rcar_version.h
@@ -1,12 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (c) 2015-2018, Renesas Electronics Corporation
+ * Copyright (c) 2015-2020, Renesas Electronics Corporation
  * All rights reserved.
  */
 
 #ifndef RCAR_VERSION_H
 #define RCAR_VERSION_H
 
-#define VERSION_OF_RENESAS "1.0.10"
+#define VERSION_OF_RENESAS "1.0.11"
 
 #endif /* RCAR_VERSION_H */
