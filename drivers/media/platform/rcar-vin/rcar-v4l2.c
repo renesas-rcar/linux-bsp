02fe8e76f8dbbc21c747b0e28cb8e5ae5cbc570c
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2021-03-03 14:57:14 +0900

    media: rcar-vin: Fix clock control flow
    About VIN drivers sequence at using CSI2, it is nesessary that
    CPG (Clock Pulse Generator) reset/release is added according to
    H/W manual. So this patch adds it.
    In addition, CPG stop status check. The start check of CPG is
    unnecessary because the CPG enable side is already supported
    by the Clock driver.
    
    Also, if the enable/disable of CPG is implemented with an open or
    release function, it may be called wastefully from another process,
    so this patch modifies it to control CPG when Stream ON/OFF.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/media/platform/rcar-vin/rcar-v4l2.c
+++ drivers/media/platform/rcar-vin/rcar-v4l2.c
@@ -921,12 +921,6 @@ static int rvin_open(struct file *file)
 	struct rvin_dev *vin = video_drvdata(file);
 	int ret;
 
-	ret = pm_runtime_get_sync(vin->dev);
-	if (ret < 0) {
-		pm_runtime_put_noidle(vin->dev);
-		return ret;
-	}
-
 	ret = mutex_lock_interruptible(&vin->lock);
 	if (ret)
 		goto err_pm;
@@ -964,7 +958,6 @@ err_open:
 err_unlock:
 	mutex_unlock(&vin->lock);
 err_pm:
-	pm_runtime_put(vin->dev);
 
 	return ret;
 }
@@ -991,8 +984,6 @@ static int rvin_release(struct file *file)
 	}
 
 	mutex_unlock(&vin->lock);
-
-	pm_runtime_put(vin->dev);
 
 	return ret;
 }
