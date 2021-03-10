78cd3924970773978b749963464f563b1b021a4b
Khiem Nguyen <khiem.nguyen.xt@renesas.com>
2021-03-10 17:03:56 +0700

    drm: rcar-rvgc: Remove DRIVER_PRIME feature
    The DRIVER_PRIME feature has been moved since v5.2
    as commit 0424fdaf88 (drm/prime: Actually remove DRIVER_PRIME everywhere).
    Hence, this patch applied the change similar to rcar-du driver.
    
    Signed-off-by: Khiem Nguyen <khiem.nguyen.xt@renesas.com>

--- drivers/gpu/drm/rcar-rvgc/rcar_rvgc_drv.c
+++ drivers/gpu/drm/rcar-rvgc/rcar_rvgc_drv.c
@@ -128,8 +128,7 @@ static int rcar_rvgc_dumb_create(struct drm_file* file, struct drm_device* dev,
 DEFINE_DRM_GEM_CMA_FOPS(rcar_rvgc_fops);
 
 static struct drm_driver rcar_rvgc_driver = {
-	.driver_features	= DRIVER_GEM | DRIVER_MODESET | DRIVER_PRIME
-	| DRIVER_ATOMIC,
+	.driver_features	= DRIVER_GEM | DRIVER_MODESET | DRIVER_ATOMIC,
 	.gem_free_object_unlocked = drm_gem_cma_free_object,
 	.gem_vm_ops		= &drm_gem_cma_vm_ops,
 	.prime_handle_to_fd	= drm_gem_prime_handle_to_fd,
