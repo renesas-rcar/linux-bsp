f92bba5328ea649c92ebf3d250b6c6b7ca3a8174
Takeshi Saito <takeshi.saito.xv@renesas.com>
2021-03-22 12:43:47 +0900

    mmc: renesas_sdhi: Disable HS400 for r8a77995
    According to HW manual, R-Car D3 (R8A77995) support only HS200.
    
    Signed-off-by: Takeshi Saito <takeshi.saito.xv@renesas.com>
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>

--- drivers/mmc/host/renesas_sdhi_core.c
+++ drivers/mmc/host/renesas_sdhi_core.c
@@ -956,6 +956,7 @@ static const struct soc_device_attribute sdhi_quirks_match[]  = {
 	{ .soc_id = "r8a77965", .data = &sdhi_quirks_r8a77965 },
 	{ .soc_id = "r8a77980", .data = &sdhi_quirks_nohs400 },
 	{ .soc_id = "r8a77990", .data = &sdhi_quirks_r8a77990 },
+	{ .soc_id = "r8a77995", .data = &sdhi_quirks_nohs400 },
 	{ /* Sentinel. */ },
 };
 
