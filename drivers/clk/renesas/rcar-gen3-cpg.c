e4cf0324f42fdc2c431adf63d2059d0e0893b64a
Geert Uytterhoeven <geert+renesas@glider.be>
2021-03-26 13:00:54 +0100

    clk: renesas: rcar-gen3: Update Z clock rate formula in comments
    The fixed divider in the calculation of the Z and Z2 clock rates was
    generalized from a hardcoded value of two to a parameterized value, but
    the comments were not updated accordingly.
    
    Fixes: 20cc05ba04a93f05 ("clk: renesas: rcar-gen3: Parameterise Z and Z2 clock fixed divisor")
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Acked-by: Stephen Boyd <sboyd@kernel.org>
    Reviewed-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Link: https://lore.kernel.org/r/20210326120100.1577596-2-geert+renesas@glider.be
    (cherry picked from commit 192c344e7cd4e19a4260752604ba219ffa41deb1)
    Signed-off-by: LUU HOAI <hoai.luu.ub@renesas.com>

--- drivers/clk/renesas/rcar-gen3-cpg.c
+++ drivers/clk/renesas/rcar-gen3-cpg.c
@@ -175,7 +175,8 @@ static struct clk * __init cpg_pll_clk_register(const char *name,
  * Traits of this clock:
  * prepare - clk_prepare only ensures that parents are prepared
  * enable - clk_enable only ensures that parents are enabled
- * rate - rate is adjustable.  clk->rate = (parent->rate * mult / 32 ) / 2
+ * rate - rate is adjustable.
+ *        clk->rate = (parent->rate * mult / 32 ) / fixed_div
  * parent - fixed parent.  No clk_set_parent support
  */
 #define CPG_FRQCRB			0x00000004
