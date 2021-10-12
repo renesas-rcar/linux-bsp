f4ab04b642478ab592570a3be7f0d808e21bdb79
Thanh Quan <thanh.quan.xn@renesas.com>
2021-10-12 16:36:10 +0700

    spi: sh-msiof: Add MSIOF parent clock
    This patch add MSIOF parent clock
    
    Signed-off-by: Thanh Quan <thanh.quan.xn@renesas.com>

--- drivers/spi/spi-sh-msiof.c
+++ drivers/spi/spi-sh-msiof.c
@@ -1254,7 +1254,7 @@ static int sh_msiof_spi_probe(struct platform_device *pdev)
 	}
 
 	/* MSIOF module clock setup */
-	ref_clk = devm_clk_get(&pdev->dev, "msiof_ref_clk");
+	ref_clk = devm_clk_get(&pdev->dev, "mso");
 	if (!IS_ERR(ref_clk)) {
 		clksrc = clk_get_rate(ref_clk);
 		if (clksrc) {
