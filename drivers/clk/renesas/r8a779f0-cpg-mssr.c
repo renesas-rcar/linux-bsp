e5f943f8cca005998a47a7d17fef873ff3326015
Tam Nguyen <tam.nguyen.xa@renesas.com>
2022-02-22 10:58:35 +0700

    clk: renesas: r8a779f0: Add SHIPS clock
    This patch adds SHIPS clock.
    
    Signed-off-by: Tam Nguyen <tam.nguyen.xa@renesas.com>
    Signed-off-by: Truong Minh Quang <quang.truong.xt@bp.renesas.com>
    Signed-off-by: Thanh Le <thanh.le.xv@renesas.com>

--- drivers/clk/renesas/r8a779f0-cpg-mssr.c
+++ drivers/clk/renesas/r8a779f0-cpg-mssr.c
@@ -154,6 +154,7 @@ static const struct mssr_mod_clk r8a779f0_mod_clks[] __initconst = {
 	DEF_MOD("tmu2",		715,	R8A779F0_CLK_SASYNCPERD2),
 	DEF_MOD("tmu3",		716,	R8A779F0_CLK_SASYNCPERD2),
 	DEF_MOD("tmu4",		717,	R8A779F0_CLK_SASYNCPERD2),
+	DEF_MOD("ships0",	719,	R8A779F0_CLK_S0D2),
 	DEF_MOD("caiplite0",	721,	R8A779F0_CLK_S0D2),
 	DEF_MOD("caiplite1",	722,	R8A779F0_CLK_S0D2),
 	DEF_MOD("caiplite2",	723,	R8A779F0_CLK_S0D2),
@@ -173,6 +174,8 @@ static const struct mssr_mod_clk r8a779f0_mod_clks[] __initconst = {
 	DEF_MOD("cmt3",		913,	R8A779F0_CLK_R),
 	DEF_MOD("pfc0",		915,	R8A779F0_CLK_CPEX),
 	DEF_MOD("tsc",		919,	R8A779F0_CLK_CL16M),
+	DEF_MOD("ships1",	1511,	R8A779F0_CLK_S0D2),
+	DEF_MOD("ships2",	1512,	R8A779F0_CLK_S0D2),
 	DEF_MOD("ufs",		1514,	R8A779F0_CLK_S0D4_HSC),
 	DEF_MOD("thermal",      919,    R8A779F0_CLK_CL16M),
 };
