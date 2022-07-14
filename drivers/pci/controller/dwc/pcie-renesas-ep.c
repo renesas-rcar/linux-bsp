4024f66e01da7c83ef74c659cc790c5420f4df2a
Tin Tran <tin.tran.xk@renesas.com>
2022-07-14 15:52:38 +0700

    PCI: Replace PCIe registers not defined in HW UM
    The following registers are not defined in the HW UM and must be removed
    from access by PCIe driver and replace by another register
    
    Signed-off-by: Tin Tran <tin.tran.xk@renesas.com>

--- drivers/pci/controller/dwc/pcie-renesas-ep.c
+++ drivers/pci/controller/dwc/pcie-renesas-ep.c
@@ -46,12 +46,9 @@
 #define  SMLH_LINK_UP		BIT(7)
 #define  RDLH_LINK_UP		BIT(6)
 
-/* PCIEC PHY */
-#define RCVRCTRLP0		0x0040
-#define  PHY0_RX1_TERM_ACDC	BIT(14)
-#define  PHY0_RX0_TERM_ACDC	BIT(13)
-
-#define REFCLKCTRLP0		0x0B8
+/* PORT LOGIC */
+#define PRTLGC5			0x0714
+#define INSERT_LANE_SKEW	BIT(6)
 
 /* Shadow regs */
 #define BAR0MASKF0		0x10
@@ -91,17 +88,6 @@ static u32 renesas_pcie_readl(struct renesas_pcie_ep *pcie, u32 reg)
 static void renesas_pcie_writel(struct renesas_pcie_ep *pcie, u32 reg, u32 val)
 {
 	writel(val, pcie->base + reg);
-}
-
-static u32 renesas_pcie_phy_readl(struct renesas_pcie_ep *pcie, u32 reg)
-{
-	return readl(pcie->phy_base + reg);
-}
-
-static void renesas_pcie_phy_writel(struct renesas_pcie_ep *pcie, u32 reg,
-				    u32 val)
-{
-	writel(val, pcie->phy_base + reg);
 }
 
 static void renesas_pcie_ltssm_enable(struct renesas_pcie_ep *pcie,
@@ -273,15 +259,12 @@ static void renesas_pcie_init_ep(struct renesas_pcie_ep *pcie)
 	}
 	dw_pcie_writel_dbi(pci, EXPCAP3, val);
 
+	val = dw_pcie_readl_dbi(pci, PRTLGC5);
+	val |= INSERT_LANE_SKEW;
+	dw_pcie_writel_dbi(pci, PRTLGC5, val);
+
 	dw_pcie_dbi_ro_wr_dis(pci);
 
-	val = renesas_pcie_phy_readl(pcie, RCVRCTRLP0);
-	val |= PHY0_RX0_TERM_ACDC | PHY0_RX1_TERM_ACDC;
-	renesas_pcie_phy_writel(pcie, RCVRCTRLP0, val);
-
-	val = renesas_pcie_phy_readl(pcie, REFCLKCTRLP0);
-	val |= BIT(10) | BIT(9);
-	renesas_pcie_phy_writel(pcie, REFCLKCTRLP0, val);
 }
 
 static int renesas_pcie_ep_enable(struct renesas_pcie_ep *pcie)
