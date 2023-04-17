73be8101dbbe82fa35296cad703451bd85ec1973
Linh Phung <linh.phung.jy@renesas.com>
2023-04-18 04:13:17 +0700

    dt-bindings: media: Add specific CSI-2 information for R-Car V4H
    Setting of CSI-2 Static Register on R-CarV4H depends on board wiring.
    This patch adds available connection-orders of R-CarV4H and MAX96712.
    
    Signed-off-by: Linh Phung <linh.phung.jy@renesas.com>

---
+++ include/dt-bindings/media/r8a779g0-csi2.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2021 Renesas Electronics Corp.
+ */
+#ifndef __DT_BINDINGS_MEDIA_R8A779G0_VIN_H__
+#define __DT_BINDINGS_MEDIA_R8A779G0_VIN_H__
+
+/* R-Car V4H (r8A779g0) RX order */
+
+#define ABC		0x0
+#define CBA		0x1
+#define ACB		0x2
+#define CAB		0x3
+#define BAC		0x4
+#define BCA		0x5
+
+#endif /* __DT_BINDINGS_MEDIA_R8A779G0_VIN_H__ */
