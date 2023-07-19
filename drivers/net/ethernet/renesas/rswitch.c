d5aa6c57ea800c5630ad1edc196908a6bdac3b6f
Tam Nguyen <tam.nguyen.xa@renesas.com>
2023-07-20 00:30:55 +0700

    net: ethernet: renesas: rswitch: Update SerDes initialization sequence
    When execute loopback test, the hardware team was detecting frames
    are not being sent or received successfully.
    To resolve the failure phenomenon, the new step tx_reset and
    rx_reset was added after completing the initialization sequence.
    
    Signed-off-by: Tam Nguyen <tam.nguyen.xa@renesas.com>

--- drivers/net/ethernet/renesas/rswitch.c
+++ drivers/net/ethernet/renesas/rswitch.c
@@ -825,6 +825,7 @@ enum rswitch_gwca_mode {
 
 #define FWPBFCSDC(j, i)         (FWPBFCSDC00 + (i) * 0x10 + (j) * 0x04)
 
+#define RSWITCH_SERDES_LOCAL_OFFSET             0x2600
 #define RSWITCH_SERDES_OFFSET                   0x0400
 #define RSWITCH_SERDES_BANK_SELECT              0x03fc
 
@@ -857,6 +858,9 @@ enum rswitch_gwca_mode {
 #define VR_XS_PMA_MP_12G_MPLLB_CTRL1            0x01d4
 #define VR_XS_PMA_MP_12G_16G_MPLLB_CTRL2        0x01d8
 #define VR_XS_PMA_MP_12G_MPLLB_CTRL3            0x01e0
+#define VR_XS_PMA_MP_12G_16G_25G_TX_GENCTRL0    0x00c0
+#define VR_XS_PMA_MP_12G_16G_25G_TX_STS         0x0100
+#define VR_XS_PMA_MP_12G_16G_25G_RX_STS         0x0180
 
 #define BANK_300                                0x0300
 #define SR_XS_PCS_CTRL1                         0x0000
@@ -1680,6 +1684,7 @@ static int rswitch_serdes_common_init(struct rswitch_etha *etha)
 static int rswitch_serdes_chan_init(struct rswitch_etha *etha)
 {
 	int ret;
+	u32 val;
 
 	/* Set channel settings*/
 	ret = rswitch_serdes_chan_setting(etha);
@@ -1693,6 +1698,42 @@ static int rswitch_serdes_chan_init(struct rswitch_etha *etha)
 
 	rswitch_serdes_write32(etha->serdes_addr, VR_XS_PCS_SFTY_UE_INTRO, BANK_380, 0);
 	rswitch_serdes_write32(etha->serdes_addr, VR_XS_PCS_SFTY_DISABLE, BANK_380, 0);
+
+	val = rswitch_serdes_read32(etha->serdes_addr, VR_XS_PMA_MP_12G_16G_25G_TX_GENCTRL0,
+				    BANK_180);
+	rswitch_serdes_write32(etha->serdes_addr, VR_XS_PMA_MP_12G_16G_25G_TX_GENCTRL0,
+			       BANK_180, val | BIT(8));
+
+	ret = rswitch_serdes_reg_wait(etha->serdes_addr, VR_XS_PMA_MP_12G_16G_25G_TX_STS,
+				      BANK_180, BIT(0), 1);
+	if (ret)
+		return ret;
+
+	rswitch_serdes_write32(etha->serdes_addr, VR_XS_PMA_MP_12G_16G_25G_TX_GENCTRL0,
+			       BANK_180, val &= ~BIT(8));
+
+	ret = rswitch_serdes_reg_wait(etha->serdes_addr, VR_XS_PMA_MP_12G_16G_25G_TX_STS,
+				      BANK_180, BIT(0), 0);
+	if (ret)
+		return ret;
+
+	val = rswitch_serdes_read32(etha->serdes_addr, VR_XS_PMA_MP_12G_16G_25G_RX_GENCTRL1,
+				    BANK_180);
+	rswitch_serdes_write32(etha->serdes_addr, VR_XS_PMA_MP_12G_16G_25G_RX_GENCTRL1,
+			       BANK_180, val | BIT(4));
+
+	ret = rswitch_serdes_reg_wait(etha->serdes_addr, VR_XS_PMA_MP_12G_16G_25G_RX_STS,
+				      BANK_180, BIT(0), 1);
+	if (ret)
+		return ret;
+
+	rswitch_serdes_write32(etha->serdes_addr, VR_XS_PMA_MP_12G_16G_25G_RX_GENCTRL1,
+			       BANK_180, val &= ~BIT(4));
+
+	ret = rswitch_serdes_reg_wait(etha->serdes_addr, VR_XS_PMA_MP_12G_16G_25G_RX_STS,
+				      BANK_180, BIT(0), 0);
+	if (ret)
+		return ret;
 
 	/* Check Link up restart */
 	return rswitch_serdes_monitor_linkup(etha);
@@ -2275,6 +2316,8 @@ static void rswitch_reset(struct rswitch_private *priv)
 	if (!parallel_mode) {
 		rs_write32(RRC_RR, priv->addr + RRC);
 		rs_write32(RRC_RR_CLR, priv->addr + RRC);
+
+		rs_write32(0, priv->serdes_addr + RSWITCH_SERDES_LOCAL_OFFSET);
 
 		reset_control_assert(priv->sd_rst);
 		mdelay(1);
