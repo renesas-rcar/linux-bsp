a43f31c2e82fb8098826e9f25cda758f1af45781
Duy Nguyen <duy.nguyen.rh@renesas.com>
2023-08-18 14:26:50 +0700

    soc: renesas: rcar-rst: Add support for R-Car V4M
    Add support for R-Car V4M (R8A779H0) to the R-Car RST driver
    However, the code is a prototype. It needs to be reviewed.
    
    Signed-off-by: Duy Nguyen <duy.nguyen.rh@renesas.com>

--- drivers/soc/renesas/rcar-rst.c
+++ drivers/soc/renesas/rcar-rst.c
@@ -88,6 +88,7 @@ static const struct of_device_id rcar_rst_matches[] __initconst = {
 	/* R-Car Gen4 */
 	{ .compatible = "renesas,r8a779g0-rst", .data = &rcar_rst_gen4 },
 	{ .compatible = "renesas,r8a779f0-rst", .data = &rcar_rst_gen4 },
+	{ .compatible = "renesas,r8a779h0-rst", .data = &rcar_rst_gen4 },
 	{ /* sentinel */ }
 };
 
