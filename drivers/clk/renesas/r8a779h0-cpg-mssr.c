e8db9a1ba776cf791486cd078b4680a60f6bfe4a
Cong Dang <cong.dang.xn@renesas.com>
2023-08-29 01:24:07 +0700

    clk: renesas: r8a779h0: Add PFC/GPIO clocks
    Add the module clocks used by the Pin Function Controller (PFC) and
    General Purpose Input/Output (GPIO) blocks on the Renesas R-Car V4M
    (R8A779H0) SoC.
    
    Signed-off-by: Cong Dang <cong.dang.xn@renesas.com>

--- drivers/clk/renesas/r8a779h0-cpg-mssr.c
+++ drivers/clk/renesas/r8a779h0-cpg-mssr.c
@@ -182,6 +182,9 @@ static const struct mssr_mod_clk r8a779h0_mod_clks[] = {
 	DEF_MOD("i2c2",		520,	R8A779H0_CLK_S0D6_PER),
 	DEF_MOD("i2c3",		521,	R8A779H0_CLK_S0D6_PER),
 	DEF_MOD("wdt1:wdt0",	907,	R8A779H0_CLK_R),
+	DEF_MOD("pfc0",		915,	R8A779H0_CLK_CL16M),
+	DEF_MOD("pfc1",		916,	R8A779H0_CLK_CL16M),
+	DEF_MOD("pfc2",		917,	R8A779H0_CLK_CL16M),
 };
 
 /*
