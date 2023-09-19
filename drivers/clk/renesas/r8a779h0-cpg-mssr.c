11a603bc1b9bf3dc4380287a0400eeaa6c1e06fa
Cong Dang <cong.dang.xn@renesas.com>
2023-09-19 20:25:22 +0700

    clk: renesas: r8a779h0: Add TPU clock
    Add the module clock used by the 16-Bit Timer Pulse Unit (TPU) on the
    Renesas R-Car V4M (R8A779H0) SoC.
    
    Signed-off-by: Cong Dang <cong.dang.xn@renesas.com>

--- drivers/clk/renesas/r8a779h0-cpg-mssr.c
+++ drivers/clk/renesas/r8a779h0-cpg-mssr.c
@@ -196,6 +196,7 @@ static const struct mssr_mod_clk r8a779h0_mod_clks[] = {
 	DEF_MOD("sdhi",		706,	R8A779H0_CLK_SD0),
 	DEF_MOD("sydm0",	709,	R8A779H0_CLK_S0D6_PER),
 	DEF_MOD("sydm1",	710,	R8A779H0_CLK_S0D6_PER),
+	DEF_MOD("tpu0",		718,	R8A779H0_CLK_SASYNCPERD4),
 	DEF_MOD("wdt1:wdt0",	907,	R8A779H0_CLK_R),
 	DEF_MOD("pfc0",		915,	R8A779H0_CLK_CL16M),
 	DEF_MOD("pfc1",		916,	R8A779H0_CLK_CL16M),
