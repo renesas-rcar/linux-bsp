478beadd776003c54d96332378241d6a8783a013
Cong Dang <cong.dang.xn@renesas.com>
2023-09-27 01:47:58 +0700

    clk: renesas: r8a779h0: Add CANFD clock
    Add the CANFD module clock on R-Car V4M (R8A779H0) SoC.
    
    Signed-off-by: Cong Dang <cong.dang.xn@renesas.com>

--- drivers/clk/renesas/r8a779h0-cpg-mssr.c
+++ drivers/clk/renesas/r8a779h0-cpg-mssr.c
@@ -174,6 +174,7 @@ static const struct mssr_mod_clk r8a779h0_mod_clks[] = {
 	DEF_MOD("avb0-rgmii0",	211,	R8A779H0_CLK_S0D8_HSC),
 	DEF_MOD("avb1-rgmii1",	212,	R8A779H0_CLK_S0D8_HSC),
 	DEF_MOD("avb2-rgmii2",	213,	R8A779H0_CLK_S0D8_HSC),
+	DEF_MOD("can-fd",	328,	R8A779H0_CLK_CANFD),
 	DEF_MOD("csitop0",	331,	R8A779H0_CLK_CSI),
 	DEF_MOD("csitop1",	400,	R8A779H0_CLK_CSI),
 	DEF_MOD("hscif0",	514,	R8A779H0_CLK_SASYNCPERD1),
