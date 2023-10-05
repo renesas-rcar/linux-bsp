6649fb508268382ff9865a2d570f5b4b1a990dc3
Thanh Le <thanh.le.xv@renesas.com>
2023-10-05 17:35:36 +0700

    dmaengine: rcar-dmac: Add support for X5H SoC
    This patch supports X5H SoC.
    
    Signed-off-by: Thanh Le <thanh.le.xv@renesas.com>

--- drivers/dma/sh/rcar-dmac.c
+++ drivers/dma/sh/rcar-dmac.c
@@ -2124,6 +2124,9 @@ static const struct of_device_id rcar_dmac_of_ids[] = {
 	}, {
 		.compatible = "renesas,dmac-r8a779g0",
 		.data = &rcar_gen4_dmac_data,
+	}, {
+                .compatible = "renesas,rcar-gen5-dmac",
+                .data = &rcar_gen4_dmac_data,
 	},
 	{ /* Sentinel */ }
 };
