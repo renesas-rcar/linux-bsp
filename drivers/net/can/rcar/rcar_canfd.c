530d6950eaa9f453f5dc0763c8cccea90e169210
Duy Nguyen <duy.nguyen.rh@renesas.com>
2023-10-05 08:45:27 +0700

    can: rcar_canfd: Add support for X5H SoC
    This patch supports X5H.
    
    Signed-off-by: Duy Nguyen <duy.nguyen.rh@renesas.com>
    Signed-off-by: Nghia Nguyen <nghia.nguyen.jg@renesas.com>
    Signed-off-by: Minh Le <minh.le.aj@renesas.com>

--- drivers/net/can/rcar/rcar_canfd.c
+++ drivers/net/can/rcar/rcar_canfd.c
@@ -735,6 +735,23 @@ static const struct rcar_canfd_hw_info rcar_gen4_hw_info = {
 	.external_clk = 1,
 };
 
+static const struct rcar_canfd_hw_info rcar_gen5_hw_info = {
+	.nom_bittiming = &rcar_canfd_gen4_nom_bittiming_const,
+	.data_bittiming = &rcar_canfd_gen4_data_bittiming_const,
+	.tdc_const = &rcar_canfd_gen4_tdc_const,
+	.regs = &rcar_gen4_regs,
+	.sh = &rcar_gen4_shift_data,
+	.rnc_field_width = 16,
+	.max_aflpn = 127,
+	.max_cftml = 31,
+	.max_channels = 8,
+	.postdiv = 1,
+	.shared_global_irqs = 1,
+	.ch_interface_mode = 1,
+	.shared_can_regs = 1,
+	.external_clk = 1,
+};
+
 static const struct rcar_canfd_hw_info rzg2l_hw_info = {
 	.nom_bittiming = &rcar_canfd_gen3_nom_bittiming_const,
 	.data_bittiming = &rcar_canfd_gen3_data_bittiming_const,
@@ -771,6 +788,11 @@ static const struct rcar_canfd_hw_info r9a09g047_hw_info = {
 
 /* Helper functions */
 static inline bool is_gen4(struct rcar_canfd_global *gpriv)
+{
+	return gpriv->info == &rcar_gen4_hw_info;
+}
+
+static inline bool is_gen5(struct rcar_canfd_global *gpriv)
 {
 	return gpriv->info == &rcar_gen4_hw_info;
 }
@@ -915,7 +937,7 @@ static int rcar_canfd_reset_controller(struct rcar_canfd_global *gpriv)
 	rcar_canfd_write(gpriv->base, RCANFD_GERFL, 0x0);
 
 	/* Set the controller into appropriate mode */
-	if (!is_gen4(gpriv)) {
+	if (!is_gen4(gpriv) || !is_gen5(gpriv)) {
 		if (gpriv->fdmode)
 			rcar_canfd_set_bit(gpriv->base, RCANFD_GRMCFG,
 					   RCANFD_GRMCFG_RCMC);
@@ -943,7 +965,7 @@ static int rcar_canfd_reset_controller(struct rcar_canfd_global *gpriv)
 		}
 
 		/* Set the controller into appropriate mode */
-		if (is_gen4(gpriv)) {
+		if (is_gen4(gpriv) || is_gen5(gpriv)) {
 			/* Do not set CLOE and FDOE simultaneously */
 			if (!gpriv->fdmode) {
 				rcar_canfd_clear_bit(gpriv->base,
@@ -2345,6 +2367,7 @@ static const __maybe_unused struct of_device_id rcar_canfd_of_table[] = {
 	{ .compatible = "renesas,rcar-gen3-canfd", .data = &rcar_gen3_hw_info },
 	{ .compatible = "renesas,rcar-gen4-canfd", .data = &rcar_gen4_hw_info },
 	{ .compatible = "renesas,rzg2l-canfd", .data = &rzg2l_hw_info },
+	{ .compatible = "renesas,r8a78000-canfd", .data = &rcar_gen5_hw_info },
 	{ }
 };
 
