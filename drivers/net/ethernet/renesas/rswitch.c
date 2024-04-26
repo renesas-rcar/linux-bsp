4cdb905702f6a8f3000557aa9ba4f6cfcd44956c
Nikita Yushchenko <nikita.yoush@cogentembedded.com>
2024-04-26 16:47:22 +0500

    net: ethernet: renesas: rswitch: handle phy ioctls and ethtool ops
    - pass unknown netdev ioctls to phy_mii_ioctl(), thus supporting phy
      ioctls on sockets,
    
    - add default handlers for [sg]et_link_ksettings ethtool ops, thus
      allowing phy configuration over ethtool.
    
    Signed-off-by: Nikita Yushchenko <nikita.yoush@cogentembedded.com>

--- drivers/net/ethernet/renesas/rswitch.c
+++ drivers/net/ethernet/renesas/rswitch.c
@@ -2466,10 +2466,11 @@ static int rswitch_do_ioctl(struct net_device *ndev, struct ifreq *req, int cmd)
 	case SIOCSHWTSTAMP:
 		return rswitch_hwstamp_set(ndev, req);
 	default:
-		break;
+		if (ndev->phydev)
+			return phy_mii_ioctl(ndev->phydev, req, cmd);
 	}
 
-	return 0;
+	return -EOPNOTSUPP;
 }
 
 static const struct net_device_ops rswitch_netdev_ops = {
@@ -2501,6 +2502,8 @@ static int rswitch_get_ts_info(struct net_device *ndev, struct ethtool_ts_info *
 
 static const struct ethtool_ops rswitch_ethtool_ops = {
 	.get_ts_info = rswitch_get_ts_info,
+	.get_link_ksettings = phy_ethtool_get_link_ksettings,
+	.set_link_ksettings = phy_ethtool_set_link_ksettings,
 };
 
 static const struct of_device_id renesas_eth_sw_of_table[] = {
