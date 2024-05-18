58fc4abae7c129da9351a76feced1130d030c093
Minh Le <minh.le.aj@renesas.com>
2024-05-18 19:36:10 +0700

    i2c: rcar: improve WA to prevent rcar condition
    This patch add WA to prevent race condition in i2c driver.
    To keep the mainline work correctly, adding an additional check
    for the suspend flag is necessary.
    
    Signed-off-by: Minh Le <minh.le.aj@renesas.com>

--- drivers/i2c/busses/i2c-rcar.c
+++ drivers/i2c/busses/i2c-rcar.c
@@ -216,10 +216,12 @@ static struct i2c_bus_recovery_info rcar_i2c_bri = {
 };
 static void rcar_i2c_init(struct rcar_i2c_priv *priv)
 {
-	/* reset master mode */
-	rcar_i2c_write(priv, ICMIER, 0);
-	rcar_i2c_write(priv, ICMCR, MDBS);
-	rcar_i2c_write(priv, ICMSR, 0);
+	if (!priv->suspended) {
+		/* reset master mode */
+		rcar_i2c_write(priv, ICMIER, 0);
+		rcar_i2c_write(priv, ICMCR, MDBS);
+		rcar_i2c_write(priv, ICMSR, 0);
+	}
 	/* start clock */
 	rcar_i2c_write(priv, ICCCR, priv->icccr);
 
@@ -1196,6 +1198,7 @@ static int rcar_i2c_remove(struct platform_device *pdev)
 		i2c_free_slave_host_notify_device(priv->host_notify_client);
 	i2c_del_adapter(&priv->adap);
 	rcar_i2c_release_dma(priv);
+	rcar_i2c_write(priv, ICMIER, 0);
 	if (priv->flags & ID_P_PM_BLOCKED)
 		pm_runtime_put(dev);
 	pm_runtime_disable(dev);
