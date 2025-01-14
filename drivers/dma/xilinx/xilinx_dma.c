0427df1be9620a127f2615b3d4bb74010841de53
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2025-01-14 20:10:20 +0100

    dmaengine: Use str_enable_disable-like helpers
    Replace ternary (condition ? "enable" : "disable") syntax with helpers
    from string_choices.h because:
    1. Simple function call with one argument is easier to read.  Ternary
       operator has three arguments and with wrapping might lead to quite
       long code.
    2. Is slightly shorter thus also easier to read.
    3. It brings uniformity in the text - same string.
    4. Allows deduping by the linker, which results in a smaller binary
       file.
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Reviewed-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org> #dw-edma
    Reviewed-by: Jernej Skrabec <jernej.skrabec@gmail.com>
    Link: https://lore.kernel.org/r/20250114191021.854080-1-krzysztof.kozlowski@linaro.org
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    (cherry picked from commit 8e63891831f3904047d7ad8078ff52dd454b6975)
    Signed-off-by: Phong Hoang <phong.hoang.wz@renesas.com>

--- drivers/dma/xilinx/xilinx_dma.c
+++ drivers/dma/xilinx/xilinx_dma.c
@@ -46,6 +46,7 @@
 #include <linux/of_platform.h>
 #include <linux/of_irq.h>
 #include <linux/slab.h>
+#include <linux/string_choices.h>
 #include <linux/clk.h>
 #include <linux/io-64-nonatomic-lo-hi.h>
 
@@ -2891,7 +2892,7 @@ static int xilinx_dma_chan_probe(struct xilinx_dma_device *xdev,
 			    XILINX_DMA_DMASR_SG_MASK)
 			chan->has_sg = true;
 		dev_dbg(chan->dev, "ch %d: SG %s\n", chan->id,
-			chan->has_sg ? "enabled" : "disabled");
+			str_enabled_disabled(chan->has_sg));
 	}
 
 	/* Initialize the tasklet */
