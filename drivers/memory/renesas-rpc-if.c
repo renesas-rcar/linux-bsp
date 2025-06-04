3c82115c22b2e3f8ab19b7cbba5a43c98c228881
Tuyen Dang <tuyen.dang.xa@renesas.com>
2025-06-04 23:35:36 +0700

    memory: renesas-rpc-if: Modify DDR Timing and burst read operation
    Modify DDR Timing and burst read operation.
    
    Signed-off-by: Tuyen Dang <tuyen.dang.xa@renesas.com>

--- drivers/memory/renesas-rpc-if.c
+++ drivers/memory/renesas-rpc-if.c
@@ -360,7 +360,7 @@ int rpcif_hw_init(struct device *dev, bool hyperflash)
 			   RPCIF_PHYCNT_STRTIM(rpc->info->strtim));
 
 	regmap_update_bits(rpc->regmap, RPCIF_PHYOFFSET1, RPCIF_PHYOFFSET1_DDRTMG(3),
-			   RPCIF_PHYOFFSET1_DDRTMG(3));
+			   RPCIF_PHYOFFSET1_DDRTMG(hyperflash ? 2 : 3));
 	regmap_update_bits(rpc->regmap, RPCIF_PHYOFFSET2, RPCIF_PHYOFFSET2_OCTTMG(7),
 			   RPCIF_PHYOFFSET2_OCTTMG(4));
 
@@ -690,8 +690,7 @@ ssize_t rpcif_dirmap_read(struct device *dev, u64 offs, size_t len, void *buf)
 		return ret;
 
 	regmap_update_bits(rpc->regmap, RPCIF_CMNCR, RPCIF_CMNCR_MD, 0);
-	regmap_write(rpc->regmap, RPCIF_DRCR,
-		     RPCIF_DRCR_RBURST(32) | RPCIF_DRCR_RBE);
+	regmap_write(rpc->regmap, RPCIF_DRCR, 0);
 	regmap_write(rpc->regmap, RPCIF_DRCMR, rpc->command);
 	regmap_write(rpc->regmap, RPCIF_DREAR,
 		     RPCIF_DREAR_EAV(offs >> 25) | RPCIF_DREAR_EAC(1));
