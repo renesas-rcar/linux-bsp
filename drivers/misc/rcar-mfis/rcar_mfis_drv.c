6be0a8734ddb7bd45c24ea1306dd1f6394ae2ed6
Phong Hoang <phong.hoang.wz@renesas.com>
2025-06-09 16:40:14 +0700

    misc: rcar-mfis: Refactor channel initialization
    Refactor channel initialization to avoid resource conflicts
    between RPMsg kernel-space and user-space.
    
    Signed-off-by: Phong Hoang <phong.hoang.wz@renesas.com>

--- drivers/misc/rcar-mfis/rcar_mfis_drv.c
+++ drivers/misc/rcar-mfis/rcar_mfis_drv.c
@@ -18,10 +18,10 @@
 #include "rcar_mfis_drv.h"
 #include <misc/rcar-mfis/rcar_mfis_public.h>
 
-#define IICR(n) (0x0000 + (n) * 0x1000)
-#define EICR(n) (0x0004 + (n) * 0x1000)
-#define IMBR(n) (0x0040 + (n) * 0x1000)
-#define EMBR(n) (0x0044 + (n) * 0x1000)
+#define IICR	(0x0000)
+#define EICR	(0x0004)
+#define IMBR	(0x0040)
+#define EMBR	(0x0044)
 
 static struct rcar_mfis_priv *rcmfis_priv;
 
@@ -30,18 +30,17 @@ static irqreturn_t mfis_irq_handler(int irq, void *data)
 	u32 value = 0;
 	struct rcar_mfis_msg msg;
 	struct rcar_mfis_ch *rcar_mfis_ch = (struct rcar_mfis_ch *)data;
-	unsigned int ch = rcar_mfis_ch->id;
 
-	value = rcar_mfis_reg_read(rcmfis_priv, IICR(ch));
+	value = rcar_mfis_reg_read(rcar_mfis_ch, IICR);
 	if (value & 0x1) {
-		msg.mbr = rcar_mfis_reg_read(rcmfis_priv, IMBR(ch));
+		msg.mbr = rcar_mfis_reg_read(rcar_mfis_ch, IMBR);
 		msg.icr = value >> 1;
 
 		atomic_notifier_call_chain(&rcar_mfis_ch->notifier_head, msg.icr,
 					   rcar_mfis_ch->notifier_data);
 
 		/* clear interrupt flag */
-		rcar_mfis_reg_write(rcmfis_priv, IICR(ch), value & (~0x1));
+		rcar_mfis_reg_write(rcar_mfis_ch, IICR, value & (~0x1));
 
 		return IRQ_HANDLED;
 	}
@@ -53,6 +52,9 @@ static struct rcar_mfis_ch *rcar_mfis_channel_get(unsigned int channel)
 {
 	struct rcar_mfis_ch *rcar_mfis_ch;
 	int i;
+
+	if (!rcmfis_priv)
+		return NULL;
 
 	for (i = 0; i < NUM_MFIS_CHANNELS; i++) {
 		if (rcmfis_priv->channels[i].initialized &&
@@ -78,12 +80,12 @@ int rcar_mfis_trigger_interrupt(int channel, struct rcar_mfis_msg msg)
 		return -EINVAL;
 
 	/* Check whether the remote proccessor is still processing a previous interrupt */
-	icr = rcar_mfis_reg_read(rcmfis_priv, EICR(channel));
+	icr = rcar_mfis_reg_read(rcar_mfis_ch, EICR);
 	if (icr & 0x1)
 		return -EBUSY;
 
-	rcar_mfis_reg_write(rcmfis_priv, EMBR(channel), msg.mbr);
-	rcar_mfis_reg_write(rcmfis_priv, EICR(channel), (msg.icr << 1) | 1);
+	rcar_mfis_reg_write(rcar_mfis_ch, EMBR, msg.mbr);
+	rcar_mfis_reg_write(rcar_mfis_ch, EICR, (msg.icr << 1) | 1);
 
 	return ret;
 }
@@ -128,12 +130,11 @@ EXPORT_SYMBOL(rcar_mfis_unregister_notifier);
 static int rcar_mfis_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
-	u32 __iomem *mmio_base, *unlock;
+	u32 __iomem *unlock;
 	struct resource *res;
 	u32 num_mfis_channels;
 	u32 value;
 	int ret, i;
-	int irq;
 
 	num_mfis_channels = of_property_count_elems_of_size(dev->of_node, "renesas,mfis-channels",
 							    sizeof(u32));
@@ -149,21 +150,6 @@ static int rcar_mfis_probe(struct platform_device *pdev)
 
 	rcmfis_priv->pdev = pdev;
 
-	/* Map MFIS registers */
-	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, "io_base");
-	if (!res) {
-		dev_err(dev, "Failed to get MFIS IO base.\n");
-		ret = -EINVAL;
-		goto free_mfis_dev;
-	}
-
-	mmio_base = (u32 __iomem *)devm_ioremap(dev, res->start, resource_size(res));
-	if (IS_ERR(mmio_base)) {
-		dev_err(dev, "Failed to remap MFIS registers.\n");
-		ret = PTR_ERR(mmio_base);
-		goto free_mfis_dev;
-	}
-
 	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, "unlock_reg");
 	if (!res) {
 		dev_err(dev, "Failed to get write protection registger.\n");
@@ -176,10 +162,10 @@ static int rcar_mfis_probe(struct platform_device *pdev)
 	iowrite32(0xACC00001, unlock);
 	iounmap(unlock);
 
-	rcmfis_priv->mmio_base = mmio_base;
-
 	for (i = 0; i < num_mfis_channels; i++) {
 		struct rcar_mfis_ch *mfis_ch;
+		char *pdev_chname, *pdev_irqname;
+		int irq;
 
 		ret = of_property_read_u32_index(dev->of_node, "renesas,mfis-channels", i, &value);
 		if (ret)
@@ -197,6 +183,27 @@ static int rcar_mfis_probe(struct platform_device *pdev)
 
 		ATOMIC_INIT_NOTIFIER_HEAD(&mfis_ch->notifier_head);
 
+		pdev_chname = kasprintf(GFP_KERNEL, "ch%u", mfis_ch->id);
+		if (!pdev_chname) {
+			dev_err(dev, "failed to create channel %d name. Skipping.\n", mfis_ch->id);
+			continue;
+		}
+
+		/* Get channel base address */
+		res = platform_get_resource_byname(pdev, IORESOURCE_MEM, pdev_chname);
+		if (!res) {
+			dev_err(dev, "missing base address for channel %d. Skipping.\n",
+				mfis_ch->id);
+			continue;
+		}
+
+		mfis_ch->base = devm_ioremap_resource(dev, res);
+		if (!mfis_ch->base) {
+			dev_err(dev, "failed to map IO memory for channel %d\n. Skipping.\n",
+				mfis_ch->id);
+			continue;
+		}
+
 		/* Get IRQ resource */
 		irq = platform_get_irq(pdev, mfis_ch->id);
 		if (!irq) {
@@ -204,8 +211,15 @@ static int rcar_mfis_probe(struct platform_device *pdev)
 			continue;
 		}
 
+		pdev_irqname = devm_kasprintf(dev, GFP_KERNEL, "mfis_ch%u", mfis_ch->id);
+		if (!pdev_irqname) {
+			dev_err(dev, "failed to create irqname of channel %d name. Skipping.\n",
+				mfis_ch->id);
+			continue;
+		}
+
 		ret = devm_request_irq(dev, irq, mfis_irq_handler, IRQF_SHARED,
-				       dev_name(dev), mfis_ch);
+				       pdev_irqname, mfis_ch);
 		if (ret < 0) {
 			dev_err(dev, "failed to request IRQ for channel %d. Skipping.\n",
 				mfis_ch->id);
--- drivers/misc/rcar-mfis/rcar_mfis_drv.h
+++ drivers/misc/rcar-mfis/rcar_mfis_drv.h
@@ -10,6 +10,7 @@
 struct rcar_mfis_ch {
 	unsigned int id;
 	int initialized;
+	void __iomem *base;
 	struct atomic_notifier_head notifier_head;
 	void *notifier_data;
 };
@@ -18,18 +19,17 @@ struct rcar_mfis_ch {
 
 struct rcar_mfis_priv {
 	struct platform_device *pdev;
-	void __iomem *mmio_base;
 	struct rcar_mfis_ch channels[NUM_MFIS_CHANNELS];
 };
 
-static inline u32 rcar_mfis_reg_read(struct rcar_mfis_priv *mfis_priv, u32 reg)
+static inline u32 rcar_mfis_reg_read(struct rcar_mfis_ch *chan, u32 reg)
 {
-	return ioread32(mfis_priv->mmio_base + reg);
+	return ioread32(chan->base + reg);
 }
 
-static inline void rcar_mfis_reg_write(struct rcar_mfis_priv *mfis_priv, u32 reg, u32 data)
+static inline void rcar_mfis_reg_write(struct rcar_mfis_ch *chan, u32 reg, u32 data)
 {
-	iowrite32(data, mfis_priv->mmio_base + reg);
+	iowrite32(data, chan->base + reg);
 }
 
 #endif
