dfa04a0628dc605bae5d81ea867fc1676e10ffeb
Nikita Yushchenko <nikita.yoush@cogentembedded.com>
2025-06-27 23:20:37 +0700

    net: renesas: rswitch: avoid use-after-put for a device tree node
    The device tree node saved in the rswitch_device structure is used at
    several driver locations. So passing this node to of_node_put() after
    the first use is wrong.
    
    Move of_node_put() for this node to exit paths.
    
    Fixes: b46f1e579329 ("net: renesas: rswitch: Simplify struct phy * handling")
    Signed-off-by: Nikita Yushchenko <nikita.yoush@cogentembedded.com>
    Reviewed-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Link: https://patch.msgid.link/20241208095004.69468-5-nikita.yoush@cogentembedded.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Tam Nguyen <tam.nguyen.xa@renesa.com>
    (cherry picked from commit 66b7e9f8)

--- drivers/net/ethernet/renesas/rswitch3.c
+++ drivers/net/ethernet/renesas/rswitch3.c
@@ -1866,7 +1866,6 @@ static int rsw3_device_alloc(struct rsw3_private *priv, unsigned int index)
 	rdev->np_port = rsw3_get_port_node(rdev);
 	rdev->disabled = !rdev->np_port;
 	err = of_get_ethdev_address(rdev->np_port, ndev);
-	of_node_put(rdev->np_port);
 
 	if (err) {
 		if (is_valid_ether_addr(rdev->etha->mac_addr))
@@ -1896,6 +1895,7 @@ out_txdmac:
 
 out_rxdmac:
 out_get_params:
+	of_node_put(rdev->np_port);
 	netif_napi_del(&rdev->napi);
 	free_netdev(ndev);
 
@@ -1912,6 +1912,7 @@ static void rsw3_device_free(struct rsw3_private *priv, unsigned int index)
 		rsw3_rxdmac_free(ndev);
 	}
 
+	of_node_put(rdev->np_port);
 	netif_napi_del(&rdev->napi);
 	free_netdev(ndev);
 }
