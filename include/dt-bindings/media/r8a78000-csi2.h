b19186d95d6630f69ad8adc89bd8f34f32da8e40
Linh Phung <linh.phung.jy@renesas.com>
2025-07-04 19:02:19 +0700

    dt-bindings: media: Define pin-swap order macro for R-Car X5H
    Define pin-swap order macro for R-Car X5H.
    
    Signed-off-by: Linh Phung <linh.phung.jy@renesas.com>

---
+++ include/dt-bindings/media/r8a78000-csi2.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause */
+/*
+ * Copyright (C) 2021 Renesas Electronics Corp.
+ */
+#ifndef __DT_BINDINGS_MEDIA_R8A78000_VIN_H__
+#define __DT_BINDINGS_MEDIA_R8A78000_VIN_H__
+
+/* R-Car V4H (r8A779g0) RX order */
+
+#define ABC		0x0
+#define CBA		0x1
+#define ACB		0x2
+#define BCA		0x3
+#define BAC		0x4
+#define CAB		0x5
+
+#endif /* __DT_BINDINGS_MEDIA_R8A78000_VIN_H__ */
