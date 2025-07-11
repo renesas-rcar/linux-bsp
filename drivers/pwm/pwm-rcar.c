c2feed933ae615e51e1ce80581374d715fedb265
Khanh Le <khanh.le.xr@renesas.com>
2025-07-11 16:36:28 +0700

    pwm: rcar: Enable clock for PWM
    Enable clock for PWM.
    
    Signed-off-by: Khanh Le <khanh.le.xr@renesas.com>

--- drivers/pwm/pwm-rcar.c
+++ drivers/pwm/pwm-rcar.c
@@ -19,6 +19,7 @@
 #include <linux/pm_runtime.h>
 #include <linux/pwm.h>
 #include <linux/slab.h>
+#include <linux/delay.h>
 
 #define RCAR_PWM_MAX_DIVISION	24
 #define RCAR_PWM_MIN_CYCLE	2
@@ -208,6 +209,61 @@ static const struct pwm_ops rcar_pwm_ops = {
 	.owner = THIS_MODULE,
 };
 
+/* Hardcoded for enable module clock */
+#define MDLC_BASE              0xc05d0000
+#define PWM_PDID               (0)
+#define PWM_CLK_MASK(n)        GENMASK((n) + 1, n)
+#define PWM_CLK_SHIFT(n)       (n)
+
+#define MDLC_PKCPROT0          (MDLC_BASE + 0x0cf0)
+#define MDLC_PKCPROT1          (MDLC_BASE + 0x0cf4)
+
+#define _MDLC_MPDG(k)          (MDLC_BASE + 0x0200 + (k) * 4)
+#define _MDLC_MPDGS(k)         (MDLC_BASE + 0x0300 + (k) * 4)
+#define MDLC_MPIER0            (MDLC_BASE + 0x0110)
+#define MDLC_MPIMR0            (MDLC_BASE + 0x0120)
+
+#define MDLC_MPDG              _MDLC_MPDG(PWM_PDID)
+#define MDLC_MPDGS             _MDLC_MPDGS(PWM_PDID)
+
+#define MDLC_MSRES(i)          (MDLC_BASE + 0x0900 + (i) * 4)
+#define MDLC_MSRESS(i) (       MDLC_BASE + 0x0960 + (i) * 4)
+
+static void pwm_module_standby_set(u8 clk_reg_no, u8 pos, u8 mode)
+{
+       void __iomem *unlock = ioremap(MDLC_PKCPROT1, 4);
+       void __iomem *msress = ioremap(MDLC_MSRESS(clk_reg_no), 4);
+       void __iomem *msres = ioremap(MDLC_MSRES(clk_reg_no), 4);
+       u32 val;
+
+       writel(0xA5A5A501, unlock);
+
+       if ((readl(msress) & PWM_CLK_MASK(pos)) == (mode << PWM_CLK_SHIFT(pos)))
+                       goto unmap;
+
+       while ((readl(msress) & PWM_CLK_MASK(pos)) != (readl(msres) & PWM_CLK_MASK(pos)))
+                       udelay(1000);
+
+       val = readl(msres);
+       val &= ~PWM_CLK_MASK(pos);
+       val |= mode << PWM_CLK_SHIFT(pos);
+       writel(val, msres);
+
+       while ((readl(msress) & PWM_CLK_MASK(pos)) != (readl(msres) & PWM_CLK_MASK(pos)))
+                       udelay(1000);
+
+unmap:
+       iounmap(unlock);
+       iounmap(msress);
+       iounmap(msres);
+}
+
+static void pwm_module_power_run(void)
+{
+       pwm_module_standby_set(7, 0, 0x03);
+}
+//--------------------------------------------------
+
 static int rcar_pwm_probe(struct platform_device *pdev)
 {
 	struct rcar_pwm_chip *rcar_pwm;
@@ -241,6 +297,8 @@ static int rcar_pwm_probe(struct platform_device *pdev)
 		pm_runtime_disable(&pdev->dev);
 		return ret;
 	}
+
+	pwm_module_power_run();
 
 	return 0;
 }
