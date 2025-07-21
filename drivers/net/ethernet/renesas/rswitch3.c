8ae925482c262514153e888e28ff8a63354fecc7
Vinh Nguyen <vinh.nguyen.xz@renesas.com>
2025-07-21 23:01:46 +0700

    net: ethernet: renesas: rswitch: check phy get first
    In the R-Car X5H (R8A78000) SoC, module's power, clock and reset are
    controlled by the SCP via SCMI driver. During initialization, the
    MP-PHY driver can be probed before the SCMI driver. It causes an
    issue that MP-PHY driver returns -EPROBE_DEFER and come back later.
    Hence, before initializing rswitch functionality, it must check
    whether the MP-PHY driver probes OK or NG.
    
    Signed-off-by: Vinh Nguyen <vinh.nguyen.xz@renesas.com>

--- drivers/net/ethernet/renesas/rswitch3.c
+++ drivers/net/ethernet/renesas/rswitch3.c
@@ -1526,6 +1526,12 @@ static int rsw3_ether_port_init_one(struct rsw3_device *rdev)
 	int err;
 
 	if (!parallel_mode) {
+		rdev->pcs = devm_of_phy_get(&rdev->priv->pdev->dev, rdev->np_port, NULL);
+		if (IS_ERR(rdev->pcs)) {
+			err = -EPROBE_DEFER;
+			goto err_pcs_phy_get;
+		}
+
 		err = rsw3_etha_mii_hw_init(rdev->etha_mii);
 		if (err < 0)
 			return err;
@@ -1537,12 +1543,6 @@ static int rsw3_ether_port_init_one(struct rsw3_device *rdev)
 		err = rsw3_mii_register(rdev);
 		if (err < 0)
 			return err;
-
-		rdev->pcs = devm_of_phy_get(&rdev->priv->pdev->dev, rdev->np_port, NULL);
-		if (IS_ERR(rdev->pcs)) {
-			err = -EPROBE_DEFER;
-			goto err_pcs_phy_get;
-		}
 
 		err = rsw3_pcs_set_params(rdev);
 		if (err < 0)
