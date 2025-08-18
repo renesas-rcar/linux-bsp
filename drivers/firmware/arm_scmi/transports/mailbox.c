d762805572188097858227ad43f7f36b30a958c9
Vinh Nguyen <vinh.nguyen.xz@renesas.com>
2025-08-19 01:29:34 +0700

    firmware: arm_scmi: Set mailbox as atomic for r8a78000
    Currently, the SCP FW for R-Car X5H (r8a78000) SoC doesn't support
    TX (agent) done by raising AP core IRQ.
    Hence, this commit together with "atomic-threshold-us = U32_MAX"
    in the  commit 72a931184a3b
    ("arm64: dts: renesas: r8a78000: Add atomic-threshold-us")
    is to make the scmi clock operation be used as atomic mode.
    
    Signed-off-by: Vinh Nguyen <vinh.nguyen.xz@renesas.com>

--- drivers/firmware/arm_scmi/transports/mailbox.c
+++ drivers/firmware/arm_scmi/transports/mailbox.c
@@ -367,6 +367,9 @@ static struct scmi_desc scmi_mailbox_desc = {
 	.max_rx_timeout_ms = 30, /* We may increase this if required */
 	.max_msg = 20, /* Limited by MBOX_TX_QUEUE_LEN */
 	.max_msg_size = 128,
+#if defined(CONFIG_ARCH_R8A78000) && defined(CONFIG_RCAR_SCP_FIXUP)
+	.atomic_enabled = IS_ENABLED(CONFIG_SCMI_FULL_HW_VIRTUALIZATION),
+#endif /* CONFIG_ARCH_R8A78000 && CONFIG_RCAR_SCP_FIXUP */
 };
 
 static const struct of_device_id scmi_of_match[] = {
