1067d9678d5bebea77cfc18d4633a1841aa7bd16
Geert Uytterhoeven <geert+renesas@glider.be>
2025-11-24 10:28:30 +0000

    can: rcar_canfd: Invert CAN clock and close_candev() order
    The CAN clock is enabled before calling open_candev(), and disabled
    before calling close_candev().  Invert the order of the latter, to
    restore symmetry.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Biju Das <biju.das.jz@bp.renesas.com>
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>
    Link: https://patch.msgid.link/20251124102837.106973-6-biju.das.jz@bp.renesas.com
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    (cherry picked from commit ddf9bbf22b70d6b1a10e7d45de8ec3b94de7c54c)
    Signed-off-by: Minh Le <minh.le.aj@renesas.com>

--- drivers/net/can/rcar/rcar_canfd.c
+++ drivers/net/can/rcar/rcar_canfd.c
@@ -1507,9 +1507,9 @@ static int rcar_canfd_close(struct net_device *ndev)
 	netif_stop_queue(ndev);
 	rcar_canfd_stop(ndev);
 	napi_disable(&priv->napi);
-
-	clk_disable_unprepare(gpriv->can_clk);
 	close_candev(ndev);
+	clk_disable_unprepare(gpriv->can_clk);
+
 	return 0;
 }
 
