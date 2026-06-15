621f1abf7fdf863e9ba2756cb5fe45124790deb3
Mai Van Anh <anh.mai.xj@bp.renesas.com>
2026-06-15 16:47:10 +0700

    Revert "media: rcar-vin: Add power management support"
    Revert commit id c9b597d9d27ae1612830c7e26cf9ee216e14ca88
    
    Signed-off-by: Anh Mai <anh.mai.xj@bp.renesas.com>
    Signed-off-by: Thinh Le <thinh.le.yn@renesas.com>

--- drivers/media/platform/renesas/rcar-vin/rcar-dma.c
+++ drivers/media/platform/renesas/rcar-vin/rcar-dma.c
@@ -1294,35 +1294,6 @@ done:
 	return IRQ_RETVAL(handled);
 }
 
-/* Need to hold qlock before calling */
-static void return_all_buffers(struct rvin_dev *vin,
-			       enum vb2_buffer_state state)
-{
-	struct rvin_buffer *buf, *node;
-	struct vb2_v4l2_buffer *freed[HW_BUFFER_NUM];
-	unsigned int i, n;
-
-	for (i = 0; i < HW_BUFFER_NUM; i++) {
-		freed[i] = vin->buf_hw[i].buffer;
-		vin->buf_hw[i].buffer = NULL;
-
-		for (n = 0; n < i; n++) {
-			if (freed[i] == freed[n]) {
-				freed[i] = NULL;
-				break;
-			}
-		}
-
-		if (freed[i])
-			vb2_buffer_done(&freed[i]->vb2_buf, state);
-	}
-
-	list_for_each_entry_safe(buf, node, &vin->buf_list, list) {
-		vb2_buffer_done(&buf->vb.vb2_buf, state);
-		list_del(&buf->list);
-	}
-}
-
 static void return_unused_buffers(struct rvin_dev *vin,
 				  enum vb2_buffer_state state)
 {
@@ -1377,20 +1348,6 @@ static void rvin_buffer_queue(struct vb2_buffer *vb)
 	struct vb2_v4l2_buffer *vbuf = to_vb2_v4l2_buffer(vb);
 	struct rvin_dev *vin = vb2_get_drv_priv(vb->vb2_queue);
 	unsigned long flags;
-
-	if (vin->suspend) {
-		if (!wait_event_timeout(vin->setup_wait,
-					!vin->suspend,
-					msecs_to_jiffies(SETUP_WAIT_TIME))) {
-			dev_warn(vin->dev, "set up timeout\n");
-			spin_lock_irqsave(&vin->qlock, flags);
-			return_all_buffers(vin, VB2_BUF_STATE_ERROR);
-			spin_unlock_irqrestore(&vin->qlock, flags);
-		}
-
-		rvin_capture_start(vin);
-		vin->suspend = false;
-	}
 
 	spin_lock_irqsave(&vin->qlock, flags);
 
@@ -1723,95 +1680,6 @@ static void rvin_stop_streaming_vq(struct vb2_queue *vq)
 	return_unused_buffers(vin, VB2_BUF_STATE_ERROR);
 }
 
-void rvin_resume_start_streaming(struct work_struct *work)
-{
-	struct delayed_work *dwork = to_delayed_work(work);
-	struct rvin_dev *vin =
-			container_of(dwork, struct rvin_dev, rvin_resume);
-	unsigned long flags;
-	int ret;
-
-	ret = rvin_set_stream(vin, 1);
-	if (ret) {
-		dev_warn(vin->dev, "Warning at streaming when resuming.\n");
-		spin_lock_irqsave(&vin->qlock, flags);
-		return_all_buffers(vin, VB2_BUF_STATE_ERROR);
-		spin_unlock_irqrestore(&vin->qlock, flags);
-	}
-
-	spin_lock_irqsave(&vin->qlock, flags);
-	vin->sequence = 0;
-	spin_unlock_irqrestore(&vin->qlock, flags);
-
-	vin->suspend = false;
-	wake_up(&vin->setup_wait);
-}
-
-void rvin_suspend_stop_streaming(struct rvin_dev *vin)
-{
-	unsigned long flags;
-	int retries = 0;
-	u32 vnmc;
-
-	spin_lock_irqsave(&vin->qlock, flags);
-
-	/* Wait for streaming to stop */
-	while (retries++ < RVIN_RETRIES) {
-		rvin_capture_stop(vin);
-
-		/* Check if HW is stopped */
-		if (!rvin_capture_active(vin))
-			break;
-
-		spin_unlock_irqrestore(&vin->qlock, flags);
-		msleep(RVIN_TIMEOUT_MS);
-		spin_lock_irqsave(&vin->qlock, flags);
-	}
-
-	/* Clear UDS usage after we have stopped */
-	if (vin->info->model == RCAR_GEN3) {
-		vnmc = rvin_read(vin, VNMC_REG) & ~(VNMC_SCLE | VNMC_VUP);
-		rvin_write(vin, vnmc, VNMC_REG);
-	}
-
-	/* Release all active buffers */
-	return_all_buffers(vin, VB2_BUF_STATE_ERROR);
-
-	spin_unlock_irqrestore(&vin->qlock, flags);
-
-	rvin_set_stream(vin, 0);
-
-	 /* disable UDS */
-	if (vin->info->scalers)
-		rvin_disable_uds(vin);
-
-	/* disable interrupts */
-	rvin_disable_interrupts(vin);
-
-	if (vin->info->use_mc) {
-		u32 timeout = MSTP_WAIT_TIME;
-
-		pm_runtime_put_sync(vin->dev);
-		pm_runtime_force_suspend(vin->dev);
-
-		while (1) {
-			bool enable;
-
-			enable = __clk_is_enabled(vin->clk);
-			if (!enable)
-				break;
-			if (!timeout) {
-				dev_warn(vin->dev, "MSTP status timeout\n");
-				break;
-			}
-			usleep_range(10, 15);
-			timeout--;
-		}
-	}
-
-	vin->suspend = true;
-}
-
 static const struct vb2_ops rvin_qops = {
 	.queue_setup		= rvin_queue_setup,
 	.buf_prepare		= rvin_buffer_prepare,
@@ -1845,8 +1713,6 @@ int rvin_dma_register(struct rvin_dev *vin, int irq)
 	spin_lock_init(&vin->qlock);
 
 	vin->state = STOPPED;
-	vin->suspend = false;
-	init_waitqueue_head(&vin->setup_wait);
 
 	for (i = 0; i < HW_BUFFER_NUM; i++)
 		vin->buf_hw[i].buffer = NULL;
@@ -1925,8 +1791,6 @@ int rvin_set_channel_routing(struct rvin_dev *vin, u8 chsel)
 		if (ifmd == (VNCSI_IFMD_DES0 | VNCSI_IFMD_DES1))
 			break;
 	}
-
-	vin->chsel = chsel;
 
 	if (ifmd) {
 		ifmd |= VNCSI_IFMD_CSI_CHSEL(chsel);
