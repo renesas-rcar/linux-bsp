ea985b65217d49c6db26fa553ea086700871921f
Phong Hoang <phong.hoang.wz@renesas.com>
2026-06-23 19:04:52 +0700

    misc: ti-tdp2004: Add suspend/resume support
    Add suspend/resume support to reinitialize hardware on resume.
    
    Signed-off-by: Phong Hoang <phong.hoang.wz@renesas.com>

--- drivers/misc/tdp2004.c
+++ drivers/misc/tdp2004.c
@@ -7,8 +7,62 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/phy/phy.h>
+#include <linux/pm.h>
 #include <linux/regmap.h>
 #include <linux/regulator/consumer.h>
+
+struct tdp2004 {
+	struct gpio_desc *enable_gpio;
+};
+
+static int tdp2004_hw_init(struct i2c_client *client)
+{
+	/* Disable test mode for all channels */
+	return i2c_smbus_write_byte_data(client, 0x84, 4);
+}
+
+static int tdp2004_probe(struct i2c_client *client)
+{
+	struct device *dev = &client->dev;
+	struct tdp2004 *tdp;
+
+	tdp = devm_kzalloc(dev, sizeof(*tdp), GFP_KERNEL);
+	if (!tdp)
+		return -ENOMEM;
+
+	tdp->enable_gpio = devm_gpiod_get_optional(dev, "enable", GPIOD_OUT_HIGH);
+	if (IS_ERR(tdp->enable_gpio))
+		return dev_err_probe(dev, PTR_ERR(tdp->enable_gpio),
+				     "unable to acquire enable gpio\n");
+
+	i2c_set_clientdata(client, tdp);
+
+	return tdp2004_hw_init(client);
+}
+
+static void tdp2004_remove(struct i2c_client *client)
+{
+}
+
+static int tdp2004_suspend(struct device *dev)
+{
+	struct tdp2004 *tdp = dev_get_drvdata(dev);
+
+	gpiod_set_value_cansleep(tdp->enable_gpio, 0);
+
+	return 0;
+}
+
+static int tdp2004_resume(struct device *dev)
+{
+	struct tdp2004 *tdp = dev_get_drvdata(dev);
+
+	gpiod_set_value_cansleep(tdp->enable_gpio, 1);
+
+	return tdp2004_hw_init(to_i2c_client(dev));
+}
+
+static DEFINE_SIMPLE_DEV_PM_OPS(tdp2004_pm_ops, tdp2004_suspend, tdp2004_resume);
 
 static const struct of_device_id tdp2004_of_match[] = {
 	{ .compatible = "ti,tdp2004" },
@@ -16,27 +70,11 @@ static const struct of_device_id tdp2004_of_match[] = {
 };
 MODULE_DEVICE_TABLE(of, tdp2004_of_match);
 
-static int tdp2004_probe(struct i2c_client *client)
-{
-	struct device *dev = &client->dev;
-	struct gpio_desc *enable_gpio;
-
-	enable_gpio = devm_gpiod_get_optional(dev, "enable", GPIOD_OUT_HIGH);
-	if (IS_ERR(enable_gpio))
-		return dev_err_probe(dev, PTR_ERR(enable_gpio), "unable to acquire enable gpio\n");
-
-	/* Disable test mode for all channels */
-	return i2c_smbus_write_byte_data(client, 0x84, 4);
-}
-
-static void tdp2004_remove(struct i2c_client *client)
-{
-}
-
 static struct i2c_driver tdp2004_driver = {
 	.driver = {
 		.name = "tdp2004",
 		.of_match_table = tdp2004_of_match,
+		.pm = pm_sleep_ptr(&tdp2004_pm_ops),
 	},
 	.probe = tdp2004_probe,
 	.remove = tdp2004_remove,
